7d8f3d2683d470c3ac15ccedd0250281735476a8
Stella Parker <116223316+whereismyprozac@users.noreply.github.com>
2023-09-30 19:27:01 -0700

    git-config.sh

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -1,1 +1,4 @@
-116223316+whereismyprozac@users.noreply.github.com
+#!/bin/sh
+
+# Enable us to push, through email privacy features
+git config --global user.email 116223316+whereismyprozac@users.noreply.github.com
