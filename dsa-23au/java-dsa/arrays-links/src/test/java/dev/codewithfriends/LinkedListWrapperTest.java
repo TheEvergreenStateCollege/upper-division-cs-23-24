98ba34cdeb7e2794922ecb99f35e5079febb1f8c
Harlee Hair <harlee.hair@evergreen.edu>
2023-10-03 22:00:59 +0000

    Made new branch, Made test for linkedlist add

--- dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/LinkedListWrapperTest.java
+++ dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/LinkedListWrapperTest.java
@@ -16,6 +16,31 @@ public class LinkedListWrapperTest {
 
     Book b1 = new Book("Some title", "some author");
 
+    public void testAdd()
+    {
+    String[] groceryItems = {"Apples", "Bananas","Carrots","Grapes"};
+
+    LinkedList<String> groceries = new LinkedList<>();
+    for(String item : groceryItems)
+    {
+        groceries.add(item);
+    }
+   
+    assertEquals(0, groceries.get(0));
+    assertEquals(1, groceries.get(1));
+    assertEquals(2, groceries.get(2));
+    assertEquals(3, groceries.get(3));
+
+    groceries.add("Candy");
+
+    assertEquals(0, groceries.get(0));
+    assertEquals(1, groceries.get(1));
+    assertEquals(2, groceries.get(2));
+    assertEquals(3, groceries.get(3));
+    assertEquals(4, groceries.get(4));
+
+    }
+
     // Create / Insert operation
     // List Spec
     @Test
@@ -26,6 +51,8 @@ public class LinkedListWrapperTest {
         l.add(b1);
         assertEquals(1, l.size());
     }
+
+
 
 }
 
