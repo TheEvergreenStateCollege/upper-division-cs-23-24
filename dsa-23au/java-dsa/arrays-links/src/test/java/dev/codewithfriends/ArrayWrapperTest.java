f904af3fb9f84d63a11f7fb3f11b7a32c7d73481
de02E <de02@evergreen.edu>
2023-10-03 19:03:10 +0000

    Fixed List not being imported + deleted two useless empty lines.

--- dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/ArrayWrapperTest.java
+++ dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/ArrayWrapperTest.java
@@ -1,14 +1,13 @@
 package dev.codewithfriends;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-
- 
 
 public class ArrayWrapperTest
 {
