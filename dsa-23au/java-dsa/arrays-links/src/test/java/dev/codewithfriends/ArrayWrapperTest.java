3e7da66632b71ba7e5c5840b484405940d8f1038
SP <bunnyhoppy3@gmail.com>
2023-10-03 19:13:21 +0000

    here it is i guess

--- dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/ArrayWrapperTest.java
+++ dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/ArrayWrapperTest.java
@@ -1,14 +1,40 @@
 package dev.codewithfriends;
-
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
-
+import java.util.List;
 /**
  * Unit test for simple App.
  */
 public class ArrayWrapperTest
 {
+    
+      @Test
+      public void testAdd()
+{    
+    String[] s1 ("eggs", "bread","penutbutter","butter");
+    
+
+        List<String> list = new ArrayWrapper<string>(10);
+        list.add(s1[0]);
+        list.add(s1[1]);
+        list.add(s1[2]);
+        list.add(s1[3]);
+
+        int k = 2;
+
+        assertEquals ("eggs", list.get(0));
+        assertEquals ("bread", list.get(1));
+        assertEquals ("penutbutter", list.get(k));
+        assertEquals ("butter", list.get(3));
+
+        list.add(2, "broccoli");
+        assertEquals ("eggs", list.get(0));
+        assertEquals ("bread", list.get(1));
+        assertEquals ("broccoli", list.get(k));
+        assertEquals ("penutbutter", list.get(k+1));
+        assertEquals ("butter", list.get(k+2));
+    }
+
     @Test
     public void add()
     {
---
+++ dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/string.java
@@ -0,0 +1,5 @@
+package dev.codewithfriends;
+
+public class string {
+
+}
