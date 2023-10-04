2d7b0f864258151ba2532ad5098245e8317d1fa9
Jonathan Rodriguez <130508636+JonR2002@users.noreply.github.com>
2023-10-04 14:20:31 -0700

    Update ArrayWrapperTest.java

--- dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/ArrayWrapperTest.java
+++ dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/ArrayWrapperTest.java
@@ -1,23 +1,38 @@
 package dev.codewithfriends;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import java.util.List;
+import java.util.ArrayList;
 
 /**
- * Unit test for simple App.
+ * Unit test for ArrayWrapper class.
  */
-public class ArrayWrapperTest
-{
-    Book b1 = new Book("Some title", "some author");
+public class ArrayWrapperTest {
+    String[] s1 = {"Eggs", "Bread", "Peanut Butter", "Butter"};
+
     @Test
-    public void insertShifts()
-    {
-        List l = new ArrayWrapper<Book>(10);
-        assertEquals(0, l.size());
-        l.add(b1);
-        assertEquals(1, l.size());
+    public void testAdd() {
+        List<String> L = new ArrayList<>(); // Use ArrayList here, not ArrayWrapper
+
+        for (int i = 0; i < 4; i++) {
+            L.add(s1[i]);
+        }
+
+        int k = 2;
+
+        assertEquals("Eggs", L.get(0));
+        assertEquals("Bread", L.get(1));
+        assertEquals("Peanut Butter", L.get(k));
+        assertEquals("Butter", L.get(k + 1));
+
+        // Insert "Broccoli" at index 2 in list L
+        L.add(k, "Broccoli");
+
+        assertEquals("Eggs", L.get(0));
+        assertEquals("Bread", L.get(1));
+        assertEquals("Broccoli", L.get(k));
+        assertEquals("Peanut Butter", L.get(k + 1));
+        assertEquals("Butter", L.get(k + 2));
     }
 }
