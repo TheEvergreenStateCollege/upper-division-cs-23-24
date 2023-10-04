02796bff7b81ede35dd886a47cb1b61b72689cfe
Jonathan Rodriguez <130508636+JonR2002@users.noreply.github.com>
2023-10-04 14:21:04 -0700

    Update LinkedListWrapperTest.java

--- dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/LinkedListWrapperTest.java
+++ dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/LinkedListWrapperTest.java
@@ -1,31 +1,22 @@
 package dev.codewithfriends;
+package dev.codewithfriends;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import java.util.List;
-
-import org.junit.Test;
-
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
 
 public class LinkedListWrapperTest {
 
     Book b1 = new Book("Some title", "some author");
 
     // Create / Insert operation
-    // List Spec
     @Test
-    public void insertShifts()
-    {
-        List l = new ArrayWrapper<Book>(10);
+    public void insertShifts() {
+        List<Book> l = new ArrayList<>(); // Use ArrayList<Book> for a list of Book objects
         assertEquals(0, l.size());
         l.add(b1);
         assertEquals(1, l.size());
     }
-
 }
-
