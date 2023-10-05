859823a7a1a63409f4856042a55ff59766c291a0
SP <bunnyhoppy3@gmail.com>
2023-10-05 05:57:26 +0000

    Homework: Arrays-Links exercise_whereismyprozac

--- dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/ArrayWrapperTest.java
+++ dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/ArrayWrapperTest.java
@@ -1,20 +1,16 @@
 package dev.codewithfriends;
-import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import java.util.List;
-/**
- * Unit test for simple App.
- */
-public class ArrayWrapperTest
-{
-    
-      @Test
-      public void testAdd()
-{    
-    String[] s1 ("eggs", "bread","penutbutter","butter");
-    
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
 
-        List<String> list = new ArrayWrapper<string>(10);
+public class ArrayListTest {
+
+    @Test
+    public void testAdd() {
+        String[] s1 = {"eggs", "bread", "penutbutter", "butter"};
+
+        List<String> list = new ArrayList<String>();
         list.add(s1[0]);
         list.add(s1[1]);
         list.add(s1[2]);
@@ -22,22 +18,22 @@ public class ArrayWrapperTest
 
         int k = 2;
 
-        assertEquals ("eggs", list.get(0));
-        assertEquals ("bread", list.get(1));
-        assertEquals ("penutbutter", list.get(k));
-        assertEquals ("butter", list.get(3));
+        assertEquals("eggs", list.get(0));
+        assertEquals("bread", list.get(1));
+        assertEquals("penutbutter", list.get(k));
+        assertEquals("butter", list.get(3));
 
         list.add(2, "broccoli");
-        assertEquals ("eggs", list.get(0));
-        assertEquals ("bread", list.get(1));
-        assertEquals ("broccoli", list.get(k));
-        assertEquals ("penutbutter", list.get(k+1));
-        assertEquals ("butter", list.get(k+2));
+        assertEquals("eggs", list.get(0));
+        assertEquals("bread", list.get(1));
+        assertEquals("broccoli", list.get(k));
+        assertEquals("penutbutter", list.get(k + 1));
+        assertEquals("butter", list.get(k + 2));
     }
 
     @Test
-    public void add()
-    {
-        assertTrue( true );
+    public void add() {
+       
     }
 }
+
