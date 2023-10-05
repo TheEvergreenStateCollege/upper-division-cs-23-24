87de057c3fc876160b860bd62f08f4b38f673a04
SP <bunnyhoppy3@gmail.com>
2023-10-05 21:31:45 +0000

    add internal array called elements

--- dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/ArrayWrapperTest.java
+++ dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/ArrayWrapperTest.java
@@ -4,7 +4,12 @@ import java.util.List;
 import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
-public class ArrayListTest {
+public class ArrayWrapper<T>
+implements List<E> {
+    private Object[] elements;
+    private int size = 0;
+}
+    public ArrayWrapper{
 
     @Test
     public void testAdd() {
