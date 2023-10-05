1444d1264e6840978f545b14fe3ebb240da73e07
Lucretian <evan.campbell@evergreen.edu>
2023-10-05 20:46:28 +0000

    Revert "Update git-config.sh"
    This reverts commit 101d55beaa5b7ae8046d9ad0f131ea8368fa081f.

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Enable us to push, through email privacy features
-git config --global user.email 146136814+Lucretian@users.noreply.github.com
+git config --global user.email 148553+cryptogoth@users.noreply.github.com
