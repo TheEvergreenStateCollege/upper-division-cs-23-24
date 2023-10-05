ed78fab73bfcedbb8b31b24959d9fcd757ab35a0
Paul Pham <148553+learner-long-life@users.noreply.github.com>
2023-10-05 19:14:42 +0000

    Start new HashMap wrapper project.

---
+++ dsa-23au/java-dsa/hash-map/src/main/java/dev/codewithfriends/HashMapWrapper.java
@@ -0,0 +1,36 @@
+package dev.codewithfriends;
+
+import java.util.Random;
+
+public class HashMapWrapper<K,V> {
+
+    private V _value_array[];
+    private K _key_array[];
+    private int a;
+    private int b;
+    private int m;
+    private int itemCount;
+
+    public HashMapWrapper(int m, int maxSize) {
+        this._value_array = new V[m]; // instantiate an array of type V with size maxSize
+        this.a = Random.nextInt(m);
+        this.b = Random.nextInt(m);
+        this.itemCount = 0;
+    }
+
+/**
+ * Inserts a key-value mapping.
+ * @param key
+ * @param value
+ * @returns
+ */
+    public V put(K key, V value) {
+        // check if exceeded max capacity
+        int hashCode = (((K.hashCode * a) + b) % m);
+        ?? // set the _value_array at hashCode to value
+        this.itemCount += 1;
+        return ?? //
+    }
+
+
+}
