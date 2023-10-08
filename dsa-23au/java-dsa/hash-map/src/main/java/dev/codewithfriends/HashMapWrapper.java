d442cfdc2b956586d625a689092e4aa17c2ea9b2
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2023-10-07 23:34:38 -0700

    Add Rabin-Miller compositeness test and a prime number sieve to improve Hash Maps.

--- dsa-23au/java-dsa/hash-map/src/main/java/dev/codewithfriends/HashMapWrapper.java
+++ dsa-23au/java-dsa/hash-map/src/main/java/dev/codewithfriends/HashMapWrapper.java
@@ -1,5 +1,6 @@
 package dev.codewithfriends;
 
+import java.util.List;
 import java.util.Random;
 
 public class HashMapWrapper<K,V> {
@@ -12,9 +13,21 @@ public class HashMapWrapper<K,V> {
     private int itemCount;
 
     public HashMapWrapper(int m, int maxSize) {
-        this._value_array = new V[m]; // instantiate an array of type V with size maxSize
-        this.a = Random.nextInt(m);
-        this.b = Random.nextInt(m);
+        Random rand = new Random();
+        this._value_array = (V[]) new Object[m]; // instantiate an array of type V with size maxSize
+        long newM = m;
+        List<Long> primes;
+
+        // Repeatedly try to find the smallest prime number still greater than m
+        do {
+            SievePrime sp = new SievePrime((int) newM+10);
+            primes = sp.getNumbersLeft();
+            newM = primes.get(primes.size()-1);
+        } while( newM  <= m);
+
+        this.m = (int)newM; // Somewhere we should check overflow and that we only want primes that fit in an integer
+        this.a = rand.nextInt(m); // guaranteed to be co-prime
+        this.b = rand.nextInt(m); // guaranteed to be co-prime
         this.itemCount = 0;
     }
 
@@ -26,10 +39,11 @@ public class HashMapWrapper<K,V> {
  */
     public V put(K key, V value) {
         // check if exceeded max capacity
-        int hashCode = (((K.hashCode * a) + b) % m);
-        ?? // set the _value_array at hashCode to value
-        this.itemCount += 1;
-        return ?? //
+        //int hashCode = (((K.hashCode * a) + b) % m);
+        // ?? // set the _value_array at hashCode to value
+        //this.itemCount += 1;
+        //return ?? //
+        return null;
     }
 
 
