9c86bcd7937a477c7e3c15bf8b38089f0da3fa3c
Lohen Kinsman <31353968+parcania@users.noreply.github.com>
2023-10-16 15:02:44 -0700

    Update Clerk.java
    I recycled the script from Driver class and used a variation in the Clerk class

--- dsa-23au/java-dsa/weiss-pham-dmv/src/main/java/dev/codewithfriends/Clerk.java
+++ dsa-23au/java-dsa/weiss-pham-dmv/src/main/java/dev/codewithfriends/Clerk.java
@@ -1,6 +1,18 @@
 public class Clerk {
     
     // member variables
+    String name;
+    String department;
+    
+
+    public Clerk(
+        String name;
+       
+    ) {
+        this.name = name;
+        this.department = department;
+    }
+
 
     public Clerk() {
         
