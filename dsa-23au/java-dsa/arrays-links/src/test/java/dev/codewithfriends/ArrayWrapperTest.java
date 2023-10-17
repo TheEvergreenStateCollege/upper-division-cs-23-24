c29a2889ff16f6f3a71c5683714c72ff034629c5
Ssellers84 Sellers <seansellers84@gmail.com>
2023-10-17 17:04:11 +0000

    testing

--- dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/ArrayWrapperTest.java
+++ dsa-23au/java-dsa/arrays-links/src/test/java/dev/codewithfriends/ArrayWrapperTest.java
@@ -4,24 +4,54 @@ import java.util.List;
 import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-public class ArrayWrapperTest{
+
+public class ArrayWrapperTest {
+
+    String[] s1 = {"Eggs", "Bread", "Peanut Butter", "Butter"};
 
     @Test
-    public void testSize(){
-        assertEquals(null, 0, 0, 0);
+    public void testAdd() {
+        List<String> L = new ArrayWrapper<String>(10); 
+        L.add(s1[0]);
+        L.add(s1[1]);
+        L.add(s1[2]);
+        L.add(s1[3]);
+
+        int k = 2;
+
+        assertEquals("Eggs", L.get(0));
+        assertEquals("Bread", L.get(1));
+        assertEquals("Peanut Butter", L.get(k));
+        assertEquals("Butter", L.get(k + 1));
+
+        // Insert "Broccoli" at index 2 in list L
+        L.add(k, "Broccoli");
+
+        assertEquals("Eggs", L.get(0));
+        assertEquals("Bread", L.get(1));
+        assertEquals("Broccoli", L.get(k));
+        assertEquals("Peanut Butter", L.get(k + 1));
+        assertEquals("Butter", L.get(k + 2);
     }
 
     @Test
-    public void test_isEmtpy(){
-        List<String> l = new ArrayWrapper<String>(10);
-        assertTrue(l.size() == 0);
+    public void testSize() {
+        List<String> L = new ArrayWrapper<String>(10);
+        assertEquals(0, L.size());
     }
 
     @Test
-    public void test_toArray(){
-        // need lots of logic
-        return;
+    public void test_isEmpty() {
+        List<String> L = new ArrayWrapper<String>(10);
+        assertTrue(L.isEmpty());
     }
+
+    @Test
+    public void test_toArray() {
+       
+    }
+}
+
 
     public void testAdd() {
         List<String> L = new ArrayList<>(); // Use ArrayList here, not ArrayWrapper
