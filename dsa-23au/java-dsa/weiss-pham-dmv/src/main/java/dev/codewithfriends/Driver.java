f7bfd0659cde9952db15394ad655097a0a358827
Stella Parker <116223316+whereismyprozac@users.noreply.github.com>
2023-10-17 10:34:20 -0700

    Update Driver.java whereismyprozac
    So as a whole it runs but with a couple of errors that aren't very obvious on how to fix.
    Here is the message.
    
    Running] cd "/workspace/upper-division-cs/dsa-23au/java-dsa/weiss-pham-dmv/src/main/java/dev/codewithfriends/" && javac Driver.java && java Driver
    Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
    Driver.java:34: error: cannot find symbol
        public boolean isLateForAppointment(Appointment appointment) {
                                            ^
      symbol:   class Appointment
      location: class Driver
    1 error

--- dsa-23au/java-dsa/weiss-pham-dmv/src/main/java/dev/codewithfriends/Driver.java
+++ dsa-23au/java-dsa/weiss-pham-dmv/src/main/java/dev/codewithfriends/Driver.java
@@ -1,11 +1,15 @@
+package dev.codewithfriends;
+import java.util.Date;
+
 public class Driver {
     
     // member variables
-    String name;
-    String address;
-    String licenseNumber;
-    Date birthdate;
-    boolean hasAppointment;
+    private String name;
+    private String address;
+    private String licenseNumber;
+    private Date birthdate;
+    private boolean hasAppointment;
+    private Date checkInTime;  // added check-in time
 
     public Driver(
         String name,
@@ -19,9 +23,50 @@ public class Driver {
         this.licenseNumber = licenseNumber;
         this.birthdate = birthdate;
         this.hasAppointment = hasAppointment;
+        this.checkInTime = null;  // Initializing with no check-in time
     }
 
-    // TODO: have a checkin method for driver, compare
-    // their checkin time with the appointment time
-    // if it's too late, move from high priority to low priority
+    // New method to handle check-in
+    public void checkIn() {
+        this.checkInTime = new Date(); // Capture the current time as check-in time
+    }
+
+    public boolean isLateForAppointment(Appointment appointment) {
+        if (this.hasAppointment && this.checkInTime != null) {
+            if (this.checkInTime.after(appointment.getDate())) { 
+                return true;  // The driver is late
+            }
+        }
+        return false; // The driver is not late
+    }
+
+   
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public boolean hasAppointment() {
+        return hasAppointment;
+    }
+
+    public Date getCheckInTime() {
+        return checkInTime;
+    }
+
+    public void setHasAppointment(boolean hasAppointment) {
+        this.hasAppointment = hasAppointment;
+    }
 }
+
