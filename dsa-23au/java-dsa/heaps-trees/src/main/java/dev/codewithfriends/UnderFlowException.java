ed4088c8860bc110100b2b8b6b32ae23e1b0dacf
Paul Pham <148553+learner-long-life@users.noreply.github.com>
2023-10-17 22:28:36 +0000

    BinarySearchTree compiles with unit test.

---
+++ dsa-23au/java-dsa/heaps-trees/src/main/java/dev/codewithfriends/UnderFlowException.java
@@ -0,0 +1,12 @@
+package dev.codewithfriends;
+
+public class UnderFlowException extends Exception {
+    
+    public UnderFlowException() {
+        super();
+    }
+    
+    public UnderFlowException(String message) {
+        super(message);
+    }
+}
