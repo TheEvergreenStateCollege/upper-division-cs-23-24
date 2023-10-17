0ee89de0f438bd65e80cd0a2beadd274a5b4db2d
Stella Parker <116223316+whereismyprozac@users.noreply.github.com>
2023-10-17 10:28:53 -0700

    Update Clerk.java whereismyprozac

--- dsa-23au/java-dsa/weiss-pham-dmv/src/main/java/dev/codewithfriends/Clerk.java
+++ dsa-23au/java-dsa/weiss-pham-dmv/src/main/java/dev/codewithfriends/Clerk.java
@@ -1,8 +1,14 @@
+package dev.codewithfriends;
+
 public class Clerk {
     
-    // member variables
-
     public Clerk() {
-        
+       
+    }
+
+    public void processDriver(Driver driver) {
+        // process a driver here
+        // print the driver's details
+        System.out.println("Processing driver: " + driver.getName());
     }
 }
