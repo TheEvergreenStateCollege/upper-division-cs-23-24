7525f04d2faf3d6700e066352e310030a7219534
Lucretian <evan.campbell@evergreen.edu>
2023-10-17 21:28:15 +0000

    Juni BinaryNode

---
+++ dsa-23au/java-dsa/heaps-trees/src/main/java/dev/codewithfriends/BinaryNode.java
@@ -0,0 +1,31 @@
+package dev.codewithfriends;
+
+private static class BinaryNode<T>
+{
+    private T data;
+    private BinaryNode<T> left;
+    private BinaryNode<T> right;
+
+        //Constructors
+    BinaryNode( T, data )
+    {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public T getData()
+    {
+        return data;
+    }
+
+    public BinaryNode<T> getLeft()
+    {
+        return right;
+    }
+
+    public BinaryNode<T> getRight()
+    {
+        return left;
+    }
+}
