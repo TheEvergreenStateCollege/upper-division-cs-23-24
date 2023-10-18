142a0fb305fb2300a12978e5f5ca1c310ee6ccbd
EC2 Default User <ec2-user@ip-172-31-7-169.us-west-1.compute.internal>
2023-10-18 19:57:33 +0000

    Adding some code for final project work/development

--- dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/main/java/com/pswishcorp/app/HighwayTune.java
+++ dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/main/java/com/pswishcorp/app/HighwayTune.java
@@ -1,50 +1,12 @@
 package com.pswishcorp.app;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class HighwayTune {
 
-    public static void main(String[] args) {
-        Map<String, Map<String, Integer>> driverNameToDriverData = new HashMap<>();
-        // Create a dictionary of dictionaries
-        
-        Map<String, Integer> innerDict1 = new HashMap<>();
-        innerDict1.put("Entry1", 42);
-        innerDict1.put("Entry2", 73);
-
-        Map<String, Integer> innerDict2 = new HashMap<>();
-        innerDict2.put("keyA", 100);
-        innerDict2.put("keyB", 200);
-
-        driverNameToDriverData.put("driver1", innerDict1);
-        driverNameToDriverData.put("driver2", innerDict2);
-
-/*
-Data structure model selected(As of 10/17)
-Example from: 
-    public static void main(String[] args) {
-        // Create a dictionary of dictionaries
-        Map<String, Map<String, Integer>> dictOfDicts = new HashMap<>(); 
-        // Add data to the dictionary of dictionaries
-        Map<String, Integer> innerDict1 = new HashMap<>();
-        innerDict1.put("key1", 42);
-        innerDict1.put("key2", 73);
-
-        Map<String, Integer> innerDict2 = new HashMap<>();
-        innerDict2.put("keyA", 100);
-        innerDict2.put("keyB", 200);
-
-        dictOfDicts.put("dict1", innerDict1);
-        dictOfDicts.put("dict2", innerDict2);
-
-        // Access values in the dictionary of dictionaries
-        int value1 = dictOfDicts.get("dict1").get("key1");
-        int value2 = dictOfDicts.get("dict2").get("keyA");
-
-        System.out.println("Value in dict1: " + value1);
-        System.out.println("Value in dict2: " + value2);
-    }
-*/
-}
+     public static void main(String[] args) {
+     // Get data for driver on date x   
+     Driver Driver = new Driver(0, null, null, null, null, null);
+     Driver.getDriverName("Paul");
+     }
 }
---
+++ dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/main/java/com/pswishcorp/app/MapHashMap.java
@@ -0,0 +1,53 @@
+package main.java.com.pswishcorp.app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapHashMap {
+        public static void main(String[] args) {
+        Map<String, Map<String, Integer>> driverNameToDriverData = new HashMap<>();
+        // Create a dictionary of dictionaries
+        
+        Map<String, Integer> innerDict1 = new HashMap<>();
+        innerDict1.put("TravelTime", 22);
+        innerDict1.put("MilesTravelled", 11);
+
+        Map<String, Integer> innerDict2 = new HashMap<>();
+        innerDict2.put("TravelTime", 100);
+        innerDict2.put("MilesTravelled", 200);
+
+        driverNameToDriverData.put("driver1", innerDict1);
+        driverNameToDriverData.put("driver2", innerDict2);
+        /// WIP
+        int value1 = driverNameToDriverData.get("dict1").get("key1");
+        
+    }
+    
+}
+
+/*
+Data structure model selected(As of 10/17)
+Example from: 
+    public static void main(String[] args) {
+        // Create a dictionary of dictionaries
+        Map<String, Map<String, Integer>> dictOfDicts = new HashMap<>(); 
+        // Add data to the dictionary of dictionaries
+        Map<String, Integer> innerDict1 = new HashMap<>();
+        innerDict1.put("key1", 42);
+        innerDict1.put("key2", 73);
+
+        Map<String, Integer> innerDict2 = new HashMap<>();
+        innerDict2.put("keyA", 100);
+        innerDict2.put("keyB", 200);
+
+        dictOfDicts.put("dict1", innerDict1);
+        dictOfDicts.put("dict2", innerDict2);
+
+        // Access values in the dictionary of dictionaries
+        int value1 = dictOfDicts.get("dict1").get("key1");
+        int value2 = dictOfDicts.get("dict2").get("keyA");
+
+        System.out.println("Value in dict1: " + value1);
+        System.out.println("Value in dict2: " + value2);
+    }
+*/
