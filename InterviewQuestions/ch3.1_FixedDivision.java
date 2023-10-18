e4ccef90bfdba870d334267cb4de8cdb991b0f94
pswish <pswish@gmail.com>
2023-10-18 13:07:41 -0700

    adding some code for final project work/development

---
+++ InterviewQuestions/ch3.1_FixedDivision.java
@@ -0,0 +1,15 @@
+class FixedMultiStack{
+    private int numberOfStacks = 3;
+    private int stackCapacity;
+    private int[] values;
+    private int[] sizes;
+
+    public FixedMultiStack(int stackSize){
+        stackCapacity = stackSize;
+        values = new int[stackSize * numberOfStacks];
+        sizes = new int[numberOfStacks];
+    }
+
+    /* Push value onto stack */
+    public void push(int stackNum, int value) thorws
+}
