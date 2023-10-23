4de6bca31b65f0489a564ad0701b13d6eadd9b0e
ndeanon25 <146136622+ndeanon25@users.noreply.github.com>
2023-10-23 18:39:21 +0000

    My mvn project

---
+++ dsa-23au/ndeanon25/src/test/java/com/ndeanon25/AppTest.java
@@ -0,0 +1,20 @@
+package com.ndeanon25;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
