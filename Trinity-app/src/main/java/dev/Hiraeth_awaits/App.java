960f30544c87ed18528d20e5531f030def8d229b
HiraethAwaits <146136772+HiraethAwaits@users.noreply.github.com>
2023-10-23 19:28:58 +0000

    git

---
+++ Trinity-app/src/main/java/dev/Hiraeth_awaits/App.java
@@ -0,0 +1,13 @@
+package dev.Hiraeth_awaits;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
