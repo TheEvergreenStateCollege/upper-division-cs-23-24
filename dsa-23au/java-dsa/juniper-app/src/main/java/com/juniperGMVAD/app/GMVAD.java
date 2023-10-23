04829d2758dc4232f8251e9a641fa3275bb0a31d
Lucretian <evan.campbell@evergreen.edu>
2023-10-23 19:04:23 +0000

    Juniper Project

---
+++ dsa-23au/java-dsa/juniper-app/src/main/java/com/juniperGMVAD/app/GMVAD.java
@@ -0,0 +1,18 @@
+package com.juniperGMVAD.app;
+
+private static void main(String args[])
+{
+
+}
+
+/*
+Consider complextiy & big O shit
+-Calculate the percent value of gmva
+-Create a maxheap of the countries by gmva
+	->first in first out?
+-take top 5 countries and put them into a hashmap
+-do same with 5 bottom
+-create something that represents the change in the top 5 countries and bottom 5 countries
+-Compare this change in the top 5 countries for wages and bottom 5 and top 5 countries for interest rate and bottom 5
+-What countries had the greatest increase? Decrease?
+*\
