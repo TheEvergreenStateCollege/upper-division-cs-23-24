18cac783dbc2119bd6ceddb212e5d46608285aea
HiraethAwaits <146136772+HiraethAwaits@users.noreply.github.com>
2023-10-24 21:12:51 +0000

    Finally compiled the Dfs Diagram

--- dsa-23au/java-dsa/heaps-trees/src/main/java/dev/codewithfriends/BSTMain.java
+++ dsa-23au/java-dsa/heaps-trees/src/main/java/dev/codewithfriends/BSTMain.java
@@ -12,7 +12,7 @@ public class BSTMain {
         }
         return false;
     }
-
+    
     public static List<Integer> dfs(BinaryNode<Integer> node, List<Integer> visited) {
 
         // mark the current node as visited
@@ -30,12 +30,44 @@ public class BSTMain {
         return visited;
         // update
     }
+    /**
+     * @param node
+     * @param diagramSofar
+     * @return
+     */
+    public static List<String> dfsDiagram(BinaryNode<Integer> node, List<String> diagramSofar) {
+
+        // mark the current node as visited
+
+        // if there is a left, or left unvisited
+        if (node.left != null) {
+            diagramSofar.add(String.format("   %s --> %s", node.data, node.left.data));
+            dfsDiagram(node.left, diagramSofar);
+        }
+        // if there is a right, or right unvisited
+        if (node.right != null) {
+            diagramSofar.add(String.format("   %s --> %s", node.data, node.right.data));
+            dfsDiagram(node.right, diagramSofar);
+        }
+        // then return
+        return diagramSofar;
+        // update
+    }
 
   public static void main(String[] args) 
     {
         LinkedList<Integer> visited = new LinkedList<>();
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        bst.insert(73);
+        bst.insert(22);
         List<Integer> result = dfs(bst.getRoot(), visited);
+        for (Integer i : result) {
+            System.out.println(i);
+        }
+        List<String> diagramLines = dfsDiagram(bst.getRoot(), new LinkedList<>());
+        for (String d : diagramLines) {
+            System.out.println(d);
+        }
 
 
 
