1c24b6bd69101876f78f370f7939e2ee38c99d68
pswish <pswish@gmail.com>
2023-10-25 01:00:53 +0000

    Adding some updates

--- dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/main/java/com/pswishcorp/app/Car.java
+++ dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/main/java/com/pswishcorp/app/Car.java
@@ -1,40 +1,40 @@
-package com.pswishcorp.app;
+// package com.pswishcorp.app;
 
-public class Car {
-    String cartype;
-    String driver;
-    int hwyMPG;
-    String carMake;
-    String carModel;
-    String fuelType;
-    String availabilityStatus;
-    double averageCostPerGallonPrem;
+// public class Car {
+//     String cartype;
+//     String driver;
+//     int hwyMPG;
+//     String carMake;
+//     String carModel;
+//     String fuelType;
+//     String availabilityStatus;
+//     double averageCostPerGallonPrem;
 
-    public Car(String driver, String cartype, int hwyMPG, double averageCostPerGallonPrem) {
-        this.cartype = cartype;
-        this.hwyMPG = hwyMPG;
-        this.averageCostPerGallonPrem = averageCostPerGallonPrem;
-    }
+//     public Car(String driver, String cartype, int hwyMPG, double averageCostPerGallonPrem) {
+//         this.cartype = cartype;
+//         this.hwyMPG = hwyMPG;
+//         this.averageCostPerGallonPrem = averageCostPerGallonPrem;
+//     }
 
-    public static void main(String[] args) {
-        Car car1 = new Car("Paul","Suv", 27, .16);
-        Car car2 = new Car("Nate", "Sedan", 28,  .17);
-        }
+//     public static void main(String[] args) {
+//         Car car1 = new Car("Paul","Suv", 27, .16);
+//         Car car2 = new Car("Nate", "Sedan", 28,  .17);
+//         }
 
-    public Object getDriver() throws Exception {
-        throw new Exception("Not yet implemented, v0.4");
-    }
+//     public Object getDriver() throws Exception {
+//         throw new Exception("Not yet implemented, v0.4");
+//     }
 
-    public Object getCarType() throws Exception {
-        throw new Exception("Not yet implemented, v0.4");
-    }
+//     public Object getCarType() throws Exception {
+//         throw new Exception("Not yet implemented, v0.4");
+//     }
 
-    public Object getHwyMPG() throws Exception {
-        throw new Exception("Not yet implemented, v0.4");
-    }
+//     public Object getHwyMPG() throws Exception {
+//         throw new Exception("Not yet implemented, v0.4");
+//     }
 
-    public double getFuelCostPerGallon() throws Exception {
-        throw new Exception("Not yet implemented, v0.4");
-    }
-    }
+//     public double getFuelCostPerGallon() throws Exception {
+//         throw new Exception("Not yet implemented, v0.4");
+//     }
+//     }
 
