fa4252af396a4c6a60958e202d4be4ca75157d68
pswish <pswish@gmail.com>
2023-10-27 06:20:15 +0000

    Fixing typos

--- dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/test/java/com/pswishcorp/app/BufferTest.java
+++ dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/test/java/com/pswishcorp/app/BufferTest.java
@@ -3,8 +3,6 @@ package com.pswishcorp.app;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
@@ -16,7 +14,7 @@ public class BufferTest {
 
     @Before
     public void setUp() {
-        // Set the file paths for testing
+        // file paths for testing
         path1 = "/home/ec2-user/workspace/Evergreen/upper-division-cs/dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/resources/DataSet_DSAau_pswish.csv";
         path2 = "/home/ec2-user/workspace/Evergreen/upper-division-cs/dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/resources/Time_Driving_Spreadsheet.csv";
     }
@@ -24,13 +22,13 @@ public class BufferTest {
     @Test
     public void testReadCSVFile1() throws IOException {
         List<String[]> data = Buffer.readCSVFile(path1);
-        assertEquals(39, data.size()); // Assuming there are 2 lines in the test file
+        assertEquals(39, data.size()); // Assuming there are 39 lines in the test file
     }
 
     @Test
     public void testReadCSVFile2() throws IOException {
         List<String[]> data = Buffer.readCSVFile(path2);
-        assertEquals(27, data.size()); // Assuming there are 3 lines in the test file
+        assertEquals(27, data.size()); // Assuming there are 27 lines in the test file
         }
     }
 
