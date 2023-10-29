f7d19da08a701e90c4526b19b94923e836c3ed53
pswish <pswish@gmail.com>
2023-10-29 02:14:39 +0000

    Final project collection, v0.55
    1. Why is this change being made?
    	Project work continues
    2. What has changed?
    	Updated Buffer class to creat an object instead of an array,
    	updated tests
    3. How was this tested? Command line output, mvn test, and unittests
    4. Anything else to discus?
    	Sort function needed, heap sort maybe?
    	Data structure needed.thinking an array of hashsets might be viable for maintinaing order
    	of sort and then adding each key to a hashset per field in line
    5. Any other documentation? Not at this time

--- dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/test/java/com/pswishcorp/app/BufferTest.java
+++ dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/test/java/com/pswishcorp/app/BufferTest.java
@@ -1,34 +1,75 @@
 package com.pswishcorp.app;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.Time;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class BufferTest {
-    private String path1;
-    private String path2;
-
-    @Before
-    public void setUp() {
-        // file paths for testing
-        path1 = "/home/ec2-user/workspace/Evergreen/upper-division-cs/dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/resources/DataSet_DSAau_pswish.csv";
-        path2 = "/home/ec2-user/workspace/Evergreen/upper-division-cs/dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/resources/Time_Driving_Spreadsheet.csv";
-    }
 
     @Test
-    public void testReadCSVFile1() throws IOException {
-        List<String[]> data = Buffer.readCSVFile(path1);
-        assertEquals(39, data.size()); // Assuming there are 39 lines in the test file
-    }
+    public void testReadCSVFile1() {
+        String path1 = "/home/ec2-user/workspace/Evergreen/upper-division-cs/dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/resources/DataSet_DSAau_pswish.csv";
 
-    @Test
-    public void testReadCSVFile2() throws IOException {
-        List<String[]> data = Buffer.readCSVFile(path2);
-        assertEquals(27, data.size()); // Assuming there are 27 lines in the test file
+        try {
+            List<Buffer.BufferObject> data1 = Buffer.readCSVFile(path1);
+
+            // Assert that the data list is not null
+            assertNotNull(data1);
+            assertTrue(data1.size() > 0);
+            assertEquals(39, data1.size()); // Assuming there are 39 lines in the test file
+
+            // Add more specific tests for your data as needed
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
+    @Test
+    public void testReadCSVFile2() {
+        String path2 = "/home/ec2-user/workspace/Evergreen/upper-division-cs/dsa-23au/java-dsa/pswish-natmcl/pswish-app/src/resources/Time_Driving_Spreadsheet.csv";
+
+        try {
+            List<Buffer.BufferObject> data2 = Buffer.readCSVFile(path2);
+
+            // Assert that the data list is not null
+            assertNotNull(data2);
+            assertTrue(data2.size() > 0);
+            assertEquals(27, data2.size()); // Assuming there are 27 lines in the test file
+
+            // Add more specific tests for your data as needed
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testBufferObjectFields() {
+        String time = String.valueOf(1800);
+        String date = String.valueOf("10/28/2023");
+        String trip = "11 mins";
+        String origin = "Origin";
+        String destination = "Destination";
+        Boolean detour = true;
+        String driver = "Driver";
+        String distance = "11 miles";
+
+        Buffer.BufferObject bufferObject = new Buffer.BufferObject(time, date, trip, origin, destination, detour, driver, distance);
+
+        // Test individual fields of the BufferObject
+        assertEquals(time, bufferObject.getTime());
+        assertEquals(date, bufferObject.getDate());
+        assertEquals(trip, bufferObject.getTrip());
+        assertEquals(origin, bufferObject.getOrigin());
+        assertEquals(destination, bufferObject.getDestination());
+        assertEquals(detour, bufferObject.getDetour());
+        assertEquals(driver, bufferObject.getDriver());
+        assertEquals(distance, bufferObject.getDistance());
+    }
+}
