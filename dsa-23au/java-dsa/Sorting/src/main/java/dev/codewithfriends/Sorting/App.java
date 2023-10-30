7d58c82d98445306aee5ab6444614f1a53ddbb93
Jonathan Rodriguez <30508636+JonR2002@users.noreply.github.com>
2023-10-30 18:07:50 +0000

    Got a build with these new methods

--- dsa-23au/java-dsa/Sorting/src/main/java/dev/codewithfriends/Sorting/App.java
+++ dsa-23au/java-dsa/Sorting/src/main/java/dev/codewithfriends/Sorting/App.java
@@ -1,5 +1,7 @@
 package dev.codewithfriends.Sorting;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -13,25 +15,38 @@ package dev.codewithfriends.Sorting;
 
 public class App 
 {
-    public static void insertionSort(int[] arr) {
-        int n = arr.length;
+ 
 
-        for (int i = 1; i < n; i++) {
-            int key = arr[i];
-            int j = i - 1;
-
-            // Move elements of arr[0..i-1] that are greater than key
-            // to one position ahead of their current position
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
+    
+        public static void main(String[] args) {
+            int[] numbers = {12, 3, 21, 8, 1, 47, 50, 88, 3, 13};
+            insertionSort(numbers);
+            System.out.println("Sorted Numbers: " + Arrays.toString(numbers));
+        }
+    
+        public static int[] insertionSort(int[] arr) {
+            int n = arr.length;
+            for (int i = 1; i < n; i++) {
+                int key = arr[i];
+                int j = i - 1;
+    
+                while (j >= 0 && arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+    
+                arr[j + 1] = key;
             }
-            arr[j + 1] = key;
+            return arr;
+        }
+
+        public static int[] mergeSort(int[] arr) {
+            
+            throw new RuntimeException();
+        }
+
+        public static int[] mergeSortHelper(int[] arr, int start, int end){
+
         }
     }
-}
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+    
