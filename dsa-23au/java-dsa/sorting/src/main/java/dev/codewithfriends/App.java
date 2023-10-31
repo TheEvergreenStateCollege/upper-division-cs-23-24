521c682ab8c7f1c629870ddb30abff881fdf82ee
Jonathan Rodriguez <30508636+JonR2002@users.noreply.github.com>
2023-10-31 17:51:16 +0000

    Added merge sort functionality (Hasnt been tested)

--- dsa-23au/java-dsa/sorting/src/main/java/dev/codewithfriends/App.java
+++ dsa-23au/java-dsa/sorting/src/main/java/dev/codewithfriends/App.java
@@ -34,12 +34,36 @@ public class App
     public static int[] mergeSortHelper(int[] arr, int start, int end){
         int[] newArray = new int[end - start + 1]; 
         int length = end - start + 1;
+        int mid = (length / 2) - 1;
 
-        mergeSortHelper(arr, start, start + (length / 2));       // left half
-        mergeSortHelper(arr, start + (length / 2), end + 1);     // right half
+        int[] arr1 = mergeSortHelper(arr, start, mid);       // left half
+        int[] arr2 = mergeSortHelper(arr, mid, end);     // right half
         //Split into left and right
         //Recursively call mergeSortHelper on left and right halves
+        int j = 0; int k = 0;
+        for (int i = 0; i < (arr1.length + arr2.length); i++) {
+            if (j == arr1.length) {
+                newArray[i] = arr2[k];
+                k++;
+                continue;
+            }
+            else if (k == arr2.length){
+                newArray[i] = arr1[j];
+                j++;
+                continue;
+                
+            }
+            if (arr1[j] >= arr2[k]) {
+                newArray[i] = arr1[j];
+                j++;
+            }
+            else {
+                newArray[i] = arr2[k];
+                k++;
+            }
+        }
+        
 
-        return arr;
+        return newArray;
     } 
 }
