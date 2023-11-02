3ada9a6a95280097c2d6f339d22c8ea6e18a8cb1
EvergreenTor s <Torsten.S@evergreen.edu>
2023-11-02 03:29:59 +0000

    updated code

--- dsa-23au/java-dsa/Torsten/src/main/java/com/ActivityTor/app/DataStorage.java
+++ dsa-23au/java-dsa/Torsten/src/main/java/com/ActivityTor/app/DataStorage.java
@@ -15,59 +15,33 @@ public class DataStorage {
     private HashMap<String, Integer> flightsClimbed = new HashMap<>();
     private HashMap<String, Double> handWashingSeconds = new HashMap<>();
 
-    public void addStepCount(String date, int stepCount ){
-        stepCounts.put(date, stepCount);
-    }
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-    public void updateStepCount(String date, int newStepCount){
-        stepCounts.put(date, newStepCount);
+    public void addStepCount(String date, int stepCount) {
+        stepCounts.put(date, stepCount);
     }
 
     public void addDistance(String date, double distance) {
         distances.put(date, distance);
     }
 
-    public void updateDistance(String date, double newDistance) {
-        distances.put(date, newDistance);
-    }
-
     public void addCalories(String date, double calorie) {
         calories.put(date, calorie);
-    }
-
-    public void updateCalories(String date, double newCalorie) {
-        calories.put(date, newCalorie);
     }
 
     public void addRestingEnergy(String date, double energy) {
         restingEnergy.put(date, energy);
     }
-
-    public void updateRestingEnergy(String date, double newEnergy) {
-        restingEnergy.put(date, newEnergy);
-    }
-
     public void addSoundLevels(String date, double level) {
         soundLevels.put(date, level);
-    }
-
-    public void updateSoundLevels(String date, double newLevel) {
-        soundLevels.put(date, newLevel);
     }
 
     public void addFlightsClimbed(String date, int flights) {
         flightsClimbed.put(date, flights);
     }
 
-    public void updateFlightsClimbed(String date, int newFlights) {
-        flightsClimbed.put(date, newFlights);
-    }
     public void addHandWashingSeconds(String date, double seconds) {
         handWashingSeconds.put(date, seconds);
-    }
-
-    public void updateHandWashingSeconds(String date, double newSeconds) {
-        handWashingSeconds.put(date, newSeconds);
     }
 }
 
