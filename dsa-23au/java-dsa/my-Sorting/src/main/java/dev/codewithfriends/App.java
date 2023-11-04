c6a5b86519ff143d174ec54dc1ce60c59a33cfa4
EvergreenTor s <Torsten.S@evergreen.edu>
2023-11-04 20:40:08 +0000

    Quicksort Addded to my-Sorting

--- dsa-23au/java-dsa/my-Sorting/src/main/java/dev/codewithfriends/App.java
+++ dsa-23au/java-dsa/my-Sorting/src/main/java/dev/codewithfriends/App.java
@@ -95,4 +95,38 @@ public class App
 
         
     }
+
+    public static void quickSort(int[] array, int low, int high){
+        if(low < high){
+            int pivotIndex = partition(array, low, high);
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+
+    }
+
+    public static int partition(int[] array, int low, int high){
+
+        int pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return i + 1;
+    }
+
+
+
+    }
 }
