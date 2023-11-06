9ca4d40e19f0cc0f79b76e869c60f2b091ab49ee
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2023-11-05 18:23:59 -0800

    Merge local changes from name-crawler native-image building.

---
+++ dsa-23au/notes/docs/PeriodicTable/grid.js
@@ -0,0 +1,71 @@
+function gridData() {
+	var data = new Array();
+	var xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
+	var ypos = 1;
+	var width = 50;
+	var height = 50;
+	var click = 0;
+	
+	// iterate for rows	
+	for (var row = 0; row < 10; row++) {
+		data.push( new Array() );
+		
+		// iterate for cells/columns inside rows
+		for (var column = 0; column < 10; column++) {
+			data[row].push({
+        text: "text",
+				x: xpos,
+				y: ypos,
+				width: width,
+				height: height,
+				click: click
+			})
+			// increment the x position. I.e. move it over by 50 (width variable)
+			xpos += width;
+		}
+		// reset the x position after a row is complete
+		xpos = 1;
+		// increment the y position for the next row. Move it down 50 (height variable)
+		ypos += height;	
+	}
+	return data;
+}
+
+var gridData = gridData();	
+// I like to log the data to the console for quick debugging
+console.log(gridData);
+
+var grid = d3.select("#grid")
+	.append("svg")
+	.attr("width","510px")
+	.attr("height","510px");
+	
+var row = grid.selectAll(".row")
+	.data(gridData)
+	.enter().append("g")
+	.attr("class", "row");
+	
+var column = row.selectAll(".square")
+	.data(function(d) { return d; })
+	.enter().append("g")
+  .append("circle")
+	.attr("class","square")
+	.attr("x", function(d) { return d.x; })
+	.attr("y", function(d) { return d.y; })
+	.attr("width", function(d) { return d.width; })
+	.attr("height", function(d) { return d.height; })
+	.style("fill", "#fff")
+	.style("stroke", "#222")
+	.on('click', function(d) {
+       d.click ++;
+       if ((d.click)%4 == 0 ) { d3.select(this).style("fill","#fff"); }
+	   if ((d.click)%4 == 1 ) { d3.select(this).style("fill","#2C93E8"); }
+	   if ((d.click)%4 == 2 ) { d3.select(this).style("fill","#F56C4E"); }
+	   if ((d.click)%4 == 3 ) { d3.select(this).style("fill","#838690"); }
+    })
+  .append("text")         // append text
+    .style("fill", "black")   // fill the text with the colour black
+    .attr("dy", ".35em")           // set offset y position
+    .attr("text-anchor", "middle") // set anchor y justification
+    .attr("transform", "translate(200,100) scale(2) rotate(10)")
+    .text("Hello World");
