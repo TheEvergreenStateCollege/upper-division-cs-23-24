8500d58330dd6a577f333516f67370934bc52453
Dx Youngblood <damiana.x.youngblood@gmail.com>
2023-11-09 19:59:38 +0000

    Fix dynamic programming app

--- dsa-23au/java-dsa/dynamic-programming/src/main/java/dev/codewithfriends/App.java
+++ dsa-23au/java-dsa/dynamic-programming/src/main/java/dev/codewithfriends/App.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.LinkedList;
 
 /**
  * Hello world!
@@ -18,12 +19,24 @@ public class App
      * @param amount
      * @return The number of ways to make change for the given amount
      */
-    public static int coinChangeRecursive(List<Integer> denoms, int amount, Map<Integer,Integer> memo) {
+
+    public static class Pair {
+        int coinsUsed;
+        int amountRemaining;
+
+        public Pair(int coinsUsed, int amountRemaining){
+            //constructor for pair object
+            this.coinsUsed = coinsUsed;
+            this.amountRemaining = amountRemaining;
+        }
+    }
+
+    public static int coinChangeRecursive(List<Integer> denoms, int amount, Map<Pair,Integer> memo) {
         // Base case
+        Pair key = new Pair(denoms.size(), amount);
 
         if (amount < 0 || denoms.size() == 0) {
-            // If it's impossible to make the change
-            // because we have no denominations or it's negative
+            // If it's impossible to make the change because we have no denominations or it's negative
             return 0;
         }
         if (amount == 0) {
@@ -37,13 +50,20 @@ public class App
 
 
         // Recursive case
-        int includeCoin = coinChangeRecursive(denoms, amount - denoms.get(0), memo);
+        int includeCoin = 1 + coinChangeRecursive(denoms, amount - denoms.get(0), memo);
+                // call to recursive function, change argument from amount to amount minus value at index 0
+                
         List<Integer> denomsExclude = Collections.unmodifiableList(denoms.subList(1, denoms.size()));
+
         int excludeCoin = coinChangeRecursive(denomsExclude, amount, memo);
 
-        memo.put(amount, includeCoin + excludeCoin);
+        int newWays = Math.min(includeCoin, excludeCoin);
 
-        return includeCoin + excludeCoin;
+
+
+        memo.put(key, newWays);
+
+        return newWays;
     }
 
     public static void coinChangeIterative() {
@@ -52,6 +72,15 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<Integer> coins = new LinkedList<Integer>();
+        coins.add(25);
+        coins.add(10);
+        coins.add(5);
+        coins.add(1);
+
+        System.out.println(coins);
+
+
+
     }
 }
