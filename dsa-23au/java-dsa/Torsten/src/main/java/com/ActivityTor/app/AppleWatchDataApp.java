fb01dc0f08bc6737520eb16a9328b25310cef3d0
EvergreenTor s <Torsten.S@evergreen.edu>
2023-11-13 21:18:34 +0000

    importlist

--- dsa-23au/java-dsa/Torsten/src/main/java/com/ActivityTor/app/AppleWatchDataApp.java
+++ dsa-23au/java-dsa/Torsten/src/main/java/com/ActivityTor/app/AppleWatchDataApp.java
@@ -1,9 +1,8 @@
 package com.ActivityTor.app;
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 public class AppleWatchDataApp 
 {
