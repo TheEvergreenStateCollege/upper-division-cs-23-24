7f220d6f65b03a0df398d4e37d80362f3ae12d9a
EvergreenTor s <Torsten.S@evergreen.edu>
2023-11-13 19:16:48 +0000

    updating project

--- dsa-23au/java-dsa/Torsten/src/main/java/com/ActivityTor/app/DataReader.java
+++ dsa-23au/java-dsa/Torsten/src/main/java/com/ActivityTor/app/DataReader.java
@@ -1,6 +1,38 @@
 package com.ActivityTor.app;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DataReader {
-    
+
+public class DataReader 
+{
+    private String filePath;
+
+    public DataReader(String filePath) 
+    {
+        this.filePath = filePath;
+    }
+
+    public List<String[]> readCSVData() 
+    {
+        List<String[]> dataList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) 
+        {
+            String line;
+            while ((line = reader.readLine()) != null) 
+            {
+                String[] values = line.split(",");
+                dataList.add(values);
+            }
+        } catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+
+        return dataList;
+    }
 }
