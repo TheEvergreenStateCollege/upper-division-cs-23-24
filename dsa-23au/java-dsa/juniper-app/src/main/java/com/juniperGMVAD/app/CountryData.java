7b45d8f09dd1948d97af55f60faa98bba76f6936
Jonah Eadie <eadjon01@evergreen.edu>
2023-11-14 22:37:06 +0000

    Implemented Database and MVA in CountryData

--- dsa-23au/java-dsa/juniper-app/src/main/java/com/juniperGMVAD/app/CountryData.java
+++ dsa-23au/java-dsa/juniper-app/src/main/java/com/juniperGMVAD/app/CountryData.java
@@ -1,12 +1,21 @@
 package com.juniperGMVAD.app;
 
 public class CountryData {
-    String name; 
-    double MVA
+    String name;
+    int year;
+    double mva;
+    double gmva;
+    double percentOfGMVA;
 
-    public CountryData(String name) 
+    public CountryData(String name, int year, double mva, double gmva) 
     {
         this.name = name;
+        this.year = year;
+        this.mva = mva;
+        this.gmva = gmva;
+
+        percentOfGMVA = mva / gmva;
     }
+
     
 }
