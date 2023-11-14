12275318eacf8e1b9370ba8fcb254d5e5ff41e13
Lucretian <evan.campbell@evergreen.edu>
2023-11-14 21:48:00 +0000

    ReadEdit

--- dsa-23au/java-dsa/juniper-app/src/main/java/com/juniperGMVAD/app/ReadData.java
+++ dsa-23au/java-dsa/juniper-app/src/main/java/com/juniperGMVAD/app/ReadData.java
@@ -2,45 +2,62 @@ package com.juniperGMVAD.app;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Hello world!
  *
  */
-public class ReadData
+public class ReadData 
 {
+    //taking the data from the CSV file and putting it into a hashmap
+    //Key is the name (+ year?) and the data is that countries MVA
+      //  Map<String, CountryData> countryDataMap = new HashMap<>();
+        
+           try (FileInputStream fis = new FileInputStream("dsa-23au/java-dsa/juniper-app/target/classes/G20-GMVA.csv")) {
+    BufferedInputStream bis = new BufferedInputStream(fis);
+    byte[] buffer = bis.readAllBytes();
+    String inputString = new String(buffer);
+    String[] lines = inputString.split("\n");
 
-    
-    public class CSVReader {
-        //taking the data from the CSV file and putting it into a hashmap
-        //Key is the name (+ year?) and the data is that countries MVA
-            Map<String, CountryData> countryDataMap = new HashMap<>();
-            
-            try (FileInputStream fis = new FileInputStream("G20-GMVA.csv")) {
-                BufferedInputStream bis = new BufferedInputStream(fis);
-                byte[] buffer = bis.readAllBytes();
-                String inputString = new String(buffer);
-                String[] lines = inputString.split("\n");
-    
-                for (String line : lines) {
-                    String[] tokens = line.split(",");
-                    if (tokens.length == 0 || line.length() == 0) {
-                        continue;
-                    }
-    
-                    CountryData countryData = new CountryData(tokens[0], tokens[1], );
-    
-                 
-                    countryDataMap.put(countryData.getName(), countryData);
+    ArrayList<String> tokens = new ArrayList<String>();
+    boolean inQuotes = false;
+
+    //remove the quotes from the numbers in strings
+    //strip out commas, ascii to double
+    for (String line : lines) 
+    {
+        int start = 0;
+        for (int i = 0; i < line.length(); i++)
+        {
+            if (line.charAt(i) == '"')
+            {
+                if (inQuotes == true)
+                {
+                    inQuotes = false;
+                    continue;
                 }
-            } catch (IOException ioe) {
-                System.err.println(ioe.toString());
+                inQuotes = true;
             }
-    
-           
+            if (line.charAt(i) == ',' && inQuotes == false)
+            {
+                tokens.add(line.substring(start, i));
+                start = i + 1;
+            }
 
-            for (Map.Entry<String, CountryData> entry : countryDataMap.entrySet()) {
-                System.out.println("Country: " + entry.getKey() + ", Data: " + entry.getValue());
-            }
+        }
+        for (String token : tokens)
+        {
+            System.out.println(token);
+        }
+
+
     }
-}  
+          
+
+    for (Map.Entry<String, CountryData> entry : countryDataMap.entrySet()) 
+    {
+        System.out.println("Country: " + entry.getKey() + ", Data: " + entry.getValue());
+    } 
+}
+} 
