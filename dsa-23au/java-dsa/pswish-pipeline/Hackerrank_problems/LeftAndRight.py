4ea076dbb196e4956e818568be01d6dfd5509387
Paul Swisher <pswish@gmail.com>
2023-11-27 19:16:44 +0000

    Problem without solution

--- dsa-23au/java-dsa/pswish-pipeline/Hackerrank_problems/LeftAndRight.py
+++ dsa-23au/java-dsa/pswish-pipeline/Hackerrank_problems/LeftAndRight.py
@@ -6,11 +6,3 @@
 # defne an array 
 # call the function = funciton(array[1])
 # print the unpacked results
-
-def left_rotate_array(array, d):
-    return array[d:] + array[:d]
-
-d = [5, 4]
-array = [1, 2, 3, 4, 5]
-result = left_rotate_array(array, d[1])
-print(*result)
