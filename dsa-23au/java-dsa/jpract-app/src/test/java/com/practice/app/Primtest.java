8dc632c920dbf6b759e67be054a317bdbbe82196
Lucretian <evan.campbell@evergreen.edu>
2023-11-27 23:03:43 +0000

    Prim

---
+++ dsa-23au/java-dsa/jpract-app/src/test/java/com/practice/app/Primtest.java
@@ -0,0 +1,17 @@
+package com.practice.app;
+
+public class Primtest{
+    Graph g = new Graph();
+    g.addNode("");
+    g.addNode("");
+    g.addNode("");
+    g.addNode("");
+    g.addNode("");
+    g.addNode("");
+    g.addNode("");
+    g.addNode("");
+  
+
+
+}
+
