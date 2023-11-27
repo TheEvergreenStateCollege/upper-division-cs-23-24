7274f5f38322d78054794130588eebe5c662aab0
EvergreenTor s <Torsten.S@evergreen.edu>
2023-11-27 19:27:50 +0000

    got rid of code

--- dsa-23au/java-dsa/Torsten/src/main/java/SherlockandArray.java
+++ dsa-23au/java-dsa/Torsten/src/main/java/SherlockandArray.java
@@ -1,47 +1,4 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-public class SherlockandArray 
-{
-    
-    /*
-     * Complete the 'balancedSums' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts INTEGER_ARRAY arr as parameter.
-     */
-
-     public static String balancedSums(List<Integer> arr) {
-        // Write your code here
-            int n = arr.size();
-            
-            // Calculate the total sum of the array
-            int totalSum = 0;
-            for (int i = 0; i < n; i++) {
-            totalSum += arr.get(i);
-        }
-            
-            int leftSum = 0;
-            
-            // Iterate through the array and check if there's an element that satisfies the condition
-            for (int i = 0; i < n; i++) {
-                int rightSum = totalSum - leftSum - arr.get(i);
-                if (leftSum == rightSum) {
-                    return "YES";
-                }
-                leftSum += arr.get(i);
-            }
-            
-            return "NO";
-    
-    
-        }
         /*  method balanceSums(arr)
          * 
          * n = length of arr
@@ -72,5 +29,5 @@ public class SherlockandArray
             // If the loop completes without finding a balance point, return "NO"
          * 
          */
- }
+ 
 
