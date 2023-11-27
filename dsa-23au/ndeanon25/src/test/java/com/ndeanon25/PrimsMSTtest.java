c7335bbda6f2f5ef914427813f1ade7628f2f1f9
ndeanon25 <146136622+ndeanon25@users.noreply.github.com>
2023-11-27 22:57:22 +0000

    Worked on the MST

---
+++ dsa-23au/ndeanon25/src/test/java/com/ndeanon25/PrimsMSTtest.java
@@ -0,0 +1,18 @@
+package com.ndeanon25;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class PrimsMSTtest {
+
+    public void testPrimsMST(){
+        Graph g = new Graph();
+        g.addNode("RN");
+        g.addNode("CP");
+        g.addNode("KB");
+        g.addNode("CK");
+        g.addNode("RG");
+        g.addNode("SB");
+        g.addNode("IGA");
+        g.addNode("GK");
+    }
+}
