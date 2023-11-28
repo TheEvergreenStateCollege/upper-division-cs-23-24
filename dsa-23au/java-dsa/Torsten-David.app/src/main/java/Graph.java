b0a6364686e03b38fe08e3f62a61288331358390
EvergreenTor s <Torsten.S@evergreen.edu>
2023-11-28 22:55:07 +0000

    updated graph.java in Torsten

--- dsa-23au/java-dsa/Torsten-David.app/src/main/java/Graph.java
+++ dsa-23au/java-dsa/Torsten-David.app/src/main/java/Graph.java
@@ -1,10 +1,49 @@
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class Graph {
     
-    //Number of vertices
-    private static final int V = 9; 
+    public Map<String, Vertex> verticesMap = new HashMap<>();
 
-    // create findMinKeyVertex() method for finding the vertex v that has minimum key-value and that is not added MST yet  
+    
+    public class Edge{
+        public int weight;
+        public boolean isIncluded;  
+        
+        public Edge(int weight){
+            this.weight = weight;
+            this.isIncluded = false;
+        }
+    }
+
+    public class Vertex{
+        public String label = null;
+        public Map<Vertex, Edge> edges = new HashMap<>();
+        public boolean isVisited = false;
+
+        public Vertex(String label){
+            this.label = label;
+            this.edges = new HashMap<>();
+            this.isVisited = false;
+        }
+    }
+    public void addNode(String label) {
+       // Maps a label to a vertex.
+        verticesMap.put(label, new Vertex(label));
+    }
+
+
+    public void addEdge(String srcNode, String destNode, int weight)
+    {
+      //Create node1 from the parameter source node
+      Vertex node1 = verticesMap.get(srcNode); 
+      Vertex node2 = verticesMap.get(destNode);
+
+      Edge edge = new Edge(weight);
+      
+      node1.edges.put(node2, edge);
+      node2.edges.put(node1, edge);
+
+    }
 
 }
