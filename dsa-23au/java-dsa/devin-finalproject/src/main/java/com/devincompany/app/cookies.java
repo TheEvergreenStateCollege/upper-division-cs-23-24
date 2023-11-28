7ec7d3f38b3db9947c92f13197545c6b8dafe475
de02E <de02@evergreen.edu>
2023-11-28 18:19:53 +0000

    added pseudocode

---
+++ dsa-23au/java-dsa/devin-finalproject/src/main/java/com/devincompany/app/cookies.java
@@ -0,0 +1,17 @@
+/*
+ * Put cookies into priorityQueue
+
+while smallest element is less than k
+	if there is less than two elements
+		return -1
+	take smallest two cookies out of queue
+	combine first cookie with double second cookie
+	add the new cookie to the queue
+	increment operation count
+return operation number
+	
+ */
+
+public class cookies {
+    
+}
