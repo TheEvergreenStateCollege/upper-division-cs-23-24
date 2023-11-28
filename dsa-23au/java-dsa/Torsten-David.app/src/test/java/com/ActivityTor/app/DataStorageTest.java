5ddc6a68b2b601a07762db5c87d1894594e233d0
David Lee Dunston <dd03@evergreen.edu>
2023-11-28 23:26:46 +0000

    new Test

--- dsa-23au/java-dsa/Torsten-David.app/src/test/java/com/ActivityTor/app/DataStorageTest.java
+++ dsa-23au/java-dsa/Torsten-David.app/src/test/java/com/ActivityTor/app/DataStorageTest.java
@@ -1,7 +1,20 @@
 package com.ActivityTor.app;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class DataStorageTest {
     
-
+    @Test
     
+public void testAddStepCount() {
+
+DataStorage dataStorage = new DataStorage();
+dataStorage.addStepCount("2023-10-16", 7402);
+assertEquals(7402, dataStorage.getStepCout("2023-10-16"));
+        // Arrange
+        String date = "2023-11-28";
+        int stepCount = 1000;
+
 }
