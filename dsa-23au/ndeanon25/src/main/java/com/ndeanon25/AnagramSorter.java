2bd645a8cae2ff9084ab286d754861bb7746a12b
ndeanon25 <146136622+ndeanon25@users.noreply.github.com>
2023-11-29 23:46:48 +0000

    hw5

--- dsa-23au/ndeanon25/src/main/java/com/ndeanon25/AnagramSorter.java
+++ dsa-23au/ndeanon25/src/main/java/com/ndeanon25/AnagramSorter.java
@@ -43,3 +43,52 @@ public class AnagramSorter {
 
     
 }
+
+/** Problem 10.10
+ *  Rank from Stream: Imagine you are reading in a stream of integers. Periodically, you wish
+ * to be able to look up the rank of a number x (the number of values less than or equal to x).
+ * Implement the data structures and algorithms to support these operations. That is, implement
+ * the method track ( int x), which is called when each number is generated, and the method
+ * getRankOfNumber( int x), which returns the number of values less than or equal to x (not
+ * including x itself).
+ * 
+ * Method track(x):
+        # Inserts x into the binary search tree and updates the count property
+        root = insert(root, x)
+
+    Method insert(node, x):
+        if node is null:
+            Create a new node with value x and count = 1
+            Return the new node
+
+        if x < node.value:
+            node.left = insert(node.left, x)
+        else if x > node.value:
+            node.right = insert(node.right, x)
+        else:
+            # If x is equal to node.value, increment node.count
+
+        # Update the count property of the current node
+        node.count = 1 + getCount(node.left) + getCount(node.right)
+
+        Return the modified node
+
+        
+ * Method getRankOfNumber(x):
+        # Returns the number of values less than or equal to x
+        return getRank(root, x)
+
+    Method getRank(node, x):
+        if node is null:
+            return 0
+
+        if x < node.value:
+            return getRank(node.left, x)
+        else if x > node.value:
+            return 1 + getCount(node.left) + getRank(node.right, x)
+        else:
+            # If x is equal to node.value, return the rank
+            return getCount(node.left)
+
+ 
+ */
