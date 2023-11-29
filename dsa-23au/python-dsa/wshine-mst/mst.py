043a36d72d2bce0c5a40d9fba167f41b1cc1dc85
WinstonShine <114693487+WinstonShine@users.noreply.github.com>
2023-11-29 09:16:08 -0800

    add version of prims alg using a min heap

--- dsa-23au/python-dsa/wshine-mst/mst.py
+++ dsa-23au/python-dsa/wshine-mst/mst.py
@@ -3,60 +3,113 @@
 # and a list of edges
 # edge (end1, end2, weight)
 import random
+import heapq
+from dataclasses import dataclass, field
 
 
-class Graph:
-    def __init__(self, vertices, edges):
-        self.vertices = vertices
-        self.edges = edges
+@dataclass(order=True)
+class Edge:
+    ends: set = field(compare=False)
+    weight: int
+
+    # edge is 3 item tuple (end1, end2, weight)
+    def __init__(self, edge):
+        self.ends = set(edge[:2])
+        self.weight = edge[2]
 
 
-def prims_alg(graph):
-    mst = Graph([], [])
-    vertex = random.choice(graph.vertices)
-    mst.vertices.append(vertex)
-    incident_edges = [e for e in graph.edges if vertex in e[:2]]
+def prims_alg_heap(vertices, edges):
+    vertex = random.choice(tuple(vertices))
+    visited = set()
+    incident_edges = []
+    mst_edges = []
+
+    while vertices:
+        # set up
+        vertices.discard(vertex)
+        visited.add(vertex)
+
+        connected_edges = filter(lambda x: vertex in x, edges)
+        for e in connected_edges:
+            heapq.heappush(incident_edges, Edge(e))
+        edges.difference_update(connected_edges)
+        # take min, check if min edge crosses boundary
+        min_edge = heapq.heappop(incident_edges)
+        while min_edge.ends.issubset(visited):
+            if not incident_edges:
+                break
+            min_edge = heapq.heappop(incident_edges)
+
+        diff = min_edge.ends.difference(visited)
+        if diff:
+            vertex = diff.pop()
+            mst_edges.append(min_edge)
+
+    return mst_edges
+
+
+def prims_alg(vertices, edges):
+    mst_visited_vertices = set()
+    mst_edges = []
+
+    # inital selection
+    vertex = random.choice(tuple(vertices))
+    incident_edges = [e for e in edges if vertex in e[:2]]
     min_edge = min(incident_edges, key=lambda e: e[2])
-    mst.edges.append(min_edge)
-
-    while len(mst.vertices) < len(graph.vertices):
-        mst.vertices.extend([v for v in min_edge[:2] if v not in mst.vertices])
+    mst_edges.append(min_edge)
+    while vertices:
+        # set add/discard both O(1)?
+        for v in min_edge[:2]:
+            mst_visited_vertices.add(v)
+            vertices.discard(v)
 
         # there is probably a more efficient way to identify incident edges
-        incident_edges = [e for e in graph.edges if (
-            e[0] in mst.vertices) ^ (e[1] in mst.vertices)]
+        incident_edges = [e for e in edges if (
+            e[0] in mst_visited_vertices) ^ (e[1] in mst_visited_vertices)]
         if incident_edges:
             min_edge = min(incident_edges, key=lambda e: e[2])
-            mst.edges.append(min_edge)
+            mst_edges.append(min_edge)
 
-    return mst.edges
+    return mst_edges
 
 
-vertices = ["SB", "IGA", "RG", "KB", "CP", "CK", "M", "GK", "RN"]
-edges = [
-    ("RN", "KB", 24),
-    ("RN", "CP", 22),
-    ("KB", "CP", 4),
-    ("SB", "KB", 8),
-    ("RG", "KB", 7),
-    ("SB", "RG", 10),
-    ("RG", "IGA", 14),
-    ("RG", "CK", 12),
-    ("CK", "KB", 10),
-    ("CK", "CP", 6),
-    ("IGA", "KB", 19),
-    ("IGA", "CP", 20),
-    ("IGA", "CK", 16),
-    ("IGA", "M", 14),
-    ("M", "GK", 12),
-    ("M", "CK", 15),
-    ("GK", "CK", 14),
-    ("GK", "CP", 17)
-]
-g = Graph(vertices, edges)
-original_cost = sum([e[2] for e in g.edges])
-mst = prims_alg(g)
-print("mst_res: {}".format(mst))
-print("mst length: {}".format(len(mst)))
-print("original cost: {}".format(original_cost))
-print("mst cost: {}".format(sum(e[2] for e in mst)))
+if __name__ == "__main__":
+    vertices = {"SB", "IGA", "RG", "KB", "CP", "CK", "M", "GK", "RN"}
+    edges = {
+        ("RN", "KB", 24),
+        ("RN", "CP", 22),
+        ("KB", "CP", 4),
+        ("SB", "KB", 8),
+        ("RG", "KB", 7),
+        ("SB", "RG", 10),
+        ("RG", "IGA", 14),
+        ("RG", "CK", 12),
+        ("CK", "KB", 10),
+        ("CK", "CP", 6),
+        ("IGA", "KB", 19),
+        ("IGA", "CP", 20),
+        ("IGA", "CK", 16),
+        ("IGA", "M", 14),
+        ("M", "GK", 12),
+        ("M", "CK", 15),
+        ("GK", "CK", 14),
+        ("GK", "CP", 17)
+    }
+
+    v = vertices.copy()
+    e = edges.copy()
+    original_cost = sum([edge[2] for edge in e])
+    mst = prims_alg(v, e)
+    print("mst_res: {}".format(mst))
+    print("mst length: {}".format(len(mst)))
+    print("original cost: {}".format(original_cost))
+    print("mst cost: {}".format(sum(edge[2] for edge in mst)))
+
+    v = vertices.copy()
+    e = edges.copy()
+    original_cost = sum([edge[2] for edge in e])
+    mst = prims_alg_heap(v, e)
+    print("mst_res: {}".format(mst))
+    print("mst length: {}".format(len(mst)))
+    print("original cost: {}".format(original_cost))
+    print("mst cost: {}".format(sum(edge.weight for edge in mst)))
