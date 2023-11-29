7b72849e5fd09e145757c85b2b81c0217d5ea06a
Stella Parker <116223316+whereismyprozac@users.noreply.github.com>
2023-11-28 19:59:33 -0800

    Update AppTest.java
    more changes

--- dsa-23au/java-dsa/stella/src/test/java/com/mycompany/app/AppTest.java
+++ dsa-23au/java-dsa/stella/src/test/java/com/mycompany/app/AppTest.java
@@ -25,22 +25,48 @@ public class AppTest
         g.addNode("CK");
         g.addNode("M");
         g.addNode("GK");
-        g.addEdge("RN", "CP");
-        g.addEdge("RN", "KB");
-        g.addEdge("CP", "KB");
-        g.addEdge("CP", "GK");
-        g.addEdge("Cp", "CK");
-        g.addEdge("KB", "SB");
-        g.addEdge("KB", "RG");
-        g.addEdge("KB", "CK");
-        g.addEdge("CK", "GK");
-        g.addEdge("CK", "M");
-        g.addEdge("CK", "IGA");
-        g.addEdge("CK", "RG");
-        g.addEdge("RG", "IGA");
-        g.addEdge("RG", "SB");
-        g.addEdge("M", "GK");
-        g.addEdge("M", "IGA");
+        g.addEdge("RN", "CP", 1);
+        g.addEdge("RN", "KB", 1);
+        g.addEdge("CP", "KB", 1);
+        g.addEdge("CP", "GK", 1);
+        g.addEdge("CP", "CK", 1);
+        g.addEdge("KB", "SB", 1);
+        g.addEdge("KB", "RG", 1);
+        g.addEdge("KB", "CK", 1);
+        g.addEdge("CK", "GK", 1);
+        g.addEdge("CK", "M", 1);
+        g.addEdge("CK", "IGA", 1);
+        g.addEdge("CK", "RG", 1);
+        g.addEdge("RG", "IGA", 1);
+        g.addEdge("RG", "SB", 1);
+        g.addEdge("M", "GK", 1);
+        g.addEdge("M", "IGA", 1);
+
+        Graph mst = g.primsMST();
+       int expectedWeight = calculateExpectedMSTWeight(); 
+
+        int totalWeight = mst.getTotalWeight();
+        assertTrue("Total weight of MST should be " + expectedWeight, totalWeight == expectedWeight);
+
+        assertTrue("MST should include all nodes", mst.getAllNodes().size() == g.getAllNodes().size());
+
         
+        assertTrue("MST should contain edge between RN and CP", mst.hasEdge("RN", "CP"));
+        assertTrue("MST should contain edge between RN and KB", mst.hasEdge("RN", "KB"));
+        assertTrue("MST should contain edge between CP and KB", mst.hasEdge("CP", "KB"));
+        assertTrue("MST should contain edge between CP and GK", mst.hasEdge("CP", "GK"));
+        assertTrue("MST should contain edge between CP and CK", mst.hasEdge("CP", "CK"));
+        assertTrue("MST should contain edge between KB and SB", mst.hasEdge("KB", "SB"));
+        assertTrue("MST should contain edge between KB and CK", mst.hasEdge("KB", "CK"));
+        assertTrue("MST should contain edge between CK and GK", mst.hasEdge("CK", "GK"));
+        
+        
+    }
+
+    private int calculateExpectedMSTWeight() {
+       
+        return 16; 
+    }
+}
     }
 }
