813599690b6efb49e8926e144d437ff000365c83
David Lee Dunston <dd03@evergreen.edu>
2023-11-30 19:46:44 +0000

    Still need to add Nodes method

--- dsa-23au/java-dsa/graphs/src/main/java/dev/codewithfriends/PrimsMST.java
+++ dsa-23au/java-dsa/graphs/src/main/java/dev/codewithfriends/PrimsMST.java
@@ -3,11 +3,11 @@ package dev.codewithfriends;
 import java.util.Map;
 import java.util.Set;
 
-public class PrimsMST<N,E> {
+public class PrimsMST<N extends Comparable<N>, E extends Comparable<E>> {
 
 
-    public static <N,E extends Comparable<E>> Set<Graph.Edge<Integer>> findMinimumSpanningTree(Graph<N,E> graph) {
-        //Set<N> nodes = graph.getNodes();
+    public static <N extends Comparable<N>, E extends Comparable<E>> Set<Graph.Edge<Integer>> findMinimumSpanningTree(Graph<N, E> graph) {
+        Set<N> nodes = graph.getNodes();
         // Put all nodes in an unvisited bag to start
         return null;
     }
