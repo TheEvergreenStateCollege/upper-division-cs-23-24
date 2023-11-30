b4ba872cbcb723b1be0d012e28b07a36b5e8fbff
Jonah Eadie <eadjon01@evergreen.edu>
2023-11-30 23:01:14 +0000

    Continued implementing Bag class and prim's algorithm

--- dsa-23au/java-dsa/jonah-prim/prim/src/main/java/com/jonaheadie/Graph.java
+++ dsa-23au/java-dsa/jonah-prim/prim/src/main/java/com/jonaheadie/Graph.java
@@ -24,8 +24,16 @@ public class Graph {
     }
 
     public void addNode(String name) {
-        Node node = new Node(name);
+        if (!hasNode(name)) {
+            Node node = new Node(name);
         nodes.add(node);
+        }
+    }
+
+    public void addNode(Node node) {
+        if (!nodes.contains(node) && !hasNode(node.getName())) {
+            nodes.add(node);
+        }
     }
 
     public void removeNodeAndEdges(String name) {
@@ -45,6 +53,20 @@ public class Graph {
         }
     }
 
+    public void removeNodeAndEdges(Node node) {
+        if (nodes.contains(node)) {
+            nodes.remove(node);
+            Iterator<Edge> iterator = edges.iterator();
+            while (iterator.hasNext()) {
+                Edge e = iterator.next();
+                if (e.isEdgeTo(node)) {
+                    iterator.remove();
+                }
+            }
+        }
+        System.err.println("ERROR (removeNodeAndEdges)");
+    }
+
     public List<String> getAdjacentNodes(String name) {
         Node node = getNode(name);
         if (node == null) {
@@ -60,7 +82,7 @@ public class Graph {
         return adj;
     }
 
-    private List<Edge> getNodeEdges(String name) {
+    public List<Edge> getNodeEdges(String name) {
         List<Edge> result = new ArrayList<Edge>();
 
         for (Edge e : edges) {
@@ -72,6 +94,23 @@ public class Graph {
         return result;
     }
 
+    public List<Edge> getNodeEdges(Node node) {
+        if (nodes.contains(node)) {
+            List<Edge> result = new ArrayList<Edge>();
+
+            for (Edge e : edges) {
+                if (e.isEdgeTo(node)) {
+                    result.add(e);
+                }
+            }
+
+            return result;
+        }
+    
+        System.err.println("ERROR (getNodeEdges): does not contain node");
+        return null;
+    }
+
     public boolean hasNode(String name) {
         Node node = getNode(name);
 
@@ -80,6 +119,14 @@ public class Graph {
         }
 
         return true;
+    }
+
+    public boolean hasNode(Node node) {
+        if (nodes.contains(node)) {
+            return true;
+        }
+
+        return false;
     }
 
     public void addEdge(String node1, String node2, int weight) {
@@ -94,6 +141,31 @@ public class Graph {
 
         Edge edge = new Edge(origin, dest, weight);
         edges.add(edge);
+    }
+
+    public void addEdge(Edge edge) {
+        if (!edges.contains(edge)) {
+            edges.add(edge);
+            return;
+        }
+
+        System.err.println("ERROR (addEdge): Already contains edge");
+    }
+
+    public void addEdge(Node node1, Node node2, int weight) {
+        //TODO: check to see if edge already exists
+        if (nodes.contains(node1) && nodes.contains(node2)) {
+            if(hasEdge(node1, node2)) {
+                System.err.println("Edge already exists between " + node1 + " and " + node2);
+                return;
+            }
+    
+            Edge edge = new Edge(node1, node2, weight);
+            edges.add(edge);
+            return;
+        }
+        
+        System.err.println("ERROR (addEdge): Graph does not contain given nodes");
     }
 
     public void removeEdge(String node1, String node2) {
@@ -113,11 +185,49 @@ public class Graph {
         }
     }
 
+    public void removeEdge(Node node1, Node node2) {
+        if (nodes.contains(node1) && nodes.contains(node2)) {
+            Iterator<Edge> iterator = edges.iterator();
+            while (iterator.hasNext()) {
+                Edge e = iterator.next();
+                if (e.isEdgeTo(node1) && e.isEdgeTo(node2)) {
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
+    public void removeEdge(Edge edge) {
+        if (edges.contains(edge)) {
+            edges.remove(edge);
+        }
+
+        System.err.println("ERROR (removeEdge)");
+    }
+
     public boolean hasEdge(String node1, String node2) {
         for (Edge e : edges) {
             if (e.isEdgeTo(node1) && e.isEdgeTo(node2)) {
                 return true;
             }
+        }
+
+        return false;
+    }
+
+    public boolean hasEdge(Node node1, Node node2) {
+        for (Edge e : edges) {
+            if (e.isEdgeTo(node1) && e.isEdgeTo(node2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean hasEdge(Edge edge) {
+        if (edges.contains(edge)) {
+            return true;
         }
 
         return false;
@@ -133,12 +243,34 @@ public class Graph {
         return -1; //error
     }
 
+    public int getEdgeWeight(Node node1, Node node2) {
+        if (nodes.contains(node1) && nodes.contains(node2)) {
+            for (Edge e : edges) {
+                if (e.isEdgeTo(node1) && e.isEdgeTo(node2)) {
+                    return e.getWeight();
+                }
+            }
+        }
+
+        return -1; //error
+    }
+
     public void setEdgeWeight(String node1, String node2, int weight) {
         for (Edge e : edges) {
             if (e.isEdgeTo(node1) && e.isEdgeTo(node2)) {
                 e.setWeight(weight);
             }
         }
+    }
+
+    public void setEdgeWeight(Node node1, Node node2, int weight) {
+        if (nodes.contains(node1) && nodes.contains(node2)) {
+            Edge edge = new Edge(node1, node2, weight);
+            edges.add(edge);
+            return;
+        }
+
+        System.err.println("ERROR: node is not in graph, cannot add edge");
     }
 
     public Graph cloneGraph() {
@@ -156,10 +288,20 @@ public class Graph {
     }
 
     public List<String> getAllNodesNames() {
-        List<String> result = ArrayList<String>();
+        List<String> result = new ArrayList<String>();
 
         for (Node n : nodes) {
             result.add(n.getName());
+        }
+
+        return result;
+    }
+
+    public List<Node> getAllNodes() {
+        List<Node> result = new ArrayList<Node>();
+
+        for (Node n : nodes) {
+            result.add(n);
         }
 
         return result;
