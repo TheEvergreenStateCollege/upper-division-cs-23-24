2948ff81ff66a40db9dd4a0aca385e6c93f28bcf
WinstonShine <114693487+WinstonShine@users.noreply.github.com>
2023-12-06 19:04:09 -0800

    update 12/6

--- dsa-23au/c++-dsa/WinstonShine-project/vtables.cpp
+++ dsa-23au/c++-dsa/WinstonShine-project/vtables.cpp
@@ -1,7 +1,10 @@
 #include <iomanip>
 #include <iostream>
-// ideally change to grant user a shell
-void win() { std::cout << "congrats you win!\n"; };
+
+void win() {
+    std::cout << "congrats you win!\n"; 
+    exit(0);
+};
 
 class Recorder {
   public:
@@ -13,9 +16,13 @@ class Recorder {
     char *message_;
 };
 
-void Recorder::print_message() { std::cout << message_ << std::endl; };
+void Recorder::print_message() {
+    std::cout << std::endl << "Recorded message is: " << message_ << std::endl;
+};
 
 void display_heap(Recorder *obj, void *message_addr, void *vtable_addr) {
+    std::cout << message_addr << std::endl;
+    std::cout << vtable_addr << std::endl;
     std::string separator = "                -------------------";
     std::cout << std::endl
               << std::right << "  [ address ]    [ heap data ]" << std::endl;
@@ -24,8 +31,19 @@ void display_heap(Recorder *obj, void *message_addr, void *vtable_addr) {
     std::cout << separator << std::endl;
     for (int i = 0; i < 12; i++, p++) {
         std::cout << std::setfill('0') << std::setw(12) << p << " | "
-                  << std::setw(16) << std::hex << *(size_t *)p << " | "<< std::endl;
-        std::cout << separator << std::endl;
+                  << std::setw(16) << std::hex << *(size_t *)p << " | ";
+        if (p == message_addr) {
+            std::cout << "  <--- message: " << obj->message();
+        }
+        if (p == vtable_addr) {
+            std::cout << "vtable | " << *(size_t *)vtable_addr << " -> "
+                      << *(size_t *)*(size_t *)vtable_addr << " |";
+            if((void*)*(size_t*)*(size_t *)vtable_addr == (void*)win){
+                std::cout << " <- address to win";
+            } 
+        }
+
+        std::cout << std::endl << separator << std::endl;
     }
 };
 
@@ -33,15 +51,39 @@ int main() {
     std::setvbuf(stdin, NULL, _IONBF, 0);
     std::setvbuf(stdout, NULL, _IONBF, 0);
 
-    Recorder *recorder = new Recorder(new char[0x20]());
-    void *vtable_addr = (void *)(*(size_t *)recorder);
+    Recorder *recorder = new Recorder(new char[0x18]());
+    void *vtable_addr = (void *)recorder;
     void *message_addr = (void *)recorder->message();
 
-    std::cin >> recorder->message();
-    std::cout << "message address: " << message_addr << std::endl;
-    std::cout << "vtable address: " << vtable_addr << " | " << std::hex
-              << *(size_t *)vtable_addr << " | " << std::hex
-              << *(size_t *)*(size_t *)vtable_addr << std::endl;
-
-    display_heap(recorder, message_addr, vtable_addr);
+    std::cout << "win function address: " << (void*)win << std::endl;
+    int opt;
+    bool exit = false;
+    while (!exit) {
+        std::cout << std::endl
+                  << "1: record message" << std::endl
+                  << "2: print message" << std::endl
+                  << "3: display heap" << std::endl
+                  << "4: exit" << std::endl
+                  << std::endl;
+        std::cin >> opt;
+        switch (opt) {
+        case 1:
+            std::cout << "Enter a mesage: ";
+            std::cin >> recorder->message();
+            break;
+        case 2:
+            recorder->print_message();
+            break;
+        case 3:
+            display_heap(recorder, message_addr, vtable_addr);
+            break;
+        case 4:
+            exit = true;
+            break;
+        default:
+            std::cout << "Invalid option." << std::endl;
+            return 0;
+        }
+    }
+    return 0;
 }
