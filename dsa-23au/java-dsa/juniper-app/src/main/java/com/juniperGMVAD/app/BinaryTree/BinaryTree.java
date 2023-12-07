47686d39da1369ad0565c06ef5681f2309c1de3f
Jonah Eadie <eadjon01@evergreen.edu>
2023-12-07 08:07:03 +0000

    Implemented range search for binary tree

--- dsa-23au/java-dsa/juniper-app/src/main/java/com/juniperGMVAD/app/BinaryTree/BinaryTree.java
+++ dsa-23au/java-dsa/juniper-app/src/main/java/com/juniperGMVAD/app/BinaryTree/BinaryTree.java
@@ -33,10 +33,60 @@ public class BinaryTree<T> {
         return searchRecursive(root, value);
     }
 
+    /**
+     * Sets key in tree equal to value, or inserts it if non-existent. Allows user to replace keys in tree with value
+     * @param value Key to be inserted into tree, or to replace existing key
+     * @return Previous key object of specified key value, null if non-existent
+     */
+    public T set(T value) {
+        return setRecursive(root, value);
+    }
+
     public List<T> asList() {
         List<T> list = new ArrayList<T>();
         asListRecursive(root, list);
         return list;
+    }
+
+    /**
+     * Method which returns an ArrayList of the T values within the tree which are in the range specified by the arguments, inclusive
+     * @param min Minimum value returned
+     * @param max Maximum value returned
+     * @return List<T> of nodes whose values are within range
+     */
+    public List<T> rangeAsList(T min, T max) {
+        List<T> result = new ArrayList<T>();
+        rangeAsListRecursive(root, result, min, max);
+        return result;
+    }
+
+    /**
+     * Helper method for building a list of a subsection of tree elements
+     * @param current
+     * @param list
+     * @param min
+     * @param max
+     */
+    private void rangeAsListRecursive(Node<T> current, List<T> list, T min, T max) {
+        if (current == null) {
+            return;
+        }
+
+        if (comparator.compare(current.value, min) < 0) { // value is less than min
+            rangeAsListRecursive(current.right, list, min, max);
+        } else if (comparator.compare(current.value, min) == 0) { // value equals min
+            list.add(current.value);
+            rangeAsListRecursive(current.right, list, min, max);
+        } else if (comparator.compare(current.value, min) > 0 && comparator.compare(current.value, max) < 0 ) { // value is greater than min and less than max
+            list.add(current.value);
+            rangeAsListRecursive(current.right, list, min, max);
+            rangeAsListRecursive(current.left, list, min, max);
+        } else if (comparator.compare(current.value, max) == 0) { // value equals max
+            list.add(current.value);
+            rangeAsListRecursive(current.left, list, min, max);
+        } else if (comparator.compare(current.value, max) > 0) { // value is greater than max
+            rangeAsListRecursive(current.left, list, min, max);
+        }
     }
 
     public int size() {
@@ -104,6 +154,31 @@ public class BinaryTree<T> {
             return searchRecursive(current.left, value);
         } else {
             return searchRecursive(current.right, value);
+        }
+    }
+
+    /**
+     * 
+     * @param current
+     * @param value
+     * @return Previous value object of specified key value, null if non-existent
+     */
+    private T setRecursive(Node<T> current, T value) {
+        // If value does not exist in tree, insert and return null
+        if (current == null) {
+            insert(value);
+            return null;
+        }
+
+        if (comparator.compare(value, current.value) < 0) { // if value less than current value, call addRecursive on left child
+            return setRecursive(current.left, value);
+        } else if (comparator.compare(value, current.value) > 0) { // if value greater than current value, call addRecursive on right child
+            return setRecursive(current.right, value);
+        } else {
+            // Current is correct value
+            T oldValue = current.value;
+            current.value = value;
+            return oldValue;
         }
     }
 
