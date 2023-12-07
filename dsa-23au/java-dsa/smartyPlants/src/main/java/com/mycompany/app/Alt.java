697e699a839ee28b445f331f08e5802e4879ceba
Dylana Wagorn <146136483+wagorndyl@users.noreply.github.com>
2023-12-07 06:15:04 +0000

    s

---
+++ dsa-23au/java-dsa/smartyPlants/src/main/java/com/mycompany/app/Alt.java
@@ -0,0 +1,52 @@
+package com.mycompany.app;
+
+import org.biojava.nbio.genome.io.fasta.FastaReader;
+import org.biojava.nbio.genome.io.fasta.Fasta;
+
+//create classes for file, reader and entries within file 
+
+//entries within file
+public class runLine {
+    private String firstLine;
+    private String[] metadata;
+    private String sequence; 
+    private String runName; //I don't know how to do this one yet, probably set from entry in metadata[0]
+
+    public runLine(firstLine, sequence){
+        this.firstLine = firstLine;
+        this.sequence = sequence;
+        this.metadata = //regular expressions happen here
+    }
+
+    public String[] getEntry(runName){
+        return this.metadata; //this should actually print each new item in the seperated metdata on its own line
+        //that might use a loop?
+    }
+
+    public String getSequence(runName){
+        return this.sequence;
+    }
+    
+    //make report of all items in file
+    public seeEntries(){
+        System.out.println(runLine.getEntry());
+        System.out.println("mRNA Sequence:");
+        System.out.println(runLine.getSequence());        
+    }
+}
+
+//get the lines and put them in the right place
+public class filesReader(){
+    FastaReader fastaReader = new FastaReader(); //something with the library...
+    List<runLine> entryItems = new ArrayList<>(); //Arraylist must be imported still!
+    //put for-loop reader here 
+    //it is very important that these are constructed correctly in order to keep each line of genetic data matched with its metadata
+    //each pair of two lines, new object instance
+    //store each object in array
+    
+    //it's probably possible to read the file name attatched to each entry read from file itself, maybe more regex extraction 
+
+}
+
+//class for reporting to user probably
+///iterate through array of runLine objects after toArray from list and print getEntry 
