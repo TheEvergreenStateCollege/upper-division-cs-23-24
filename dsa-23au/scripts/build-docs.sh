01bbeefb61df68a712c9f686eb4319a62e6caeb6
Paul Pham <paulpham@yahoo.com>
2023-12-10 18:32:58 -0800

    Build docs now that all dsa-23au scripts are in the same directory.

--- dsa-23au/scripts/build-docs.sh
+++ dsa-23au/scripts/build-docs.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # Copy GitHub Markdown documents into mkdocs site documentation
-cp ../*.md ./docs/ 
-#
+# because symlinks don't work
+# These are ignored from committing into GitHub with ./docs/.gitignore
+cp *.md ./docs/ 
+
 # Build to top-level GitHub Pages source directory 
-mkdocs build -d ../../docs/dsa-23au
+mkdocs build -d ../docs/dsa-23au
 
