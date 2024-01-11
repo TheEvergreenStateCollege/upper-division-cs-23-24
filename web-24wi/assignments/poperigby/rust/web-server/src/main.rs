728e124c40437688a5c46dce70c7e666d76f8797
Pope_Rigby <poperigby@mailbox.org>
2024-01-11 14:08:00 -0800

    Refactor

--- web-24wi/assignments/poperigby/rust/web-server/src/main.rs
+++ web-24wi/assignments/poperigby/rust/web-server/src/main.rs
@@ -18,21 +18,16 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("../hello.html").unwrap();
-        let length = contents.len();
-
-        let response = format!("{status_line}\rContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "../hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("../404.html").unwrap();
-        let length = contents.len();
+        ("HTTP/1.1 404 NOT FOUND", "../404.html")
+    };
 
-        let response = format!("{status_line}\rContent-Length: {length}\r\n\r\n{contents}");
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    let response = format!("{status_line}\rContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
