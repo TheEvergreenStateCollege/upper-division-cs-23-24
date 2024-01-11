dc33bbfcd89f2ba08dc7a19223305c0f0ee0a4c1
Pope_Rigby <poperigby@mailbox.org>
2024-01-11 13:46:45 -0800

    Write a response

--- web-24wi/assignments/poperigby/rust/web-server/src/main.rs
+++ web-24wi/assignments/poperigby/rust/web-server/src/main.rs
@@ -22,5 +22,7 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {:#?}", http_request);
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
