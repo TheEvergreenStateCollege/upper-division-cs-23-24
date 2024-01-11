4f11169461e892b1b0ba04ee2794f47d8f73678e
Pope_Rigby <poperigby@mailbox.org>
2024-01-11 14:04:31 -0800

    Validate the request and selectively respond

--- web-24wi/assignments/poperigby/rust/web-server/src/main.rs
+++ web-24wi/assignments/poperigby/rust/web-server/src/main.rs
@@ -16,18 +16,23 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        // A line with an empty string signals the end of an HTTP request
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("../hello.html").unwrap();
-    let length = contents.len();
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("../hello.html").unwrap();
+        let length = contents.len();
 
-    let response = format!("{status_line}\rContent-Length: {length}\r\n\r\n{contents}");
+        let response = format!("{status_line}\rContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("../404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\rContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
