230eff22a78dc8bd4a8df8eab1bfff6347e6b7e3
Pope_Rigby <poperigby@mailbox.org>
2024-01-11 14:38:34 -0800

    Umm some multithreading stuff

--- web-24wi/assignments/poperigby/rust/web-server/src/main.rs
+++ web-24wi/assignments/poperigby/rust/web-server/src/main.rs
@@ -3,14 +3,15 @@ use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
+use web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        handle_connection(stream);
+        pool.execute(|| handle_connection(stream));
     }
 }
 
