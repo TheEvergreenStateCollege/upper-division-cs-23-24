b74015e122f5cdbf0ede01f7cbb18a039067f7ab
Pope_Rigby <poperigby@mailbox.org>
2024-01-16 14:45:55 -0800

    Wahhhh

--- web-24wi/assignments/poperigby/frontend/week1/src/SearchParams.jsx
+++ web-24wi/assignments/poperigby/frontend/week1/src/SearchParams.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import Pet from "./Pet"
-import useBreedList from "./useBreedList"
+import Results from "./Results";
+import useBreedList from "./useBreedList";
 
 const ANIMALS = ["bird", "cat", "rabbit", "reptile"];
 
@@ -75,11 +75,7 @@ const SearchParams = () => {
                 </label>
                 <button>Submit</button>
             </form>
-            {
-                pets.map((pet) => (
-                    <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
-                ))
-            }
+            <Results pets={pets} />
         </div>
     );
 }
