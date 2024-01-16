9475f2bc2d6aa321a6d0754bf1dffbd943bcd5f3
Pope_Rigby <poperigby@mailbox.org>
2024-01-16 10:41:24 -0800

    Fix

--- web-24wi/assignments/poperigby/frontend/week1/src/Pet.jsx
+++ web-24wi/assignments/poperigby/frontend/week1/src/Pet.jsx
@@ -1,11 +1,11 @@
 const Pet = (props) => {
-    return {
+    return (
         < div >
             <h1>{props.name}</h1>
             <h2>{props.animal}</h2>
             <h3>{props.breed}</h3>
         </div >
-    };
+    );
 };
 
 export default Pet;
