eefa9b30ae36080c3f3e3739517c1dd692b6304e
Pope_Rigby <poperigby@mailbox.org>
2024-01-15 17:12:39 -0800

    Multithreading!

--- web-24wi/assignments/poperigby/rust/web-server/src/lib.rs
+++ web-24wi/assignments/poperigby/rust/web-server/src/lib.rs
@@ -1,8 +1,14 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 pub struct ThreadPool {
-    threads: Vec<Worker>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -15,19 +21,30 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+
+        // Putting the receiver into an Arc<Mutex>> allows all of the Workers to share a handle to
+        // the receiving end of the channel.
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            threads.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { threads }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        // Create a new Job from the closure we receive
+        let job = Box::new(f);
+
+        // Send the job down the channel to be picked up by a worker
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -37,10 +54,22 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         Worker {
             id,
-            thread: thread::spawn(|| {}),
+            thread: thread::spawn(move || loop {
+                //  The Worker's thread loops forever, operating on any Jobs that get sent down the
+                //  channel.
+                let job = receiver
+                    .lock()
+                    .expect("Another Worker thread panicked while holding the lock, instead or releasing it.")
+                    .recv()
+                    .expect("The thread holding the receiver has shut down.");
+
+                println!("Worker {id} got a job; executing.");
+
+                job();
+            }),
         }
     }
 }
