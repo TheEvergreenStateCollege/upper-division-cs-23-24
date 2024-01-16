167c2ff2b8de15ee90d58a620252cec17f5e5cf8
Pope_Rigby <poperigby@mailbox.org>
2024-01-16 11:56:20 -0800

    No work );

--- web-24wi/assignments/poperigby/frontend/week1/src/SearchParams.jsx
+++ web-24wi/assignments/poperigby/frontend/week1/src/SearchParams.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Pet from "./Pet"
 
 const ANIMALS = ["bird", "cat", "rabbit", "reptile"];
 
@@ -6,7 +7,21 @@ const SearchParams = () => {
     const [location, updateLocation] = useState("");
     const [animal, updateAnimal] = useState("");
     const [breed, setBreed] = useState("");
+    const [pets, setPets] = useState("");
     const breeds = [];
+
+    useEffect(() => {
+        requestPets();
+    }, []);
+
+    async function requestPets() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+        );
+        const json = await res.json();
+
+        setPets(json.pets);
+    }
 
     return (
         <div className="search-params">
@@ -59,6 +74,11 @@ const SearchParams = () => {
                 </label>
                 <button>Submit</button>
             </form>
+            {
+                pets.map((pet) => (
+                    <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+                ))
+            }
         </div>
     );
 }
