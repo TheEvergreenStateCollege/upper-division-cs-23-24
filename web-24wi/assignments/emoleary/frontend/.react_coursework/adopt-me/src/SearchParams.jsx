397dca044581f47d1afbad24c644a12b7637322e
Emma O'Leary <oleemm31@evergreen.edu>
2024-01-20 17:45:41 -0800

    Updated code with useEffect hook and custom useBreedList hook to fetch data from available API

--- web-24wi/assignments/emoleary/frontend/.react_coursework/adopt-me/src/SearchParams.jsx
+++ web-24wi/assignments/emoleary/frontend/.react_coursework/adopt-me/src/SearchParams.jsx
@@ -1,18 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Pet from './Pet.jsx'
 
-const ANIMALS = ['rug', 'doggo', 'abolute dog', 'ABSOLUT dog', 'fish or bird', 'not fish', 'not bird', 'other']
-const BREEDS = ["slug"]
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
+const SearchParams = () => {
+  const [pets, setPets] = useState([]);
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const BREEDS = [];
 
-const SearchParams = () => {  
+  useEffect(() => {
+    requestPets();
+  }, []); 
 
-  const [location, setLocation] = useState("Seattle, WA");
-  const[animal, setAnimal] = useState("")  
-  const[breed, setBreed] = useState("")  
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (    
     <div>
-      <form>
+      <form
+        onSubmit={(e) => {
+            e.preventDefault()
+            requestPets()
+          }}
+        >
+          
         <label htmlFor="location">
           Location
           <input 
@@ -45,6 +64,7 @@ const SearchParams = () => {
           Breed
           <select
             id="breed"
+            disabled={BREEDS.length===0}
             value={breed}
             onChange={(e) => setBreed(e.target.value)
           }
@@ -58,6 +78,11 @@ const SearchParams = () => {
           </select>
               </label>
       </form>
+
+      {pets.map((pet) => (
+        <Pet name={pet.name} value={pet.value} key={pet.id}></Pet> 
+      ))
+      }
     </div>
   );
 };
