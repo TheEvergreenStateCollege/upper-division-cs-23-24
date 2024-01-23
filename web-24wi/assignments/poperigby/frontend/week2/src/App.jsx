e85578b09cb2a39e4ceb8b24af59d966a6e6b182
Pope_Rigby <poperigby@mailbox.org>
2024-01-22 20:51:59 -0800

    Wahhh

--- web-24wi/assignments/poperigby/frontend/week2/src/App.jsx
+++ web-24wi/assignments/poperigby/frontend/week2/src/App.jsx
@@ -1,11 +1,18 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
     return (
         < div >
-            <h1> Adopt Me!</h1>
-            <SearchParams />
+            <BrowserRouter>
+                <h1>Adopt me!</h1>
+                <Routes>
+                    <Route path="/details/:id" element={<Details />} />
+                    <Route path="/" element={<SearchParams />} />
+                </Routes>
+            </BrowserRouter>
         </div >
     );
 };
---
+++ web-24wi/assignments/poperigby/frontend/week2/src/Details.jsx
@@ -0,0 +1,5 @@
+const Details = () => {
+    return <h2>hi!</h2>;
+}
+
+export default Details;
