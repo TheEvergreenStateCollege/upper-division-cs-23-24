785a147277bbcd314ddd902b079d0787c209a4dc
Pope_Rigby <poperigby@mailbox.org>
2024-01-23 22:09:24 -0800

    Create setup_vertices

--- web-24wi/assignments/poperigby/rust/week2/wasm/README.md
+++ web-24wi/assignments/poperigby/rust/week2/wasm/README.md
@@ -8,3 +8,4 @@ https://blog.logrocket.com/implement-webassembly-webgl-viewer-using-rust
     * Shaders: Small programs that run on the GPU and can manipulate pixels to create rendering effects
         * Vertex shaders: Manipulate vertices
         * Fragment shaders: Can manipulate colors inside a shape. Trying to lookup what it did beyond that broke my brain.
+    * Vertex buffer: A buffer where vertex data can be uploaded and modified before drawing to the canvas, instead of directly drawing to the canvas and manipulating vertices afterwards.
--- web-24wi/assignments/poperigby/rust/week2/wasm/src/lib.rs
+++ web-24wi/assignments/poperigby/rust/week2/wasm/src/lib.rs
@@ -5,8 +5,7 @@ use web_sys::{HtmlCanvasElement, WebGlProgram, WebGlRenderingContext, WebGlShade
 extern crate js_sys;
 
 pub fn init_webgl_context(canvas_id: &str) -> Result<WebGlRenderingContext, JsValue> {
-    // Create an HTML document context with some very naughty unwraps that will probably
-    // crash our program.
+    // Create an HTML document context
     let document = web_sys::window().unwrap().document().unwrap();
 
     // Create a WebGL canvas
@@ -125,4 +124,47 @@ pub fn setup_shaders(rendering_context: &WebGlRenderingContext) -> Result<WebGlP
                 .unwrap_or_else(|| "Unknown error while linking shader program".to_string()),
         ))
     }
+}
+
+// Setup up the given array of vertices, and sets them up so WebGL can use them
+pub fn setup_vertices(
+    rendering_context: &WebGlRenderingContext,
+    vertices: &[f32],
+    shader_program: &WebGlProgram,
+) {
+    // Create a JS array based on our passed in vertices
+    let vertices = unsafe { js_sys::Float32Array::view(vertices) };
+
+    // Create a vertex buffer
+    let vertex_buffer = rendering_context.create_buffer().unwrap();
+
+    // Bind the vertex buffer to our rendering context at the ARRAY_BUFFER binding port (stores vertex
+    // attributes, such as coordinates)
+    rendering_context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&vertex_buffer));
+    rendering_context.buffer_data_with_array_buffer_view(
+        WebGlRenderingContext::ARRAY_BUFFER,
+        &vertices,
+        WebGlRenderingContext::STATIC_DRAW,
+    );
+
+    // Find the coordinates attribute in our shader program, which tells us how to interpret the
+    // vertex data
+    let coordinates_location = rendering_context.get_attrib_location(shader_program, "coordinates");
+
+    // rendering_context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&vertex_buffer));
+    // Tell WebGL how to read the vertex data from the buffer:
+    //   * Store them in the coordinates array attribute
+    //   * A vertex is made up of three values (x, y, z)
+    //   * Each coordinates is a float
+    //   * They're normalized
+    //   * They're tightly packed
+    rendering_context.vertex_attrib_pointer_with_i32(
+        coordinates_location as u32,
+        3,
+        WebGlRenderingContext::FLOAT,
+        false,
+        0,
+        0,
+    );
+    rendering_context.enable_vertex_attrib_array(coordinates_location as u32);
 }
