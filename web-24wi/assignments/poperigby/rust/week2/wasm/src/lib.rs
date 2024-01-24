fac1ab07601d31c6308d90744d19e756d62cbfbc
Pope_Rigby <poperigby@mailbox.org>
2024-01-23 21:36:46 -0800

    Create setup_shaders

--- web-24wi/assignments/poperigby/rust/week2/wasm/src/lib.rs
+++ web-24wi/assignments/poperigby/rust/week2/wasm/src/lib.rs
@@ -63,3 +63,66 @@ pub fn create_shader(
         ))
     }
 }
+
+/// Creates a WebGL shader program, attaches our vertex and fragment shaders to it, checks for
+/// errors, and then returns it.
+pub fn setup_shaders(rendering_context: &WebGlRenderingContext) -> Result<WebGlProgram, JsValue> {
+    // Create a vertex shader
+    let vertex_shader_source = "
+        attribute vec3 cordinates;
+
+        void main(void) {
+            gl_Position = vec4(coordinates, 1.0);
+        }
+        ";
+    let vertex_shader = create_shader(
+        rendering_context,
+        WebGlRenderingContext::VERTEX_SHADER,
+        vertex_shader_source,
+    )
+    .unwrap();
+
+    // Create a fragment shader
+    let fragment_shader_source = "
+        precision mediump float;
+        uniform vec4 fragColor;
+
+        void main(void) {
+            gl_FragColor = fragColor;
+        }
+        ";
+    let fragment_shader = create_shader(
+        rendering_context,
+        WebGlRenderingContext::FRAGMENT_SHADER,
+        fragment_shader_source,
+    )
+    .unwrap();
+
+    // Create a new WebGL shader program
+    let shader_program = rendering_context.create_program().unwrap();
+
+    // Attach the vertex and fragment shader to our shader program
+    rendering_context.attach_shader(&shader_program, &vertex_shader);
+    rendering_context.attach_shader(&shader_program, &fragment_shader);
+
+    // Link the shader program to the rendering context
+    rendering_context.link_program(&shader_program);
+
+    // If the shader program is able to link to the rendering context, we can set it as
+    // part of the current rendering state and return it.
+    if rendering_context
+        .get_program_parameter(&shader_program, WebGlRenderingContext::LINK_STATUS)
+        .as_bool()
+        .unwrap()
+    {
+        rendering_context.use_program(Some(&shader_program));
+        Ok(shader_program)
+    // Return an error if we're unable to link
+    } else {
+        Err(JsValue::from_str(
+            &rendering_context
+                .get_program_info_log(&shader_program)
+                .unwrap_or_else(|| "Unknown error while linking shader program".to_string()),
+        ))
+    }
+}
