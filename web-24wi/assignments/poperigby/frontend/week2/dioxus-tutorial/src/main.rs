f133fff052dc597f3d02757a4dd9cd57d66e8421
Pope_Rigby <poperigby@mailbox.org>
2024-01-25 19:34:08 -0800

    My first component :)

--- web-24wi/assignments/poperigby/frontend/week2/dioxus-tutorial/src/main.rs
+++ web-24wi/assignments/poperigby/frontend/week2/dioxus-tutorial/src/main.rs
@@ -1,13 +1,29 @@
+#![allow(non_snake_case)]
+
+mod story_listing;
+
 use dioxus::prelude::*;
+use story_listing::{StoryItem, StoryListing};
 
 fn main() {
-    dioxus_web::launch(app);
+    dioxus_web::launch(App);
 }
 
-fn app(context: Scope) -> Element {
-    context.render(rsx! {
-    div {
-        "Hello, world!"
+fn App(cx: Scope) -> Element {
+    render! {
+        StoryListing {
+            story: StoryItem {
+                id: 0,
+                title: "Hello, Hacker News!".to_string(),
+                url: None,
+                text: None,
+                by: "Cassidy".to_string(),
+                score: 0,
+                descendants: 0,
+                time: chrono::Utc::now(),
+                kids: vec![],
+                r#type: "".to_string(),
+            }
+        }
     }
-    })
 }
