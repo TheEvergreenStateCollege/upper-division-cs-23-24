9cbec1223586e8b0e3e5d63ae900e23b6385c939
Emma O'Leary <oleemm31@evergreen.edu>
2024-01-27 19:11:01 -0800

    Implemented an uncrontrolled form which handles user's mouse click interaction with pet photos, indicating caraousel component's active photo state.

--- web-24wi/assignments/emoleary/frontend/week3/10-12/src/Carousel.jsx
+++ web-24wi/assignments/emoleary/frontend/week3/10-12/src/Carousel.jsx
@@ -9,6 +9,14 @@ class Carousel extends Component {
         images: ["http://pets-images.dev-apis.com/pets/none.jpg"]
     }
 
+    // = () => ... : Arrow Function/Event Listener doesn't create new scope, instead its scope is Carousel
+    // () { ...         : New Function : (this keyword is undefined)
+    handleIndexClick = (event) => {
+        this.setState({
+          active: +event.target.dataset.index,
+        });
+      };
+
     render () {
         const { active} = this.state
         const { images } = this.props
@@ -16,13 +24,16 @@ class Carousel extends Component {
         return (
             <div className='carousel'>
                 <img src={images[active]} alt="animal hero" />
+                <p>ACTIVE: {active}</p>
                 <div className='carousel-smaller'>
                     {images.map((photo, index) => (
                         <img
-                            key={photo}
+                            onClick={this.handleIndexClick}
+                            key={index}
                             src={photo}
                             className={index === active ? 'active' : ''}
                             alt="animal thumbnail"
+                            data-index={index}
                         />
                     )
                 )}
