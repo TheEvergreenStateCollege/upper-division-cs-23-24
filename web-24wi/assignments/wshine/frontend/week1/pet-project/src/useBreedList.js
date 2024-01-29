ec0dbce658c43dafe527eda3f2f0dc86d233f961
WinstonShine <114693487+WinstonShine@users.noreply.github.com>
2024-01-29 14:31:34 -0800

    week 3 react started, halfway done with section 5

--- web-24wi/assignments/wshine/frontend/week1/pet-project/src/useBreedList.js
+++ web-24wi/assignments/wshine/frontend/week1/pet-project/src/useBreedList.js
@@ -1,32 +1,8 @@
-import { useState, useEffect } from "react";
-
-const localCache = {};
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
 
 export default function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
 
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-    } else {
-      requestBreedList();
-    }
-
-    async function requestBreedList() {
-      setBreedList([]);
-      setStatus("loading");
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-      const json = await res.json();
-      localCache[animal] = json.breeds || [];
-      setBreedList(localCache[animal]);
-      setStatus("loaded");
-    }
-  }, [animal]);
-
-  return [breedList, status];
+    const results = useQuery(["breeds", animal], fetchBreedList);
+    return [results?.data?.breeds ?? [], results.status];
 }
