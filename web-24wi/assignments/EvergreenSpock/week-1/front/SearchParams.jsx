6d8f3d0dccfe2844ebab410e4748bf0bf672ccbc
EvergreenSpock <114445048+EvergreenSpock@users.noreply.github.com>
2024-01-29 11:39:23 -0800

    added app.jsx

--- web-24wi/assignments/EvergreenSpock/week-1/front/SearchParams.jsx
+++ web-24wi/assignments/EvergreenSpock/week-1/front/SearchParams.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = ["Poodle"];
+
   return (
     <div className="search-params">
       <form>
@@ -14,6 +19,39 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={breeds.length === 0}
+            value={breed}
+            onChange={e => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breeds) => (
+              <option key={breeds}>{breeds}</option>
+            ))}
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
     </div>
