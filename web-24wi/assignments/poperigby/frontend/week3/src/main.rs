5a393bcfafce48ea2f88f5967c65fc501d2b84cd
Pope_Rigby <poperigby@mailbox.org>
2024-01-30 10:54:46 -0800

    Logging

--- web-24wi/assignments/poperigby/frontend/week3/src/main.rs
+++ web-24wi/assignments/poperigby/frontend/week3/src/main.rs
@@ -1,5 +1,23 @@
+#![allow(non_snake_case)]
+
 use dioxus::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    dioxus_logger::init(log::LevelFilter::Info).unwrap();
+    dioxus_web::launch(App);
+}
+
+fn App(cx: Scope) -> Element {
+    // Setup a state variable with a default value
+    let name = use_state(cx, || "John Doe".to_string());
+
+    cx.render(rsx! {
+        input {
+            value: "{name}",
+        },
+        button {
+            onclick: |event| log::info!("Clicked! {event:?}"),
+            "Click me ( ͡° ͜ʖ ͡°)"
+        }
+    })
 }
