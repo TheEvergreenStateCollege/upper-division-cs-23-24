513170be13f009439c34ad7caa664bcb0763461d
Pope_Rigby <poperigby@mailbox.org>
2024-01-30 11:36:06 -0800

    Some more components

--- web-24wi/assignments/poperigby/frontend/week3/src/main.rs
+++ web-24wi/assignments/poperigby/frontend/week3/src/main.rs
@@ -8,16 +8,52 @@ fn main() {
 }
 
 fn App(cx: Scope) -> Element {
-    // Setup a state variable with a default value
-    let name = use_state(cx, || "John Doe".to_string());
+    cx.render(rsx! {
+        h1 { "Adopt me!" },
+        SearchParams {},
+    })
+}
+
+#[derive(PartialEq, Props)]
+struct PetProps<'a> {
+    // The 'a specifies that every str will live as long as the PetProps struct
+    name: &'a str,
+    animal: &'a str,
+    breed: &'a str,
+}
+
+#[component]
+fn Pet<'a>(cx: Scope<'a, PetProps<'a>>) -> Element {
+    cx.render(rsx! {
+        div {
+            h1 { "{cx.props.name}" }
+            h2 { "{cx.props.animal}" }
+            h2 { "{cx.props.breed}" }
+        }
+    })
+}
+
+#[component]
+fn SearchParams(cx: Scope) -> Element {
+    // Setup location state with a default value of an empty string
+    let location = use_state(cx, || "".to_string());
 
     cx.render(rsx! {
-        input {
-            value: "{name}",
-        },
-        button {
-            onclick: |event| log::info!("Clicked! {event:?}"),
-            "Click me ( ͡° ͜ʖ ͡°)"
+        div {
+            class: "search-params",
+            form {
+                label {
+                    r#for: "location",
+                    "Location ",
+                    input {
+                        id: "location",
+                        value: "{location}",
+                        placeholder: "Location",
+                        onchange: move |event| location.set(event.value.clone())
+                    },
+                },
+                button { "Submit" },
+            }
         }
     })
 }
