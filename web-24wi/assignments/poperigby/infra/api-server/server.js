9a3a463b9e755771123e9f11fab32b78af3eebd5
Pope_Rigby <poperigby@mailbox.org>
2024-02-01 13:23:05 -0800

    Fix index.js

--- web-24wi/assignments/poperigby/infra/api-server/server.js
+++ web-24wi/assignments/poperigby/infra/api-server/server.js
@@ -13,6 +13,10 @@ app.get("/", (_req, res) => {
     // Send index.html when browser requests /
     res.sendFile(path.resolve("www/index.html"));
 });
+app.get("/index.js", (_req, res) => {
+    // Send index.html when browser requests /
+    res.sendFile(path.resolve("www/index.js"));
+});
 
 app.get("/search-hit/:hit", (req, res) => {
     res.sendFile(path.resolve(`www/search-${req.params.hit}.html`));
