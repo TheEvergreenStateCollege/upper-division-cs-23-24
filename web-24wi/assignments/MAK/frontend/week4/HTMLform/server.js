ba0bf7df7df2c3c14ce5f3abac73006af6e23bd3
mk-code <114369920+MKrause-code@users.noreply.github.com>
2024-02-01 13:59:06 -0800

    Add files via upload

---
+++ web-24wi/assignments/MAK/frontend/week4/HTMLform/server.js
@@ -0,0 +1,19 @@
+const button = document.getElementById("submit");
+const username = document.getElementById("input-username");
+const password = document.getElementById("input-password");
+
+button.addEventListener("click", async (event) => {
+  event.preventDefauly();
+  const response = await fetch("http://localhost:5000",{
+    "method": "post",
+    "body": {username, password}
+  });
+  console.log("I've been clicked.")
+});
+
+app.post("/login", (req, res) => {
+  //semding back an HTML file that a browser can render on the screen.
+  console.log(` ${req.body}`);
+  const bodyJSON = JSON.parse(req.body);
+  res.json(bodyJSON);
+})
