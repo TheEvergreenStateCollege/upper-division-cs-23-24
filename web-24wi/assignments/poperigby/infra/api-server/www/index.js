a859033e2ad8c629e6b09cc4fc4c1e13a67ec6dd
Pope_Rigby <poperigby@mailbox.org>
2024-02-01 13:19:50 -0800

    Add index.js

---
+++ web-24wi/assignments/poperigby/infra/api-server/www/index.js
@@ -0,0 +1,10 @@
+const name_input = document.getElementById("input-name");
+const password_input = document.getElementById("input-password");
+const submit_button = document.getElementById("button-submit");
+
+submit_button.addEventListener("click", hitIt);
+
+function hitIt() {
+    console.log("HIT!");
+}
+
