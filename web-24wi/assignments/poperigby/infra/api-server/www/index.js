c40e116bbdfb1a1a8a992c9bd8250c69b9d06dd1
Pope_Rigby <poperigby@mailbox.org>
2024-02-01 13:48:35 -0800

    Update index.js

--- web-24wi/assignments/poperigby/infra/api-server/www/index.js
+++ web-24wi/assignments/poperigby/infra/api-server/www/index.js
@@ -2,9 +2,8 @@ const name_input = document.getElementById("input-name");
 const password_input = document.getElementById("input-password");
 const submit_button = document.getElementById("button-submit");
 
-submit_button.addEventListener("click", hitIt);
-
-function hitIt() {
-    console.log("HIT!");
-}
+submit_button.addEventListener("click", async (event) => {
+    const response = await fetch("http://cassidy.arcology.builders:5000", { "method": "post", "body": { username, password } });
+    console.log("I've been clicked 😳")
+});
 
