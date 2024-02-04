b0b9e079e984165357b580afc4832c1f52c69185
EvergreenSpock <114445048+EvergreenSpock@users.noreply.github.com>
2024-02-04 12:39:18 -0800

    pushed week 1 hw for React front end

---
+++ web-24wi/assignments/EvergreenSpock/week-1/front/useBreedList.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+const localCache = {};
+
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  UseEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+  })
+}
