18dda802c47c47246b0fdb6356b304351bd670f1
EvergreenSpock <114445048+EvergreenSpock@users.noreply.github.com>
2024-02-04 12:39:18 -0800

    pushed week 1 hw for React front end

--- web-24wi/assignments/EvergreenSpock/week-1/front/SearchParams.jsx
+++ web-24wi/assignments/EvergreenSpock/week-1/front/SearchParams.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
   const breeds = ["Poodle"];
+
+  useEffect(() => {
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
+
+
+
 
   return (
     <div className="search-params">
@@ -54,6 +72,11 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
+      {
+        pets.map((pet) => (
+          <Pet name=(pet.name) animal = { pet.animal } breed = { pet.breed } key = { pet.id } />
+      ))
+      }
     </div>
   );
 };
