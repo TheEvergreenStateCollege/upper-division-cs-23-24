5872c95927f11b05a9ed6533d37d1b494072fcfb
Ubuntu <ubuntu@ip-172-31-1-136.us-west-1.compute.internal>
2024-02-04 20:20:37 +0000

    committing recent changes

--- web-24wi/projects/pswish/react_tutorial/src/Pet.jsx
+++ web-24wi/projects/pswish/react_tutorial/src/Pet.jsx
@@ -7,11 +7,11 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
   }
 
   return (
-    <Link to={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="relative block">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
-      <div className="info">
+      <div className="absolute bottom-0 left-0 bg-gradient-to-tr from-white to-transparant pr-2 pt-2">
         <h1>{name}</h1>
         <h2>
           {animal} - {breed} - {location}
