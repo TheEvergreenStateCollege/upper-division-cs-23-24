8ae0603a97983c30f7fc03bcf3399b51e07cf373
Pope_Rigby <poperigby@mailbox.org>
2024-02-05 17:58:39 -0800

    Fix some stuff

--- web-24wi/assignments/poperigby/frontend/pet-adoption/src/results.rs
+++ web-24wi/assignments/poperigby/frontend/pet-adoption/src/results.rs
@@ -8,8 +8,21 @@ pub struct ResultsProps<'a> {
 
 pub fn Results<'a>(cx: Scope<'a, ResultsProps<'a>>) -> Element {
     cx.render(rsx! {
-        for pet in cx.props.pets {
-            Pet { name: &pet.name, animal: &pet.animal, breed: &pet.breed  }
+        div {
+            class: "search",
+            if cx.props.pets.is_empty() {
+                rsx! {
+                    h1 {
+                        "No pets found ;("
+                    }
+                }
+            } else {
+                rsx! {
+                    for pet in cx.props.pets {
+                        Pet { name: &pet.name, animal: &pet.animal, breed: &pet.breed  }
+                    }
+                }
+            }
         }
     })
 }
