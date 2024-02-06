02dac702ec3145509a3b0cbf850f787598f67a04
rilesbe <wrilesb@gmail.com>
2024-02-06 00:28:25 +0000

    Changed API server directory, did in class exercise

--- web-24wi/assignments/rilesbe/infra/api-server/server.js
+++ web-24wi/assignments/rilesbe/infra/api-server/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const path = require("path");
+const { PrismaClient } = require('@prisma/client');
+const { parsed } = require('dotenv').config();
+
+console.log(parsed['DATABASE_URL']);
+console.log(process.env['DATABASE_URL']);
+const prisma = new PrismaClient();
 
 app.use(express.static("static"));
 
@@ -16,6 +22,22 @@ app.get("/", (req, res) => {
 app.get("/search-hit/:hit", (req, res) => {
   // sending back an HTML file that a browser can render on the screen.
   res.sendFile(path.resolve(`pages/search-hit-${req.params.hit}.html`));
+});
+
+
+app.get("/users", async (req, res) => {
+	const allUsers = await prisma.user.findMany();
+	res.json(allUsers);
+});
+
+app.post("/user", async (req, res) => {
+	const newUser = await prisma.user.create({
+		data: {
+			username: req.body.username,
+			password: '',
+		},
+	});
+	console.log("created");
 });
 
 
