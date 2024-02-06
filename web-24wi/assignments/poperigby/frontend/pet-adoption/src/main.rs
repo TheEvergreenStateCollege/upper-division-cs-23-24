d5a6a4fcc03b5a98cd0d03facefac8bdc779dab7
Pope_Rigby <poperigby@mailbox.org>
2024-02-05 18:50:29 -0800

    dixous_router

--- web-24wi/assignments/poperigby/frontend/pet-adoption/src/main.rs
+++ web-24wi/assignments/poperigby/frontend/pet-adoption/src/main.rs
@@ -1,11 +1,14 @@
 #![allow(non_snake_case)]
 
 mod breed;
+mod details;
 mod pet;
 mod results;
 mod search;
 
+use details::Details;
 use dioxus::prelude::*;
+use dioxus_router::prelude::*;
 use search::SearchParams;
 
 fn main() {
@@ -13,9 +16,17 @@ fn main() {
     dioxus_web::launch(App);
 }
 
+#[derive(Routable, PartialEq, Debug, Clone)]
+enum Route {
+    #[route("/")]
+    SearchParams {},
+    #[route("/details:id")]
+    Details { id: i64 },
+}
+
 fn App(cx: Scope) -> Element {
     cx.render(rsx! {
         h1 { "Adopt me!" },
-        SearchParams {},
+        Router::<Route> { }
     })
 }
