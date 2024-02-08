dd54a1533e1f9f05bbb6d14287924c63e946a787
cassidy <cassidy.wilson@evergreen.edu>
2024-02-08 21:38:31 +0000

    Cool stuff

--- web-24wi/assignments/poperigby/infra/api-server/server.js
+++ web-24wi/assignments/poperigby/infra/api-server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const PrismaClient = require("@prisma/client");
+const parsed = require("dotenv");
 
 const app = express();
 const port = 80;
