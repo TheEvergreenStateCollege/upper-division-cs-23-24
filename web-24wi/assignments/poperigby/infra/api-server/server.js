7c9cc72b44704b14a6dc9b0c387c4be2cd68f3be
cassidy <cassidy.wilson@evergreen.edu>
2024-02-12 19:55:35 +0000

    Don't crash if we try to create a user with an existing username :|

--- web-24wi/assignments/poperigby/infra/api-server/server.js
+++ web-24wi/assignments/poperigby/infra/api-server/server.js
@@ -11,12 +11,21 @@ const port = 5000;
 app.use(express.json());
 app.use(express.static("static"));
 
+// Create and start a server for our API on a defined port
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// ROUTES
+
 //console.log(parsed["DATABASE_URL"]);
 //console.log(process.env["DATABASE_URL"]);
 
 /**
  * app.[method]([route], [route handler])
  */
+
+// Main page
 app.get("/", (_req, res) => {
     // Send index.html when browser requests /
     res.sendFile(path.resolve("www/index.html"));
@@ -26,34 +35,29 @@ app.get("/index.js", (_req, res) => {
     res.sendFile(path.resolve("www/index.js"));
 });
 
+// Search hits
 app.get("/search-hit/:hit", (req, res) => {
     res.sendFile(path.resolve(`www/search-${req.params.hit}.html`));
 });
 
-app.post("/login", (req, res) => {
-    console.log(`${JSON.stringify(req.body)}`);
-    res.json(req.body);
-})
-
-// Create and start a server for our API on a defined port
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
-
-// PostgreSQL
+// API
 app.get("/api/users", async (req, res) => {
     const allUsers = await prisma.user.findMany();
     res.json(allUsers);
 });
 
 app.post("/api/user", async (req, res) => {
-    const newUser = await prisma.user.create({
-        data: {
-            username: req.body.username,
-	    password: '',
-	},
-    })
+    try {
+        const newUser = await prisma.user.create({
+            data: {
+                username: req.body.username,
+                password: '',
+            },
+        })
 
-    console.log("Created a user!")
+        console.log("Created a user!")
+    } catch (error) {
+	console.log(error);
+    }
 });
 
