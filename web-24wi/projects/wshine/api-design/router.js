672b8e6770e78f189894d6bd1e511f3222f854a1
WinstonShine <114693487+WinstonShine@users.noreply.github.com>
2024-02-13 11:47:38 -0800

    update 2-13

--- web-24wi/projects/wshine/api-design/router.js
+++ web-24wi/projects/wshine/api-design/router.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
 
-router.get("/user", (req, res) => {
-    res.json({ message: "product" });
+router.get("/users", async (req, res) => {
+    const allUsers = await prisma.users.findMany();
+    res.json(allUsers);
 });
 
 router.get("/user/:id", (req, res) => {
@@ -12,9 +14,16 @@ router.get("/user/:id", (req, res) => {
     console.log("GET");
 });
 
-router.post("/user", (req, res) => {
+router.post("/user", async (req, res) => {
     console.log(req.body);
     console.log("POST");
+
+    const newUser = await prisma.users.create({
+        data: {
+            username:req.body.username,
+            password: '',
+        }
+    })
 });
 
 router.put("/user/:id", (req, res) => {
