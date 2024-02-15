5c69305ef91753151c51e6847d8e4eb8968e5d43
WinstonShine <114693487+WinstonShine@users.noreply.github.com>
2024-02-15 12:47:19 -0800

    Wshine week6 (#982)
    * week 3 react started, halfway done with section 5
    
    * 2/1 inclass activity init
    
    * rename/move files
    
    * edit express server/api
    
    * copy pet project to week3 dir
    
    * finish section 5 of react reading
    
    * finish react reading section 6
    
    * finish tailwindcss reading
    
    * start adding more routes to api example
    
    * add schema mock up
    
    * fix example site, start movign site to project dir
    
    * 2/13 clean up
    
    * update 2-13
    
    * update server.js to enable db queries
    
    * cleanup
    
    * fix merge errors
    
    * update .gitignore

---
+++ web-24wi/projects/wshine/api-design/prisma/migrations/20240213212259_init/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "user";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
