4c0d0b94be44a308967ee66385886a546467b897
Ubuntu <ubuntu@ip-172-31-5-244.us-east-2.compute.internal>
2024-02-16 01:20:08 +0000

    updated node server

--- web-24wi/assignments/Gavin-Bowers/backend/api-server/server.js
+++ web-24wi/assignments/Gavin-Bowers/backend/api-server/server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 const port = 5000;
@@ -7,12 +6,18 @@ const path = require("path");
 app.use(express.static("static"));
 app.use(express.json());
 
+const http = require('http');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+
 const { PrismaClient } = require('@prisma/client');
 const { parsed } = require('dotenv').config();
 
 console.log(parsed['DATABASE_URL']);
 console.log(process.env['DATABASE_URL']);
 const prisma = new PrismaClient();
+
+/* API SERVER - Handles authentication and will possible handle other stuff*/
 
 app.get("/users", async (req, res) => {
 	const allUsers = await prisma.user.findMany();
@@ -29,6 +34,23 @@ app.post("/user", async (req, res) => {
 	console.log("created new user");
 });
 
+/* Static Website - Serves a static website, which will use client-side JS to interface with the API and the music server*/
+
+app.get("/", function(req, res) {
+        res.sendFile("/home/ubuntu/src/upper-division-cs/web-24wi/assignments/Gavin-Bowers/backend/api-server/index.html");
+}); /*you could replace this with a relative path using a function*/
+
+
+/*Music Server - Streams music files*/
+
+const musicServer = http.createServer((req, res) => {
+	//handle requests here
+});
+
+musicServer.listen(3000, () => {
+	console.log("Music server running on port 3000");
+});
+
 app.listen(port, () => {
-	console.log("example app listening at port ?");
+	console.log("Web app listening at port 5000");
 });
