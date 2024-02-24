3253d47c8839f468e8b43ef2a39f6d56431f4864
pswish <pswish@gmail.com>
2024-02-24 01:19:52 +0000

    adding some helper scripts

--- web-24wi/projects/pswish/restart-flask.sh
+++ web-24wi/projects/pswish/restart-flask.sh
@@ -1,16 +1,68 @@
 #!/bin/bash
 
+# ANSI color codes for text decoration
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 # Run netstat, filter by port 5000, and extract the process ID
 process_id=$(netstat -nlp | grep 5000 | awk '{split($7, a, "/"); print a[1]}')
 
+# Function to restart the system process
+restart_system_process() {
+    echo ""
+    echo "Restarting system process..."
+    sudo systemctl restart flaskProd.service
+    sleep 2
+    sudo systemctl status flaskProd.service
+}
+
 # Check if the process ID is not empty
 if [ -n "$process_id" ]; then
-    # If process 5000 is running
+    # If process 5000 is running, stops it if it is
     sudo systemctl stop flaskProd.service
+    echo -e "${RED}Stopping running flask service${NC}"
 else
-    echo "No process found for port 5000, starting server interactively"
+    echo "No process found for port 5000"
 fi
 
-sleep 2
-cd /home/ubuntu/Workspace/upper-division-cs/web-24wi/projects/pswish/prod/driver_flask/
-python3 app.py
+# Prompt the user to choose an option to begin the interaction
+echo "Press 1 to restart the system process."
+echo "Press 2 to start the server interactively."
+echo "Press 3 to exit."
+
+# Read user input
+read -n 1 option
+echo ""
+
+# Process user input
+case $option in
+    1)
+        restart_system_process
+        ;;
+    2)
+        echo "Starting server interactively..."
+        cd /home/ubuntu/Workspace/upper-division-cs/web-24wi/projects/pswish/prod/driver_flask/
+        python3 app.py
+
+        # Interactive terminal option
+	# Delayed function to prompt the user to restart the service when interactive server is killed
+        
+	sleep 1
+        echo -e " ...${BLUE}Do you want to restart the system process? (y/n)${NC}"
+        echo ""
+        read -n 1 restart_option
+
+        if [ "$restart_option" == "y" ]; then
+            restart_system_process
+        fi
+        ;;
+    3)
+        echo "Exiting."
+        exit 0
+        ;;
+    *)
+        echo "Invalid option. Exiting."
+        exit 1
+        ;;
+esac
---
+++ web-24wi/scripts/create_branch_and_push.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Prompt the user for a new branch name
+read -p "Enter a name for your new branch: " branch_name
+
+# Create the new branch
+git checkout -b "$branch_name"
+
+# Ask user if ready to push
+read -p "Are you ready to push changes to main branch? (y/n): " ready_to_push
+
+if [[ $ready_to_push == "y" ]]; then
+    # Push changes to main branch
+    git push -u origin main
+elif [[ $ready_to_push == "n" ]]; then
+    # Display git status
+    git status
+else
+    echo "Invalid input. Exiting."
+    exit 1
+fi
+
