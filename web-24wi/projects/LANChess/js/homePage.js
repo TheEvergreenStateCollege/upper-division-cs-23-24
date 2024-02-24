93db0e4cfdc76428862f70c7745d483c3a2d4fd8
tw-edu <156841820+tw-edu@users.noreply.github.com>
2024-02-24 03:50:58 +0000

    homePage updates

--- web-24wi/projects/LANChess/js/homePage.js
+++ web-24wi/projects/LANChess/js/homePage.js
@@ -2,11 +2,20 @@
 const baseURL = 'http://localhost:5000'
 var initOpponentUsername = '';
 
+function createNewGame(){
+    //Will make POST to server to generate a new game. 
+}
+
 function initOpponentUsername(){
     initOpponentUsername = document.getElementById('findOpponentByUsernameForm').value;
 }
 
-
+function checkIfOpponentCanBeFound() {
+    //Will query database to see if there is an active game created by an opponent user with that name. If True the player will join that game,
+    //if false then it will let the user know that it either
+    //1. The user cannot be found, prompt user to try name again.
+    //2. If the user is found, then an active game with that user cannot be found, tell opponent to create a game, or return to the menu and create your own.
+}
 
 
 
