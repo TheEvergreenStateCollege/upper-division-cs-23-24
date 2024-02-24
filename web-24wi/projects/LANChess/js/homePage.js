9c5162499b8cfddeeb5e12a8349dfa3069873f0b
tw-edu <156841820+tw-edu@users.noreply.github.com>
2024-02-24 03:38:05 +0000

    homePage Updates

--- web-24wi/projects/LANChess/js/homePage.js
+++ web-24wi/projects/LANChess/js/homePage.js
@@ -1,17 +1,14 @@
 //homePage.js
 const baseURL = 'http://localhost:5000'
-var opponentUsername = '';
+var initOpponentUsername = '';
 
-
-function storeFormValueIntoOpponentUsername(callback) {
-    opponentUsername = document.getElementById('textBoxFormForUserToCreateNewUsername').value;
-    console.log('storeFormValueIntoOpponentUsername function ran'); //Test case of method run success to console.
-    callback(); //Callback for order of operations with loadGamePage().
+function initOpponentUsername(){
+    initOpponentUsername = document.getElementById('findOpponentByUsernameForm').value;
 }
 
-//This function loads the replacement page.
-function loadGamePage() {
-console.log(username); //Test case of username to console.
-window.location.replace("gamePage.html"); //Replacing current page with gamePage.html
-}
+
+
+
+
+
 
