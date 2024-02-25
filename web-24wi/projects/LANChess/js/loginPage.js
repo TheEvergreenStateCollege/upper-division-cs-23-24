51fdcf6e044cfc9aa5fb289e8a2fc83fa11269f6
tw-edu <156841820+tw-edu@users.noreply.github.com>
2024-02-25 23:43:08 +0000

    loginPage

--- web-24wi/projects/LANChess/js/loginPage.js
+++ web-24wi/projects/LANChess/js/loginPage.js
@@ -1,18 +1,25 @@
 //loginPage.js
 const baseURL = 'http://localhost:5000';
-var initUsername
-var checkedUsername;
+var initUsername;
+var initPassword;
+var checkedInitUsername;
+var checkedInitPassword;
+
 
 //This function stores the value of the login page's "username" textbox form into the initial initUsername
 function getInitUsername(){
     initUsername = document.getElementById('textBoxFormForUserToCreateNewUsername').value;
 }
 
+function getInitPassword(){
+    initPassword = document.getElementById('textBoxFormForUserToCreateNewPassword').value;
+}
+
 //This function checks if the initUsername is valid (ie. less than 10 charachters and whatever we specify)
 function checkIfInitUsernameIsValid(){
     if(initUsername.length <= 10){
         console.log('initUsername length is valid');
-        checkedUsername = initUsername; 
+        checkedInitUsername = initUsername; 
         //Make POST to server with checkedUsername.
         //Make GET to server to load homePage for user.
     } else {
@@ -22,11 +29,28 @@ function checkIfInitUsernameIsValid(){
     }
 }
 
-function okButtonForUsernameTextbox(){
+function checkIfInitPasswordIsValid(){
+    if(initUsername.length <= 10){
+        console.log('initPassword length is valid');
+        checkedInitPassword = initPassword; 
+        //Make POST to server with checkedUsername.
+        //Make GET to server to load homePage for user.
+    } else {
+        console.log('Username is invalid, must be 10 charachters or less, please try again.');
+        window.alert("Username is invalid, must be 10 charachters or less, please try again.");
+        location.reload();
+    }
+}
+
+function initialCheckUsernameAndPasswordTextboxes(){
     getInitUsername();
     checkIfInitUsernameIsValid();
-    console.log('The final value of checkedUsername that was POSTED to the server was ' + '[' + checkedUsername + ']');
+    console.log('The final value of checkedUsername that was POSTED to the server was ' + '[' + checkedInitUsername + ']');
+    getInitPassword();
+    checkIfInitPasswordIsValid();
+    console.log('The final value of checkedPassword that was POSTED to the server was ' + '[' + checkedInitPassword + ']');
 }
+
 
 
 
