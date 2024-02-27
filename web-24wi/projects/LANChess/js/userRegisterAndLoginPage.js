5da9f6c6c1be120b3edc2da3bcd1251e46ceaea2
tw-edu <156841820+tw-edu@users.noreply.github.com>
2024-02-27 22:48:59 +0000

    update

--- web-24wi/projects/LANChess/js/userRegisterAndLoginPage.js
+++ web-24wi/projects/LANChess/js/userRegisterAndLoginPage.js
@@ -6,6 +6,11 @@ var initPasswordValueEntered;
 var initPasswordValueEnteredLen;
 var initUsernameForRegistrationWithValidSyntax;
 var initPasswordForRegistrationWithValidSyntax;
+var enteredUsernameAndPasswordSyntaxIsValidForRegistration;
+
+var invalidUsernameAlertMessage = 'Username is invalid, must be between 2-10 charachters long, please try again.';
+var invalidPasswordAlertMessage = 'Password is invalid, must be between 2-10 charachters long, please try again.';
+
 
 
 //This function stores the value of the login page's "username" textbox form into the initial initUsername
@@ -20,29 +25,26 @@ function getInitPasswordValueEntered(){
     
 }
 
-//This function checks if the initUsername is valid (ie. less than 10 charachters and whatever we specify)
+//This function checks if the initUsername is valid (ie. between 2 and 10 char, or whatever we specify)
 function checkIfInitUsernameSyntaxIsValidForRegistration(){
     if(initUsernameValueEnteredLen >=2 && initUsernameValueEnteredLen <= 10){
-        console.log('Initial username entered has valid syntax for registration');
+        //console.log('Initial username entered has valid syntax for registration');
         initUsernameForRegistrationWithValidSyntax = initUsernameValueEntered; 
-        //Make POST to server with checkedUsername.
-        //Make GET to server to load homePage for user.
+    
     } else {
-        console.log('Username is invalid, must be between 2-10 charachters long, please try again..');
-        window.alert('Username is invalid, must be between 2-10 charachters long, please try again..');
+        //console.log('Username is invalid, must be between 2-10 charachters long, please try again..');
+        window.alert(invalidUsernameAlertMessage);
         location.reload();
     }
 }
 
 function checkIfInitPasswordSyntaxIsValidForRegistration(){
     if(initPasswordValueEnteredLen >=2 && initPasswordValueEnteredLen <= 10){
-        console.log('Initial password entered has valid syntax for registration');
+        //console.log('Initial password entered has valid syntax for registration');
         initPasswordForRegistrationWithValidSyntax = initPasswordValueEntered;
-        //Make POST to server with checkedUsername.
-        //Make GET to server to load homePage for user.
+        
     } else {
-        console.log('Password is invalid, must be between 2-10 charachters long, please try again.');
-        window.alert('Password is invalid, must be between 2-10 charachters long, please try again..');
+        window.alert(invalidPasswordAlertMessage);
         location.reload();
     }
 }
@@ -50,17 +52,30 @@ function checkIfInitPasswordSyntaxIsValidForRegistration(){
 async function initialCheckIfUsernameAndPasswordSyntaxIsValidForRegistration() {
     getInitUsernameValueEntered();
     checkIfInitUsernameSyntaxIsValidForRegistration()
-    console.log('The username entered which passed check for valid syntax was' + '[' + initUsernameForRegistrationWithValidSyntax + ']');
+    //console.log('The username entered which passed check for valid syntax was' + '[' + initUsernameForRegistrationWithValidSyntax + ']');
     
     getInitPasswordValueEntered();
     checkIfInitPasswordSyntaxIsValidForRegistration();
-    console.log('The password entered which passed check for valid syntax was ' + '[' + initPasswordForRegistrationWithValidSyntax+ ']');
+
+    //console.log('The password entered which passed check for valid syntax was ' + '[' + initPasswordForRegistrationWithValidSyntax+ ']');
+
+    var enteredUsernameAndPasswordSyntaxIsValidForRegistration = true;
+
+
+
 }
+
 
 async function registerRequest(){
     initialCheckIfUsernameAndPasswordSyntaxIsValidForRegistration();
-    console.log('REGISTER REQ PASS ' + '[' + initUsernameForRegistrationWithValidSyntax + ']')
-    console.log('REGISTER REQ PASS ' + '[' + initPasswordForRegistrationWithValidSyntax + ']');
+
+    if (initPasswordForRegistrationWithValidSyntax = true) {
+        
+        console.log('REGISTER REQ PASS ' + '[' + initUsernameForRegistrationWithValidSyntax + ']')
+        console.log('REGISTER REQ PASS ' + '[' + initPasswordForRegistrationWithValidSyntax + ']');
+    }
+
+  
 }
 
 
