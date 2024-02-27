8d190e6b7b7145a5ddfc2926f71ee5802bcc5c46
AkinaSS <114690470+AkinaSS@users.noreply.github.com>
2024-02-27 00:18:07 -0800

    Update changes for homework/classwork submission

--- web-24wi/assignments/AkinaSS/frontend/week7/graph-and-map/server.cjs
+++ web-24wi/assignments/AkinaSS/frontend/week7/graph-and-map/server.cjs
@@ -60,3 +60,37 @@ app.get("/randomGraph", async (req, res) => {
 	}
 	res.json({ results });
 });
+
+console.log(Object.keys(prisma));
+
+app.get("/users", async (req, res) => {
+	const allUsers = await prisma.user.findMany();
+	res.json(allUsers);
+});
+
+app.post("/user", async (req, res) => {
+	const result = await prisma.user.create({
+		data: {
+			username: req.body.username,
+			password: req.body.password,
+		}
+	});
+	res.json(result)
+});
+
+app.get("/cities", async (req, res) => {
+	const allCities = await prisma.uscity.findMany();
+	res.json(allCities);
+});
+
+app.post("/city", async (req, res) => {
+	const result = await prisma.uscity.create({
+		data: {
+			name: req.body.cityName,
+			longitude: Number(req.body.longitude),
+			latitude: Number(req.body.latitude),
+			authorId: Number(req.body.authorId),
+		}
+	});
+	res.json(result);
+});
---
+++ web-24wi/assignments/AkinaSS/frontend/week7/graph-and-map/src/modules/auth.js
@@ -0,0 +1,49 @@
+import jwt from "jsonwebtoken";
+import * as bcrypt from "bcrypt";
+
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  );
+  return token;
+};
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+
+  if (!bearer) {
+    res.status(401);
+    res.send("Not authorized");
+    return;
+  }
+
+  const [, token] = bearer.split(" ");
+  if (!token) {
+    console.log("here");
+    res.status(401);
+    res.send("Not authorized");
+    return;
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = payload;
+    console.log(payload);
+    next();
+    return;
+  } catch (e) {
+    console.error(e);
+    res.status(401);
+    res.send("Not authorized");
+    return;
+  }
+};
+
+export const comparePasswords = (password, hash) => {
+  return bcrypt.compare(password, hash);
+};
+
+export const hashPassword = (password) => {
+  return bcrypt.hash(password, 5);
+};
