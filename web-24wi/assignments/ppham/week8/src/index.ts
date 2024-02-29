66da3bab398dc6b352d74d6ccb190cd555b93b70
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2024-02-29 00:20:34 -0800

    Week 8: Hello World express server, step one.

---
+++ web-24wi/assignments/ppham/week8/src/index.ts
@@ -0,0 +1,32 @@
+import * as dotenv from 'dotenv';
+import * as http from 'http';
+dotenv.config();
+
+import app from './server';
+
+// Start with default port, some servers may already be listening on port 5000
+const DEFAULT_PORT: number = Number(process.env.API_PORT) || 5000;
+let port = DEFAULT_PORT;
+
+// Wrap the Express app with an HTTP server manually 
+const server = http.createServer(app);
+const serverFunc = () => {
+  server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+};
+
+server.on('error', (error: NodeJS.ErrnoException) => {
+  if (error.code === 'EADDRINUSE') {
+    console.error(`Port ${port} is already in use. If this is not expected,`);
+    console.error(`you may have run this server previously, possibly on a zombie process.`);
+    console.error(`Trying the next port number.`);
+    port += 1;
+    setTimeout(serverFunc, 1000);
+  } else {
+    console.error('Error starting server:', error);
+  }
+  // Optional: Perform additional actions like exiting the application
+});
+// Kick off the chain of server listen retries with the original port
+serverFunc();
