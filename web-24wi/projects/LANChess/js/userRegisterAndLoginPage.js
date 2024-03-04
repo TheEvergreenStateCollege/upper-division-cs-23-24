a8970574e4209ab8f2a45bb40ac000c6d6e23042
tw-edu <156841820+tw-edu@users.noreply.github.com>
2024-03-04 01:49:14 +0000

    cleaned up register and login page

--- web-24wi/projects/LANChess/js/userRegisterAndLoginPage.js
+++ web-24wi/projects/LANChess/js/userRegisterAndLoginPage.js
@@ -1,75 +1,28 @@
 //userRegisterAndLoginPage.js
 const baseURL = 'http://localhost:5000';
-var initUsernameValueEntered;
-var initUsernameValueEnteredLen;
-var initPasswordValueEntered;
-var initPasswordValueEnteredLen;
-var initUsernameForRegistrationWithValidSyntax;
-var initPasswordForRegistrationWithValidSyntax;
-var enteredUsernameAndPasswordSyntaxIsValidForRegistration;
+var usernameValue;
+var usernameValueLen;
+var passwordValue;
+var passwordValueLen;
 
-var invalidUsernameAlertMessage = 'Username is invalid, must be between 2-10 charachters long, please try again.';
-var invalidPasswordAlertMessage = 'Password is invalid, must be between 2-10 charachters long, please try again.';
-
-
-
-
-function getInitUsernameValueEntered(){ //This function stores the value of the login page's "username" textbox form into the initial initUsername
-    initUsernameValueEntered = document.getElementById('textBoxFormForUserToCreateNewUsername').value;
-    initUsernameValueEnteredLen = initUsernameValueEntered.length;
+async function credentials() { //This function stores the value of the login page's "username" textbox form into the initial initUsername
+    usernameValue = document.getElementById('usernameForm').value;
+    passwordValue = document.getElementById('passwordForm').value;
+    usernameValueLen = usernameValue.length;
+    passwordValueLen = passwordValue.length;
 }
 
-function getInitPasswordValueEntered(){
-    initPasswordValueEntered = document.getElementById('textBoxFormForUserToCreateNewPassword').value;
-    initPasswordValueEnteredLen = initPasswordValueEntered.length;
-    
-}
-
-function checkIfInitUsernameSyntaxIsValidForRegistration(){ //This function checks if the initUsername is valid (ie. between 2 and 10 char, or whatever we specify)
-    if(initUsernameValueEnteredLen >=2 && initUsernameValueEnteredLen <= 10){
-        
-        initUsernameForRegistrationWithValidSyntax = initUsernameValueEntered; 
-    
+async function credentialsSyntaxCheckForRegistration(){
+    if ((usernameValueLen && passwordValueLen >=2) && (usernameValueLen && passwordValueLen <=10)) {
+        usernameValue = usernameValue;
+        passwordValue = passwordValue;
+        console.log("success");
     } else {
-        
-        window.alert(invalidUsernameAlertMessage);
-        location.reload();
+        window.alert('Invalid username or password');
     }
 }
 
-function checkIfInitPasswordSyntaxIsValidForRegistration(){
-    if(initPasswordValueEnteredLen >=2 && initPasswordValueEnteredLen <= 10){
-       
-        initPasswordForRegistrationWithValidSyntax = initPasswordValueEntered;
-        
-    } else {
-        window.alert(invalidPasswordAlertMessage);
-        location.reload();
-    }
+async function registerNewUser(){
+    credentials();
+    credentialsSyntaxCheckForRegistration();
 }
-
-async function initialCheckIfUsernameAndPasswordSyntaxIsValidForRegistration() {
-    getInitUsernameValueEntered();
-    checkIfInitUsernameSyntaxIsValidForRegistration()
-   
-    
-    getInitPasswordValueEntered();
-    checkIfInitPasswordSyntaxIsValidForRegistration();
-
-    
-
-    var enteredUsernameAndPasswordSyntaxIsValidForRegistration = true;
-}
-
-async function registerRequest(){
-    initialCheckIfUsernameAndPasswordSyntaxIsValidForRegistration();
-
-    if (initPasswordForRegistrationWithValidSyntax = true) {
-        
-        console.log('REGISTER REQ PASS ' + '[' + initUsernameForRegistrationWithValidSyntax + ']');
-        console.log('REGISTER REQ PASS ' + '[' + initPasswordForRegistrationWithValidSyntax + ']');
-    } 
-}
-
-
-
