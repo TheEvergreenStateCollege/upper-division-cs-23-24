ddd1954f045a1be9e6d3b03b97e59b06cc130d07
Ubuntu <ubuntu@ip-172-31-29-219.us-east-2.compute.internal>
2024-03-04 00:48:58 +0000

    all my stuff

---
+++ web-24wi/assignments/ty_f/Week-8/workspace/modules/auth.js
@@ -0,0 +1,48 @@
+import jwt from "jsonwebtoken";
+
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  );
+  return token;
+};
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+
+  if (!bearer) {
+    res.status(401);
+    res.send("Not authorized");
+    return;
+  }
+
+  const [, token] = bearer.split(" ");
+  if (!token) {
+    console.log("here");
+    res.status(401);
+    res.send("Not authorized");
+    return;
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = payload;
+    console.log(payload);
+    next();
+    return;
+  } catch (e) {
+    console.error(e);
+    res.status(401);
+    res.send("Not authorized");
+    return;
+  }
+};
+import * as bcrypt from "bcrypt";
+
+export const comparePasswords = (password, hash) => {
+  return bcrypt.compare(password, hash);
+};
+
+export const hashPassword = (password) => {
+  return bcrypt.hash(password, 5);
+};
