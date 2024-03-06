e508fd6b6c8b08cffe9a56b4bbc935343d0af456
ddnsc <91648500+ddnsc@users.noreply.github.com>
2024-03-06 04:30:35 -0800

    cp finalproject

---
+++ web-24wi/assignments/ddnsc/infra/finalproject/client/src/main.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import Browser from "./browser"
+import "./styles.css";
+import axios from 'axios';
+import Browser from "./components/browser";
+import Register from "./components/landForm";
+import Profile from "./components/profile";
+import Desktop from "./Desktop";
+import Post from "./components/Post"
+import Menu from "./components/menu";
+import CatchAll from "./components/CatchAll";
+import Filled from "./components/test";
+
+
+
+// const apiCall = () => {
+//   axios.get('http://localhost:3001').then((data) => {
+//     //this console.log will be in our frontend console
+    
+//     console.log(data)
+    
+//   })
+// }
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Browser/>
+  </React.StrictMode>,
+);
+
+// apiCall();
