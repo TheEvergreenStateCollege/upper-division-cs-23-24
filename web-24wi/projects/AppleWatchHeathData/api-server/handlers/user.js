9bb9f03ec5987f32674d4611df03cd18b6345c64
Torsten Spieler <torstenspieler@e083317.evergreen.edu>
2024-03-08 10:21:04 -0800

    can now post a user with this version

--- web-24wi/projects/AppleWatchHeathData/api-server/handlers/user.js
+++ web-24wi/projects/AppleWatchHeathData/api-server/handlers/user.js
@@ -2,31 +2,36 @@ const prisma = require('../db');
 const { hashPassword, comparePasswords } = require('../modules/auth');
 const { createJWT } = require('../modules/auth');
 
-    createNewUser = async function(req, res) {
+    console.log(JSON.stringify(Object.keys(prisma)));
+
+    exports.createNewUser = async function(req, res, next) {
     try {
         const user = await prisma.user.create({
             data: {
                 username: req.body.username,
-                password: await hashPassword(req.body.password)
+                passwordHash: await hashPassword(req.body.password)
             }
         });
-
+        console.log(user);
         const token = createJWT(user);
         res.json({ token });
 
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal server error' });
+        next(error)
     }
 };
 
 
 exports.signin = async function(req, res) {
     try {
-        const user = await prisma.user.findUnique({
+
+        const user = await prisma.user.findFirst({
             where: {
                 username: req.body.username,
             }
+            
         });
 
         if (!user) {
--- web-24wi/projects/AppleWatchHeathData/api-server/index.js
+++ web-24wi/projects/AppleWatchHeathData/api-server/index.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 const app = require('./server');
 
-app.listen(5000, () => {
-    console.log('Server is running on http://localhost:5000');
-});
+// app.listen(5000, () => {
+//     console.log('Server is running on http://localhost:5000');
+// });
