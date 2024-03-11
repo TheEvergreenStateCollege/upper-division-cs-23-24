8e4cb4ceba9b955e9bcd38d305cb27d7bdb905dd
ddnsc <91648500+ddnsc@users.noreply.github.com>
2024-03-11 05:45:33 -0700

    adjust menu window size

--- web-24wi/projects/Hostile/finalproject/client/src/components/window.tsx
+++ web-24wi/projects/Hostile/finalproject/client/src/components/window.tsx
@@ -2,31 +2,28 @@ import { useState } from "react";
 import WindowButtons from "./WindowButtons";
 
 const Window = ({ content }: { content?: any }) => {
-  // states for background colors
-  const [panl] = useState("bg-panelgray");
-  const [whi] = useState("bg-white");
-  const [text] = useState("text-black");
-  const [changed, setChanged] = useState(false);
-
-  // state for button size
-  const [sizeb, setSizeb] = useState(60);
-
-  // state for button margin
-  const [sizer, setSizer] = useState(6);
-
-  // state for top margin
-  const [sizet, setSizet] = useState(8);
-
-  // state for window enlarge
-  const margb = `mb-${sizeb}`;
-  const margr = `mr-${sizer}`;
-  const margt = `mt-${sizet}`;
   const [enlarged, setEnlarged] = useState(false);
+
+  // Callback functions for button actions
+  const handleMinimize = () => {
+    // rick roll
+    console.log("Minimize action");
+  };
+
+  const handleMaximize = () => {
+    // max hedrum gif
+    console.log("Maximize action");
+  };
+
+  const handleClose = () => {
+    // go to landing page
+    console.log("Close action");
+  };
 
   return (
       <div className="flex flex-col">
         <div
-            className={`flex justify-stretch flex-col ${panl} ${margb} ${margr} ${margt} border border-cus md:flex-1`}
+            className={`flex justify-stretch flex-col bg-panelgray 20 border border-cus`}
         >
           <div className={`flex flex-row bg-blue-900 mx-1`}>
             <img
@@ -35,13 +32,17 @@ const Window = ({ content }: { content?: any }) => {
                 alt={"alt=\"evergreen online messenger image\""}
             />
             <p className="w-full px-1 text-lg text-white">Main Menu</p>
-            <WindowButtons />
+            <WindowButtons
+                onMinimize={handleMinimize}
+                onMaximize={handleMaximize}
+                onClose={handleClose}
+            />
           </div>
           <div
               className={`mx-2 my-2 border-2 border-bargray md:flex-1 overflow-y-auto`}
           >
             <div
-                className={`flex flex-row ${whi} ${text} `}
+                className={`flex flex-row text-black`}
             >
               {content}
             </div>
--- web-24wi/projects/Hostile/finalproject/client/src/main.tsx
+++ web-24wi/projects/Hostile/finalproject/client/src/main.tsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
 
 
-    <Desktop />
+   <Menu />
 
 
   </React.StrictMode>,
