4a5d19455e41afdece2afefa8c9d1972e34de0e8
tw-edu <156841820+tw-edu@users.noreply.github.com>
2024-03-11 23:02:03 +0000

    update

--- web-24wi/projects/LANChess/js/homePage.js
+++ web-24wi/projects/LANChess/js/homePage.js
@@ -3,6 +3,8 @@ const baseURL = 'http://localhost:5000';
 var initOpponentUsername;
 
 
+
+
 async function createNewOnlineGame(){
 
     console.log("Create new game selected");
@@ -32,11 +34,35 @@ async function createNewOnlineGame(){
     localStorage.setItem('createdGameID', createdGameID);
     localStorage.setItem('createdGameStart', createdGameStart);
     localStorage.setItem('createdGameStatus', createdGameStatus);
-    
+
+    window.location.href = "http://localhost:5000/game"; //Redirect to game page
 
     
     } catch (error) {
         console.error("Failed to create a game: ", error);
+    }
+}
+
+async function connectOnlineGameClientToCreatedGame(){
+   
+    try {
+        const response = await fetch("http://localhost:5000/api/games" + createdGameID,  {
+            method: "GET", 
+            headers: {
+                'Authorization': 'Bearer ' + UserToken,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+      })
+     
+    })
+
+    const responseForConnecting = await response.json();
+    console.log(responseForConnecting);
+    
+    } catch (error) {
+        console.error("Failed to fetch game: ", error);
     }
 }
 
