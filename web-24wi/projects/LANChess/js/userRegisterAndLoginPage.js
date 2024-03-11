738cf59404017e9105d39726b3cc87cad8f09164
tw-edu <156841820+tw-edu@users.noreply.github.com>
2024-03-11 00:16:03 +0000

    completed login

--- web-24wi/projects/LANChess/js/userRegisterAndLoginPage.js
+++ web-24wi/projects/LANChess/js/userRegisterAndLoginPage.js
@@ -5,12 +5,8 @@ var usernameValueLen;
 var passwordValue;
 var passwordValueLen;
 
-async function setCookie(name, value, daysToLive) {
-    const date = new Date();
-    date.setTime(date.getTime() + (daysToLive * 24 * 60 * 60 * 1000))
-    let expires = "expires=" + date.toUTCString();
-    document.cookie = '${name}=${value}; ${expires}; path=/';
-}
+
+
 
 async function credentials() { //This function stores the value of the login page's "username" textbox form into the initial initUsername
     usernameValue = document.getElementById('textBoxFormForUserToCreateNewUsername').value;
@@ -81,16 +77,28 @@ async function loginUser(){
                 const resultObj = await response.json();
                 const userID = resultObj.id;
                 const userToken = resultObj.token;
-                console.log("Success: ");
-                console.log(userID);
-                console.log(userToken);
 
-                setCookie("userID", userID, 1);
-                console.log(document.cookie);
+                localStorage.setItem('userID', userID);
+                localStorage.setItem('userToken', userToken);
 
 
-
-    
+                try {
+                    const response = await fetch("/login", {
+                        method: "POST", 
+                            headers: {
+                                'Accept': 'application/json',
+                                "Content-Type": "application/json",
+                            },
+            
+                            body: JSON.stringify({
+                                username: usernameValue,
+                                password: passwordValue
+                      })})
+                    
+                } catch (error) {
+                    console.error("Error:", error);
+                }
+                
             } catch (error) {
                 console.error("Error:", error);
             }
