eadf04dc1cb939f152c248fba6c4edf783e858d6
tw-edu <156841820+tw-edu@users.noreply.github.com>
2024-03-11 03:59:05 +0000

    syntax

--- web-24wi/projects/LANChess/js/homePage.js
+++ web-24wi/projects/LANChess/js/homePage.js
@@ -2,14 +2,9 @@
 const baseURL = 'http://localhost:5000';
 var initOpponentUsername;
 
-
-
 async function createNewOnlineGame(){
-    
     console.log("create new online game button works");
-
     const userToken = localStorage.getItem('tokenObj');
-
     console.log(userToken);
     
     try {
@@ -20,11 +15,8 @@ async function createNewOnlineGame(){
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
         },
-
         body: JSON.stringify({
-        
       })
-
     })
     
     } catch (error) {
@@ -37,10 +29,7 @@ async function initOpponentUsername(){
 }
 
 async function checkIfOpponentCanBeFound() {
-    //Will query database with 'initOpponentUsername' to see if there is an active game created by an opponent user with that name. If True the player will join that game,
-    //if false then it will let the user know that it either
-    //1. The user cannot be found, prompt user to try name again.
-    //2. If the user is found, then an active game with that user cannot be found, tell opponent to create a game, or return to the menu and create your own.
+   
 }
 
 async function okButtonForOpponentSearchTextbox() {
