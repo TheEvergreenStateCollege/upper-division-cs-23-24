870e4e9c8e05cbcb100fad0f0556ee8d817ac0a5
ddnsc <91648500+ddnsc@users.noreply.github.com>
2024-03-13 08:47:24 -0700

    new setup

---
+++ web-24wi/assignments/ddnsc/infra/api/src/index.js
@@ -0,0 +1,23 @@
+import { createServer } from 'http';
+import dotenv from 'dotenv';
+
+const path = "../.env"
+dotenv.config({ path });
+
+const server = createServer((req, res) => {
+    if (req.url === '/' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ message: 'hello' }));
+        res.end();
+        return;
+    }
+
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Not Found' }));
+});
+
+const PORT = process.env.API_PORT;
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${API_PORT}`);
+});
