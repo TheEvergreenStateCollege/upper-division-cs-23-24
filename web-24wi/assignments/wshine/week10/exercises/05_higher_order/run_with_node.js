72c158a37ca8bef7c91cc88665b7b263b5b3c1aa
wshine <114693487+WinstonShine@users.noreply.github.com>
2024-03-19 08:49:11 -0700

    eloquent js chapter 5 exercises complete

---
+++ web-24wi/assignments/wshine/week10/exercises/05_higher_order/run_with_node.js
@@ -0,0 +1,18 @@
+// load dependencies
+require("./code/load")("code/scripts.js", "code/chapter/05_higher_order.js", "code/intro.js");
+
+function textScripts(text) {
+  let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.name : "none";
+  }).filter(({name}) => name != "none");
+
+  let total = scripts.reduce((n, {count}) => n + count, 0);
+  if (total == 0) return "No scripts found";
+
+  return scripts.map(({name, count}) => {
+    return `${Math.round(count * 100 / total)}% ${name}`;
+  }).join(", ");
+}
+
+console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
