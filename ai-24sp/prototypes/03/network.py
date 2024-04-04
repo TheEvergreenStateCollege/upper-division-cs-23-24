bb035073c2956a3c830ec053ba28c69b18be7a6b
julian <57913387+Julianc02@users.noreply.github.com>
2024-04-04 14:23:04 -0700

    Ignore MNIST data files and add beginning Python scripts.

---
+++ ai-24sp/prototypes/03/network.py
@@ -0,0 +1,7 @@
+class Network(object):
+    
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
