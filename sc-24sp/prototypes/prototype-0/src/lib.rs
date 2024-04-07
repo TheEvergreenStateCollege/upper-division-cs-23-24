2f1b15ef7d3517ae58eb87d6db1f0b9aa86335ba
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2024-04-06 19:52:35 -0700

    Continue module examples from Rust Book with breakfast / restaurant.

---
+++ sc-24sp/prototypes/prototype-0/src/lib.rs
@@ -0,0 +1,60 @@
+pub mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn server_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+pub mod back_of_house {
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: & str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+fn deliver_order() {}
+
+pub fn eat_at_restaurant() {
+
+    // Order a breakfast in the summer with Rye toast 
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed 
+    // to see or modify the seasonal fruit that coes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    /*
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+     */
+}
--- sc-24sp/prototypes/prototype-0/src/main.rs
+++ sc-24sp/prototypes/prototype-0/src/main.rs
@@ -1,3 +1,5 @@
+use prototype_0::eat_at_restaurant;
+
 use crate::garden::vegetables::Asparagus;
 
 pub mod garden;
@@ -5,4 +7,5 @@ pub mod garden;
 fn main() {
     let plant = Asparagus {};
     println!("I'm growing {:?}!", plant);
+    eat_at_restaurant();
 }
