284c3759ea2d81464418bdf6e5bb05b5c83648e7
SP <bunnyhoppy3@gmail.com>
2024-04-08 21:54:39 +0000

    lab 4/8/24

--- sc-24sp/prototypes/prototype-05/src/main.rs
+++ sc-24sp/prototypes/prototype-05/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use regex_lite::Regex;
+
+use prototype_0::moves::MoveError;
+use prototype_0::moves::enumerator::list_moves;
+use prototype_0::types::{Board,Player,Move};
+use prototype_0::validators::win_validator;
+
+fn do_move<'a>(mut board: Board<'a>, next_move: &Move, player: &Player) -> Board<'a> {
+    let (move_error: Option<MoveError>) = board.make_move(new_move: next_move: next_move, player);
+    prntln!("{:?"}, board);
+    match move_error {
+        Some(MoveError::OutOfBounds)=> {
+            prntln!("move out of bounds");
+
+        },
+        Some(MoveError::CellTaken)=> {
+
+        },
+        Some(MoveError::WrongPlayer)=> {
+            prntln!("Wrong player, skipping turn");
+
+        },
+        None => {
+
+        },
+
+    }
+    board
 }
+
+fn main() {
+    let board:Board<'static> = Board::new();
+    let mut board_in_progress: Board = board.clone();
+    let (moves: Vec<Move>, mut _board_ref: &Board<'static>) = list_moves(&board);
+
+    let moves_iter: Iter<'static>, Move> = moves.iter();
+
+    let re: Regex::new(pattern: r"\(([0-2]),([0-2])\)").unwrap();
+
+    prntln!("{:?}", board);
+}
+
--- sc-24sp/prototypes/prototype-05/src/validators/mod.rs
+++ sc-24sp/prototypes/prototype-05/src/validators/mod.rs
@@ -11,7 +11,41 @@ fn col_win_validator(board: &Board, col: u32)-> bool{
     let mut all_equal: bool = true;
     for i: usize in 0..2 {
         all_equal = all_equal && (board.cells[i][col as usize] == board.cells[i+1][col as usize])
-        
-}
     }
+    all_equal && (board.cells)
+}
+fn diag1_win_validator(board: &Board) -> bool {
+    let mut all_equal: bool = true;
+    for i: usize in 0..2 {
+        all_equal = all_equal &&  board.cells[i][i] == board.cells[i+1][i+1];
+    }
+    all_equal && (board.cells[0][0] != Option::None)
+    }
+    fn diag1_win_validator(board: &Board) -> bool {
+        let mut all_equal: bool = true;
+        for i: usize in 0..2 {
+            all_equal = all_equal && board.cells[i][2-i] == board.cells[i+1][1-i];;
+        }
+        all_equal && (board.cells[0][2] != Option::None)
+        }
+        pub fn win_validator(board: &Board)-> bool {
+            for row: u32 in 0..3 {
+                if row_win_validator(board, row){
+                    return true
+                }
+            }
+            for row: u32 in 0..3 {
+                if row_win_validator(board, row){
+                    return true
+        }
+}
+    if diag1_win_validator(board){
+        println!("diag1 was winning");
+        return true
+    }
+    if diag2_win_validator(board){
+        println!("diag2 was winning");
+        return true
+    }
+    return false;
 }
