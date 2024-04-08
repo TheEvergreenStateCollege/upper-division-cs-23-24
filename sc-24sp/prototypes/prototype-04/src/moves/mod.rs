1f47d6dc682539163154d56413ca47bd4b8d7150
Pope_Rigby <poperigby@mailbox.org>
2024-04-08 14:22:16 -0700

    Second commit

--- sc-24sp/prototypes/prototype-04/src/moves/mod.rs
+++ sc-24sp/prototypes/prototype-04/src/moves/mod.rs
@@ -1,3 +1,23 @@
 pub mod enumerator {
     use crate::types::{Board, Cell, Move};
+
+    pub fn list_moves<'a>(board: &'a Board) -> &[Move] {
+        let mut moves: Vec<Move> = Vec::new();
+        for row in 0..3 {
+            for col in 0..3 {
+                if board.cells[row][col] == Cell::None {
+                    moves.push(Move { coords: (row, col) });
+                }
+            }
+        }
+
+        moves.into()
+    }
+}
+
+#[derive(Debug)]
+pub enum MoveError {
+    WrongPlayer,
+    CellTaken,
+    OutOfBounds,
 }
