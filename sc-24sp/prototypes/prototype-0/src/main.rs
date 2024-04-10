12104d164d5caed856eb2bff43c8dddf147bfa63
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2024-04-09 22:10:29 -0700

    Pass mutable &board ref, no need to return board ownership. Use moves iterator in main loop instead of looping over moves_list outside player input.

--- sc-24sp/prototypes/prototype-0/src/main.rs
+++ sc-24sp/prototypes/prototype-0/src/main.rs
@@ -9,7 +9,7 @@ use prototype_0::moves::enumerator::list_moves;
 use prototype_0::types::{Board,Player,Move};
 use prototype_0::validators::win_validator;
 
-fn do_move<'a>(mut board: Board<'a>, next_move: &Move, player: &Player) -> Board<'a> {
+fn do_move<'a>(board: &mut Board<'a>, next_move: &Move, player: &Player) {
     let (move_error) = board.make_move(next_move, player);
     println!("{:?}", board);
     match move_error {
@@ -31,14 +31,13 @@ fn do_move<'a>(mut board: Board<'a>, next_move: &Move, player: &Player) -> Board
 
 
     } 
-    board               
 }
+
 fn main() {
-    let board = Board::new();
-    let mut board_in_progress: Board = board.clone();
+    let mut board: Board = Board::new();
     let (moves, mut _board_ref) = list_moves(&board);
 
-    let moves_iter = moves.iter();
+    let mut moves_iter = moves.iter();
 
     let re = Regex::new(r"\(([0-2]),([0-2])\)").unwrap();
 
@@ -47,13 +46,12 @@ fn main() {
     // Creating a first move
     // let first_move = Move { coords: (1,1), player: &Player::O };
 
-    // Main loop
-    // We are currently a naive move picker
-    // We already have a fixed list of all possible moves that we iterate through in order,
-    // We let the player choose their move, the player starts first
-    // But this does not affect our "strategy" at all
-    for next_move in moves_iter {
-
+    loop {
+        // Main loop
+        // We are currently a naive move picker
+        // We already have a fixed list of all possible moves that we iterate through in order,
+        // We let the player choose their move, the player starts first
+        // But this does not affect our "strategy" at all
         println!("Input your move as a tuple: (y,x) where x,y are 0 1 2: ");
         let mut guess = String::new();
 
@@ -61,23 +59,34 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let mut board1;
+        let mut caps_iter = re.captures_iter(&guess).map(|c| c.extract());
+        let (_, [row_cap, col_cap]) = caps_iter.next().unwrap();
 
-        for (_, [row_cap, col_cap]) in re.captures_iter(&guess).map(|c| c.extract()) {
-            match (row_cap.parse::<u8>(), col_cap.parse::<u8>()) {
-                (Ok(_row), Ok(_col)) => {
-                    let player_move = Move { coords: ( _row, _col)};
-                    board1 = do_move(board_in_progress, &player_move, &Player::O);
-                    // Our solver's move
-                    board_in_progress = do_move(board1, next_move, &Player::X);
-                }
-                _ => {
-                    println!("Sorry I couldn't understand this move ({:?},{:?}, let's try again", row_cap, col_cap);
-                }
-            };
+        match (row_cap.parse::<u8>(), col_cap.parse::<u8>()) {
+            (Ok(_row), Ok(_col)) => {
+                let player_move = Move { coords: ( _row, _col)};
+                do_move(&mut board, &player_move, &Player::O);
+                // Our solver's move
+            }
+            _ => {
+                println!("Sorry I couldn't understand this move ({:?},{:?}, let's try again", row_cap, col_cap);
+                continue; // Don't skip ahead and let the solver move
+            }
+        };
+
+        match moves_iter.next() {
+            Some(next_move) => {
+                do_move(&mut board, next_move, &Player::X);
+            }
+            None => {
+                // no more moves are left
+                // This shouldn't happen, but if so, solver forfeits
+                println!("🏆 GAME WON 🏆 \n by {:?}", board.next_to_move);
+                break;    
+            }
         }
 
-        if win_validator(&board_in_progress) {
+        if win_validator(&board) {
             println!("🏆 GAME WON 🏆 \n by {:?}", board.next_to_move);
             break;
         }    
