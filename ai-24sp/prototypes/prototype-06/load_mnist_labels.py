5c646ea7871863ccc5665d1670c13237f241546a
Gavin Bowers <supercat648@gmail.com>
2024-04-11 14:19:44 -0700

    added network.py and loaders from prototype-0

--- ai-24sp/prototypes/prototype-06/load_mnist_labels.py
+++ ai-24sp/prototypes/prototype-06/load_mnist_labels.py
@@ -1,19 +1,21 @@
 f = open("train-labels-idx1-ubyte", "rb")
+
 data = f.read()
 
 def convert_four(arr):
-    result=0
+    result = 0
     for i in arr:
         print(f"result {result} {i}")
-        result *= 255 # Shifting by 8 bits
-        result += i
+        result *= 255
+        result += i 
     return result
 
 size = int(data[6]*256+data[7])
 print(f"Number of images {size}")
-labels = []
 
-f2 = open("label.txt", "w")
+labels = [] 
+
+f2 = open("labels.txt", "w")
 
 for i in range(size):
     print(chr(data[8+i]+48))
