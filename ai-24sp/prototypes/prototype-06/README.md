4950804afe6cf01a03766987ec030efd9bf86af4
Gavin Bowers <supercat648@gmail.com>
2024-04-11 15:06:57 -0700

    updated loader and added readme

---
+++ ai-24sp/prototypes/prototype-06/README.md
@@ -0,0 +1,7 @@
+
+Julian and Gavin worked on this segment
+
+Gavin's note:
+
+I deleted the outdated files that were here previously and added the current loaders files and copied network.py from the source again, and quickly fixed the python2 outdated syntax. I wasn't able to try running the network because Paul didn't finish the loader until the end of class, and I lack the knowledge of Python to translate from Python 2 to 3.
+
--- ai-24sp/prototypes/prototype-06/load_mnist.py
+++ ai-24sp/prototypes/prototype-06/load_mnist.py
@@ -79,12 +79,150 @@ def load_training_label(i):
     print(f"Label of image {i} is {chr(label+48)} {one_hot_vector}")
 
 
-    #for i in range(size):
-    #    print(chr(data[8+i]+48))
-    #    f2.write(chr(data[8+i]+48))
 
-    #f2.close()
-    #f.close()
 
-load_training_image(127)
-load_training_label(127)
+
+
+
+# Return ndarray of shape [60000, 784]
+def load_all_training_images():
+    f = open("train-images-idx3-ubyte", "rb")
+
+    data = f.read()
+
+    size = int(data[6]*256+data[7])
+    print(f"Number of images {size}")
+
+    width = int(bytes_to_num(data[8:12]))
+    height = int(bytes_to_num(data[12:16]))
+
+    print(f"width {width}")
+    print(f"height {height}")
+
+    start=16
+    end=16 + 784*size
+    
+    print(f"start image data at byte {start}")
+    print(f"end image data at byte {end}")
+    data_list = list(data[16:16 + 784 * size]) # I think this is bytearray type
+    # we want list of bytes
+    print(f"length of sliced 1D image data {type(data_list)}")
+
+    data_arr = np.array(data_list, dtype=np.ubyte).reshape((size, width*height, 1))
+
+    #Expect (60000*784, 1)  (the ,1) is a grayscale pixel value 0-255
+    # or (47040000, 1)
+    print(f"Shape of data straight from file {data_arr.shape}")
+
+    return (data_arr, width, height) 
+
+# Return ndarray of shape [10000, 784]
+def load_all_test_images():
+    f = open("t10k-images-idx3-ubyte", "rb")
+
+    data = f.read()
+
+    size = int(data[6]*256+data[7])
+    print(f"Number of images {size}")
+
+    width = int(bytes_to_num(data[8:12]))
+    height = int(bytes_to_num(data[12:16]))
+
+    print(f"width {width}")
+    print(f"height {height}")
+
+    start=16
+    end=16 + 784*size
+    
+    print(f"start image data at byte {start}")
+    print(f"end image data at byte {end}")
+    data_list = list(data[16:16 + 784 * size]) # I think this is bytearray type
+    # we want list of bytes
+    print(f"length of sliced 1D image data {type(data_list)}")
+
+    data_arr = np.array(data_list, dtype=np.ubyte).reshape((size, width*height, 1))
+
+    #Expect (60000*784, 1)  (the ,1) is a grayscale pixel value 0-255
+    # or (47040000, 1)
+    print(f"Shape of data straight from file {data_arr.shape}")
+
+    return (data_arr, width, height) 
+
+# 0x7  ->  [ 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 ]
+# label is integer 0-9 to index output vector
+def one_hot_vector_from_label(label):
+    e = np.zeros((10, 1))
+    e[label] = 1.0
+    return e
+
+# Return ndarray of shape [10000, 10]
+def load_all_training_labels():
+    f = open("train-labels-idx1-ubyte", "rb")
+
+    data = f.read()
+
+    size = bytes_to_num(data[6:8])
+    print(f"Number of images {size}")
+
+    return np.array([one_hot_vector_from_label(l) for l in data[8:]])
+
+#    label = data[8+i]
+#    one_hot_vector = one_hot_vector_from_label(label)
+
+#    print(f"Label of image {i} is {chr(label+48)} {one_hot_vector}")
+
+# Return ndarray of shape [10000, 10]
+def load_all_test_labels():
+    f = open("t10k-labels-idx1-ubyte", "rb")
+
+    data = f.read()
+
+    size = bytes_to_num(data[6:8])
+    print(f"Number of test labels {size}")
+
+    return np.array([one_hot_vector_from_label(l) for l in data[8:]])
+
+
+## LOAD TRAINING IMAGES AND LABELS
+# shape is (60000, 784, 1)
+(original_training_images, width, height) = load_all_training_images()
+print(f"Training data shape {original_training_images.shape}")
+# save_single_image(original_training_images[127], width, height)
+
+# shape is (60000, 10)
+training_labels = load_all_training_labels()
+# load_training_label(127)
+
+# shape is (60000, ((784, 1), 10))
+# 60000 2-tuples, of (x,y) image,label training pair
+original_training_data = zip(original_training_images, training_labels)
+
+# shape is (10000, ((784, 1), 10))
+#validation_data = original_training_data[50000:60000]
+#print(f"Validation data shape {validation_data.shape}")
+
+# Use all 60000 of original data for training for now
+training_data = original_training_data
+
+# shape is (10000, 784, 1)
+(test_images, width, height) = load_all_test_images()
+# shape is (10000, 10)
+test_labels = load_all_test_labels()
+# shape is (10000, ((784, 1), 10))
+# 10000 2-tuples, of (x,y) image,label training pair
+test_data = zip(test_images, test_labels)
+
+from network import Network
+
+nn = Network([784, 100, 10])
+nn.SGD(
+    training_data=training_data,
+    epochs=30,
+    mini_batch_size=10,
+    eta=0.001,
+    test_data=test_data
+)
+
+# When this stops, you'll have a model with ~96% success rate (4% error)
+
+# Validate here with validate_data
