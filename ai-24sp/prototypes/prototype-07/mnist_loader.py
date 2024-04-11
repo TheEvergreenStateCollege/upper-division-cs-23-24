b7dfdddcd7d7d6882b4f791454634277df2e0b21
Stella Parker <116223316+whereismyprozac@users.noreply.github.com>
2024-04-11 21:24:21 +0000

    ( ͡° ͜ʖ ͡° )

---
+++ ai-24sp/prototypes/prototype-07/mnist_loader.py
@@ -0,0 +1,28 @@
+import mnist_loader
+import network
+
+import pickle
+import gzip
+import numpy as np
+
+def load_data():
+    with gzip.open('mnist.pkl.gz', 'rb') as f:
+        training_data, validation_data, test_data = pickle.load(f, encoding='latin1')
+    return (training_data, validation_data, test_data)
+
+def load_data_wrapper():
+    tr_d, va_d, te_d = load_data()
+    training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
+    training_results = [vectorized_result(y) for y in tr_d[1]]
+    training_data = list(zip(training_inputs, training_results))
+    validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
+    validation_data = list(zip(validation_inputs, va_d[1]))
+    test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
+    test_data = list(zip(test_inputs, te_d[1]))
+    return (training_data, validation_data, test_data)
+
+def vectorized_result(j):
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+    return e
+
