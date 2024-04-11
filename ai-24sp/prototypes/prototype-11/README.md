8a721a9b56b2a60ef15581ee465d66a01668c52b
Jonah Eadie <jonaheadie@gmail.com>
2024-04-11 15:02:52 -0700

    Implemented load_mnist.py, compatible with Python3

--- ai-24sp/prototypes/prototype-11/README.md
+++ ai-24sp/prototypes/prototype-11/README.md
@@ -1,3 +1,11 @@
+##### 2024-04-11 JonahEadieEvergreen, Rilesbe
+
+Began work on adding functionality from mnist-loader.py (written for Python2) to
+load_mnist.py, along with Paul and the rest of the class.
+
+Copied Paul's load_mnist.py, though ran into errors running it with our 
+version of network.py (`line 20, object of type zip has no len()`).
+
 ##### 2024-04-04 wshine
 
 Finished typing through `update_mini_batch`, unsure how many typos were made and
@@ -5,3 +13,4 @@ only the creation of a network object was tested
 
 image/label loader saves to a directory data/ which is ignored and might need to
 be created
+
--- ai-24sp/prototypes/prototype-11/network.py
+++ ai-24sp/prototypes/prototype-11/network.py
@@ -50,11 +50,55 @@ class Network(object):
 
     # not yet implemented
     def backprop(self, x, y):
-        pass
+        """Return a tuple ``(nabla_b, nabla_w)`` representing the
+        gradient for the cost function C_x.  ``nabla_b`` and
+        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
+        to ``self.biases`` and ``self.weights``."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feedforward
+        activation = x
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * \
+            sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        # Note that the variable l in the loop below is used a little
+        # differently to the notation in Chapter 2 of the book.  Here,
+        # l = 1 means the last layer of neurons, l = 2 is the
+        # second-last layer, and so on.  It's a renumbering of the
+        # scheme in the book, used here to take advantage of the fact
+        # that Python can use negative indices in lists.
+        for l in xrange(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
 
     # not yet implemented
     def evaluate(self, data):
-        pass
+        """Return the number of test inputs for which the neural
+        network outputs the correct result. Note that the neural
+        network's output is assumed to be the index of whichever
+        neuron in the final layer has the highest activation."""
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        """Return the vector of partial derivatives partial C_x
+        partial a for the output activations."""
+        return (output_activations-y)
 
 
 def sigmoid(z):
