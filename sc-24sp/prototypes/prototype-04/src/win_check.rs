d870f69c4eb47f3d4724be2b46c60d8960597924
Pope_Rigby <poperigby@mailbox.org>
2024-04-12 11:15:40 -0700

    Stuff

--- sc-24sp/prototypes/prototype-04/src/win_check.rs
+++ sc-24sp/prototypes/prototype-04/src/win_check.rs
@@ -1,12 +1,6 @@
 use crate::board::{Board, CellState};
 
-pub enum WinState {
-    X,
-    O,
-    Neither,
-}
-
-pub fn check_diagonal(board: &Board) -> WinState {
+pub fn check_diagonal(board: &Board) -> bool {
     let cells = [
         board.get_cell(0, 0),
         board.get_cell(1, 1),
@@ -14,12 +8,8 @@ pub fn check_diagonal(board: &Board) -> WinState {
     ];
 
     if cells.iter().all(|&cell| cell == cells[0]) {
-        match cells[0] {
-            CellState::X => WinState::X,
-            CellState::O => WinState::O,
-            CellState::Empty => WinState::Neither,
-        }
+        true
     } else {
-        WinState::Neither
+        false
     }
 }
