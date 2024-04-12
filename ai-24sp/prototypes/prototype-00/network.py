cbd602d65f636fcb922d90f4d56beacde9e53088
Paul Pham <148553+learner-long-life@users.noreply.github.com>
2024-04-12 00:34:38 +0000

    Loads data, fixed Python2 bugs, runs but never outputs message for Epoch 1 on GitPod even with 16 cores and 70% usage on each.

--- ai-24sp/prototypes/prototype-00/network.py
+++ ai-24sp/prototypes/prototype-00/network.py
@@ -1,9 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+network.py
+~~~~~~~~~~~
+
+By Mike Nielsen, https://github.com/mnielsen/neural-networks-and-deep-learning?tab=readme-ov-file
+A module to implement the stochastic gradient descent learning
+algorithm for a feedforward neural network. Gradients are
+calculated using backpropagation. Note that Mike Nielsen
+focused on making the code simple, easily readable, and easily modifiable.
+It is not optimized, and omits many desirable features.
+"""
+
+import numpy as np
+import random
+
 class Network(object):
 
     def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[i]]
+        # QUESTION: Why do we initialized randomly?
+        # By analogy: if you are exploring a strange landscape for an unknown prize, is
+        # it always better to start in a random location?
+        # QUESTION: Why do we not have biases for the first layer? Because we slice off sizes[1:]
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
             for x, y in zip(sizes[:-1], sizes[1:])]
     
@@ -23,24 +44,125 @@ class Network(object):
         eta,
         test_data=None
     ) :
-    """Train the neural network using mini-batch stochastic
-    gradient descent. The "training data" is a list of tuples
-    "(x, y)" representing the training inputs and the desired
-    outputs. The other non-optional parameters are
-    self-explanatory. If "test_data" is provided then the
-    network will be evaluated against the test data after each
-    epoch, and partial progress printed out. This is useful for
-    tracking progress, but slows things down substantially."""
-    if test_data:
-        n_test = len(test_data)
-    n = len(training_data)
+        """Train the neural network using mini-batch stochastic
+        gradient descent. The "training data" is a list of tuples
+        "(x, y)" representing the training inputs and the desired
+        outputs. The other non-optional parameters are
+        self-explanatory. If "test_data" is provided then the
+        network will be evaluated against the test data after each
+        epoch, and partial progress printed out. This is useful for
+        tracking progress, but slows things down substantially."""
+        if test_data:
+            n_test = len(test_data)
+        n = len(training_data)
 
-    for j in xrange(epochs):
-        random.shuffle(training_data)
-        mini_batches = [
-            training_data[k:k+mini_batch_size]
-            for k in xrange(0, n, mini_batch_size)
-        ]
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in range(0, n, mini_batch_size)
+            ]
 
-        for mini_batch in mini_batches:
-            self.update_mini_batch(mini_batches, eta)
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+
+            if test_data:
+                print(f"Epoch {j}: {self.evaluate(test_data)} / {n_test}")
+            else:
+                print(f"Epoch {j} complete")
+
+    def update_mini_batch(self, mini_batch, eta):
+        """
+        Update the network's weights and biases by applying
+        gradient descent using backpropagation to a single mini batch.
+        The ``mini_batch`` is a list of tuples ``(x,y)``, and ``eta``
+        is the learning rate.
+        """
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x,y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+            for w,nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+            for b,nb in zip(self.biases, nabla_b)]
+
+    # x and y 
+    def backprop(self, x, y):
+        """
+        Return a tuple ``(nabla_b, nabla_w)`` representing the
+        gradient for the cost function C_x. ``nabla_b`` and 
+        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
+        to ``self.biases`` and ``self.weights``.
+        """
+        # nabla's are of the same dimension as the biases and weights
+        # QUESTION: Why do we keep the nabla_b's separate from nabla_w's
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feedforward
+        activation = x  # this looks like where we place training inputs
+        activations = [x] # list to store all the activations, layer by layer
+        # Activations is like the "wave" of current data, passing through
+        # layer by layer.
+        # But each layer is of different size 
+        # So if the network is [784, 30, 10] with three layers,
+        # The activations after each 
+        zs = [] # list to store all the z vectors, layer by layer 
+        # QUESTION: What the heck are z vectors? Check the reading again.
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b 
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        # backward pass 
+
+        # we take the cost_derivative (derivative with respect to )
+        delta = self.cost_derivative(activations[-1], y) * \
+            sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta 
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        # Note that the variable l in the loop below is used a little
+        # differently to the notation in Chapter 2 of the book. Here,
+        # l = 1 means the last layer of neurons, l = 2 is the second-last
+        # layer, and so on. It's a renumbering of the
+        # scheme in the book, used here to take advantage of the fact
+        # that Python can use negative indices in lists.
+        for l in range(2, self.num_layers):
+            # QUESTION: Why do go backwards in the activations starting from the second-to-last?
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            # QUESTION: What shape is delta? Does it stay the same through all the layers?
+            #print(-l+1, self.weights[-l+1].transpose().shape, delta.shape, sp.shape)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp 
+            nabla_b[-l] = delta 
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
+    def evaluate(self, test_data):
+        """
+        Return the number of test inputs for which the neural
+        network outputs the correct result. Note that the neural
+        network's output is assumed to be the index of whichever
+        neuron in the final layer has the highest activation.
+        """
+        test_results = [(np.argmax(self.feedforward(x)), y)
+            for (x,y) in test_data]
+        return sum(int(x == y) for (x,y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        """
+        Return the vector of partial derivatives \partial C_x /
+        \partial a for the output activations.
+        """
+        return (output_activations-y)
+
+#### Miscellaneous functions
+def sigmoid(z):
+    """The sigmoid function."""
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
