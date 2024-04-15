07e06998a7f884f376f021df2600e3c282d01ee0
Pope_Rigby <poperigby@mailbox.org>
2024-04-15 11:42:20 -0700

    Cleanup

--- sc-24sp/prototypes/prototype-04/src/ai.rs
+++ sc-24sp/prototypes/prototype-04/src/ai.rs
@@ -9,31 +9,34 @@ use crate::board::{Board, GameResult, Player};
 pub struct AI {}
 
 impl AI {
-    /// Make the AI perform a move given a board.
+    /// Make the AI perform a move, given a board.
     pub fn make_move(&self, b: &mut Board) {
-        // Check if one of the next possible moves is a win. If so, make it.
+        // If one of the next possible moves is a win. If so, make it.
         if let Some((x, y)) = self.check_for_possible_win(b, Player::AI) {
             b.place(x, y, Player::AI);
         // Otherwise, if one of the human's next possible moves is a win, block it.
         } else if let Some((x, y)) = self.check_for_possible_win(b, Player::Human) {
             b.place(x, y, Player::AI);
-        // Otherwise, if center is not taken, take it.
-        } else if b.get_cell(1, 1).is_none() {
-            b.place(1, 1, Player::AI);
-        // Otherwise, make any move.
-        } else if let Some((row, column)) = self.check_for_empty_spaces(b) {
-            // TODO: Maybe take a random spot?
-            b.place(row, column, Player::AI);
-        }
+        } else {
+            // Thinking two steps ahead:
+            // For every possible move:
+            //     Make move
+            //     If check_for_possible_win() returns a possible win
+            //       Make the move that will get us a future win
+            //       Break out of loop and exit function
+            //   Otherwise
+            //     Undo the move
+            // }
 
-        // TODO: How can I fit this into the hierarchy of possible moves?
-        // Thinking two steps ahead:
-        // For every possible move:
-        //     Make move
-        //   If check_for_possible_win() returns a possible win
-        //     Make winning move
-        //   Otherwise
-        //     Undo the move
+            // If center is not taken, take it.
+            if b.get_cell(1, 1).is_none() {
+                b.place(1, 1, Player::AI);
+            // Otherwise, make any move.
+            } else if let Some((row, column)) = self.check_for_empty_spaces(b) {
+                // TODO: Maybe take a random spot?
+                b.place(row, column, Player::AI);
+            }
+        }
     }
 
     /// Check if any of the next moves will win
