8d530cc4544daf97eb9757a96e0126dcad61a397
Jonah Eadie <jonaheadie@gmail.com>
2024-04-15 13:59:43 -0700

    Documented lines in ranker.rs and winning.rs to understand logic

--- sc-24sp/prototypes/prototype-07/src/moves/ranker.rs
+++ sc-24sp/prototypes/prototype-07/src/moves/ranker.rs
@@ -11,11 +11,13 @@ struct RankedMove {
 
 // Return all moves that are equally likely to lead to a draw or for next player to win
 pub fn rank_moves(board: &mut Board) -> Vec<Move> {
-    let all_valid_moves = list_moves(board);
-    let mut best_moves = Vec::<Move>::with_capacity(all_valid_moves.len());
+    let all_valid_moves = list_moves(board); // vector of all available spaces
+    let mut best_moves = Vec::<Move>::with_capacity(all_valid_moves.len()); // instantiates vector with length of above
     let mut best_in_a_row = 0;
-    for state in get_winning_states().iter() {
+    for state in get_winning_states().iter() { // iterates through all 8 winning sets of moves
         let (board_match, whose_ahead) = state.match_board(board);
+        // board_match: struct containing player and the num of moves in a row they already have made
+        // whose_head: enum representing player ahead (O, X, TIE)
         // YOUR CODE HERE
         // given all moves that help either player win,
         // how to choose moves to return?
