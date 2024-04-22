2d7a6ff6d2fe28f8c143d12b7f949ecaaee80ed6
Ryan Geiser <rgeiser1@gmail.com>
2024-04-22 21:27:35 +0000

    working on in class activity

--- sc-24sp/assignments/ryan-geiser/city-addresses/src/city_drawer.rs
+++ sc-24sp/assignments/ryan-geiser/city-addresses/src/city_drawer.rs
@@ -12,57 +12,33 @@ pub struct AddressAvenue {
 }
 
 pub struct AddressStreet {
-    pub x: usize,
+    pub y: usize,
 }
 
 pub const WIDTH: usize = 50;
 pub const HEIGHT: usize = 50;
 
-pub fn city_drawer(n_s_avenues: &mut Vec<Avenue>, e_w_streets: &mut Vec<Street>) {
-    n_s_avenues.sort_by(|a,b| a.x.cmp(&b.x));
-    e_w_streets.sort_by(|a,b| a.y.cmp(&b.y));
-
-    let mut grid: [[char; WIDTH]; HEIGHT] = [['.' as char; WIDTH]; HEIGHT];
+pub fn city_drawer(n_s_avenues: &[Avenue], e_w_streets: &[Street]) {
+    // Create a city layout
+    let mut grid: [[char; WIDTH]; HEIGHT] = [['.'; WIDTH]; HEIGHT];
 
     let mut n_s_iter = n_s_avenues.iter();
-    loop {
-        match n_s_iter.next() {
-            Some(avenue) => {
-                for y in 0..HEIGHT {
-                    grid[avenue.x as usize][y] = '#';
-                }
-            }
-            None => {
-                // no more 
-                break;
-            }
-        }
-    }
-
     let mut e_w_iter = e_w_streets.iter();
-    loop {
-        match e_w_iter.next() {
-            Some(street) => {
-                for x in 0..WIDTH {
-                    grid[x][street.y as usize] = '#';
-                }
-            }
-            None => {
-                // no more 
-                break;
-            }
+    
+    while let (Some(avenue), Some(street)) = (n_s_iter.next(), e_w_iter.next()) {
+        for y in 0..HEIGHT {
+            grid[avenue.x][y] = '#';
+        }
+        for x in 0..WIDTH {
+            grid[x][street.y] = '#';
         }
     }
 
-    for row in 0..HEIGHT {
-        for col in 0..WIDTH {
-            match grid[col][row] {
-                '.' => { print!(".") }
-                '#' => { print!("#") }
-                _ => { }
-            }
+    // Print city
+    for row in grid.iter() {
+        for &cell in row.iter() {
+            print!("{}", cell);
         }
-        println!("");
-
+        println!();
     }
 }
