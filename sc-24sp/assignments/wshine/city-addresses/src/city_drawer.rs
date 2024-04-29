8d47f07897caa2add03fd13c5972879f07f808d4
crowllx <114693487+crowllx@users.noreply.github.com>
2024-04-29 10:30:48 -0700

    city roads lab (#1441)
    * software construction lab 4
    
    * finish random road generator
    
    ---------
    
    Co-authored-by: wshine <114693487+WinstonShine@users.noreply.github.com>

---
+++ sc-24sp/assignments/wshine/city-addresses/src/city_drawer.rs
@@ -0,0 +1,95 @@
+use rand::Rng;
+pub fn gen_random_roads<T: Road + std::fmt::Debug>(bounds: usize, ctor: fn(usize) -> T) -> Vec<T> {
+    let mut rng = rand::thread_rng();
+    let mut random_roads = vec![];
+    Street::new(3);
+    for _ in 0..=10 {
+        random_roads.push(ctor(rng.gen_range(0..bounds)));
+    }
+    println!("{:#?}", random_roads);
+    random_roads
+}
+
+pub trait Road {
+    fn new(coord: usize) -> Self;
+}
+#[derive(Debug)]
+pub struct Street {
+    pub y: usize,
+}
+impl Road for Street {
+    fn new(coord: usize) -> Street {
+        Street { y: coord }
+    }
+}
+#[derive(Debug)]
+pub struct Avenue {
+    pub x: usize,
+}
+impl Road for Avenue {
+    fn new(coord: usize) -> Avenue {
+        Avenue { x: coord }
+    }
+}
+pub struct AddressAvenue {
+    pub x: usize,
+}
+
+pub struct AddressStreet {
+    pub x: usize,
+}
+
+pub const WIDTH: usize = 50;
+pub const HEIGHT: usize = 50;
+
+pub fn city_drawer(n_s_avenues: &mut Vec<Avenue>, e_w_streets: &mut Vec<Street>) {
+    n_s_avenues.sort_by(|a, b| a.x.cmp(&b.x));
+    e_w_streets.sort_by(|a, b| a.y.cmp(&b.y));
+
+    let mut grid: [[char; WIDTH]; HEIGHT] = [['.' as char; WIDTH]; HEIGHT];
+
+    let mut n_s_iter = n_s_avenues.iter();
+    loop {
+        match n_s_iter.next() {
+            Some(avenue) => {
+                for y in 0..HEIGHT {
+                    grid[avenue.x as usize][y] = '#';
+                }
+            }
+            None => {
+                // no more
+                break;
+            }
+        }
+    }
+
+    let mut e_w_iter = e_w_streets.iter();
+    loop {
+        match e_w_iter.next() {
+            Some(street) => {
+                for x in 0..WIDTH {
+                    grid[x][street.y as usize] = '#';
+                }
+            }
+            None => {
+                // no more
+                break;
+            }
+        }
+    }
+
+    for row in 0..HEIGHT {
+        for col in 0..WIDTH {
+            match grid[col][row] {
+                '.' => {
+                    print!(".")
+                }
+                '#' => {
+                    print!("#")
+                }
+                _ => {}
+            }
+        }
+        println!("");
+    }
+}
---
+++ sc-24sp/assignments/wshine/city-addresses/src/main.rs
@@ -0,0 +1,12 @@
+pub mod city_drawer;
+
+use crate::city_drawer::{
+    city_drawer, gen_random_roads, Avenue, Street, Road, HEIGHT, WIDTH,
+};
+
+fn main() {
+    println!("Hello, city!");
+    let mut n_s_avenues = gen_random_roads(HEIGHT, Avenue::new);
+    let mut e_w_streets = gen_random_roads(WIDTH, Street::new);
+    city_drawer(&mut n_s_avenues, &mut e_w_streets);
+}
