c6ad53ceb64bc98f28048b2c8e67a838bff1a19c
Paul Pham <148553+learner-long-life@users.noreply.github.com>
2024-04-29 18:45:49 +0000

    Finished city drawer addressing.

--- sc-24sp/assignments/ppham/city-addresses/src/city_drawer.rs
+++ sc-24sp/assignments/ppham/city-addresses/src/city_drawer.rs
@@ -63,10 +63,28 @@ pub fn city_drawer<'a>(in_grid: &'a mut Grid, roads: &'a Vec<Road>) -> &'a mut G
                     match road.road_type {
                         RoadDirection::NorthSouth => {
                             in_grid[road.coord as usize][w] = '#';
+                            // Is this meant for the North-South orientation,
+                            // if first coord is horizontal (east-west)
+                            // and second coord is vertical (north-south)
+                            // if in_grid[road.coord as usize][w-1] == '.' {
+                            //    in_grid[road.coord as usize][w-1] = 'o';
+                            //}
+                            if road.coord > 0 && in_grid[road.coord-1 as usize][w] == '.' {
+                                in_grid[road.coord-1 as usize][w] = 'o';
+                             }
+                             if road.coord < BOUND-1 && in_grid[road.coord as usize + 1][w] == '.' {
+                                in_grid[road.coord+1 as usize][w] = 'o';
+                             } 
                         }
                         RoadDirection::EastWest => {
                             in_grid[w][road.coord] = '#';
+                            if road.coord > 0 && in_grid[w][road.coord-1 as usize] == '.' {
+                                in_grid[w][road.coord-1 as usize] = 'o';
+                            }
 
+                            if road.coord < BOUND-1 && in_grid[w][road.coord+1 as usize] == '.' {
+                                in_grid[w][road.coord+1 as usize] = 'o';
+                            }
                         }
                     }
                 }
@@ -86,6 +104,7 @@ pub fn draw_grid(grid: Grid) {
             match grid[col][row] {
                 '.' => { print!(".") }
                 '#' => { print!("#") }
+                'o' => { print!("o") }
                 _ => { }
             }
         }
