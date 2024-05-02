1ec9eeb8b98ef76ecca0aa2932aa5abebf440e92
crowllx <114693487+crowllx@users.noreply.github.com>
2024-05-02 14:46:48 -0700

    init tokenizer

--- ai-24sp/assignments/wshine/week5/tokenizer.py
+++ ai-24sp/assignments/wshine/week5/tokenizer.py
@@ -1,5 +1,16 @@
+import re
 
-with open("../data/Reversing Secrets Of Reverse Engineering (2005).txt", "r", encoding="utf-8") as f:
+with open(
+    "../data/Reversing Secrets Of Reverse Engineering (2005).txt", "r", encoding="utf-8"
+) as f:
     raw_text = f.read()
 
 print("Total number of charactersi: ", len(raw_text))
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [w.strip() for w in preprocessed if w.strip()]
+
+print(preprocessed[:99])
+
+
+print(len(preprocessed))
