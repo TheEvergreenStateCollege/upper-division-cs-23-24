465a79b3b6d21048c7bdafd8c158a0a9c5460f78
crowllx <114693487+crowllx@users.noreply.github.com>
2024-05-02 14:11:27 -0700

    lab5 init

---
+++ ai-24sp/assignments/wshine/week5/tokenizer.py
@@ -0,0 +1,5 @@
+
+with open("../data/Reversing Secrets Of Reverse Engineering (2005).txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+print("Total number of charactersi: ", len(raw_text))
