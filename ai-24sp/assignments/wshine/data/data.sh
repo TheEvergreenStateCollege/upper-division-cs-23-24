b7a9fca153ad57900480fb8dc5e06600cfe1d6d6
crowllx <114693487+crowllx@users.noreply.github.com>
2024-05-05 18:26:48 -0700

    chapter 2 complete

--- ai-24sp/assignments/wshine/data/data.sh
+++
@@ -1,4 +0,0 @@
-FILE=Reversing\ Secrets\ Of\ Reverse\ Engineering\ \(2005\).pdf
-
-wget https://github.com/codingliang/Hacking/blob/0b693d090856e3f08bcfc73b9bba0a92e75aee6c/Reversing%20Secrets%20Of%20Reverse%20Engineering%20(2005).pdf
-
--- ai-24sp/assignments/wshine/week5/tokenizer.py
+++ ai-24sp/assignments/wshine/week5/tokenizer.py
@@ -1,5 +1,42 @@
 from importlib.metadata import version
 import tiktoken
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+# data set class
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, maxlength, stride):
+        self.tokenizer = tokenizer
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)
+
+        for i in range(0, len(token_ids) - maxlength, stride):
+            input_chunk = token_ids[i : i + maxlength]
+            target_chunk = token_ids[i + 1 : i + maxlength + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+# data loader
+def create_dataloader_v1(
+    txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True
+):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
+    )
+    return dataloader
+
 
 print("tiktoken version: ", version("tiktoken"))
 
@@ -10,29 +47,37 @@ with open(
 print("Total number of charactersi: ", len(raw_text))
 # tokenizer
 tokenizer = tiktoken.get_encoding("gpt2")
+enc_text = tokenizer.encode(raw_text, allowed_special={"<|endoftext|>"})
 
 
-# example 
-# text = "Akwwirw ier"
-# integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-# strings = tokenizer.decode(integers)
-# print(integers)
-# print(strings)
-# print(tokenizer.decode([33901, 1383, 343, 86, 220, 959]))
+# parameters
+vocab_size = len(enc_text)
+output_dim = 256
 
+print("Vocab size: ", vocab_size)
 
-# tokenizing actual data
-enc_text = tokenizer.encode(raw_text,allowed_special={"<|endoftext|>"})
-enc_sample = enc_text[50:]
+# using torch/dataloader
+# small batch sizes lead to less memory use in training but adds noise to model updates
+# this is one of the hyperparameters we use for optimization
+max_length = 4
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False
+)
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
 
+print("Token ids:\n", inputs)
+print("\nInputs Shape:\n",inputs.shape)
 
-context_size = 4
+# embedding layer
+# embedding layer is a lookup from token id -> weight matrix
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+token_embeddings = token_embedding_layer(inputs)
+pos_embedding_layer = torch.nn.Embedding(max_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(max_length))
 
-x = enc_sample[:context_size]
-y = enc_sample[1:context_size+1]
+input_embeddings = token_embeddings + pos_embeddings
 
-for i in range(1, context_size+1):
-    context = enc_sample[:i]
-    desired = enc_sample[i]
-    print(tokenizer.decode(context), "------>", tokenizer.decode([desired]))
-
+print("embeddings dimensions: ", token_embeddings.shape)
+print("pos embeddings dimensions: ",pos_embeddings.shape)
+print("input embeddings dimensions: ",input_embeddings.shape)
