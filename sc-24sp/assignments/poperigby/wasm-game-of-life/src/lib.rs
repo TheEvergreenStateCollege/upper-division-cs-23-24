848a46453af09e348e51640a581fa7e872fb6cbe
Pope_Rigby <poperigby@mailbox.org>
2024-05-13 14:02:06 -0700

    Rust code

--- sc-24sp/assignments/poperigby/wasm-game-of-life/src/lib.rs
+++ sc-24sp/assignments/poperigby/wasm-game-of-life/src/lib.rs
@@ -1,6 +1,113 @@
 mod utils;
 
+use std::fmt;
 use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    pub fn new(width: u32, height: u32) -> Self {
+        Self {
+            width,
+            height,
+            cells: (0..width * height)
+                .map(|i| {
+                    if i % 2 == 0 || i % 7 == 0 {
+                        Cell::Alive
+                    } else {
+                        Cell::Dead
+                    }
+                })
+                .collect(),
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let index = self.index(row, col);
+                let cell = self.cells[index];
+                let live_neighbor_count = self.live_neighbor_count(row, col);
+
+                let next_cell = match (cell, live_neighbor_count) {
+                    // Rule 1: Any live cell with fewer than two live neighbors
+                    // dies, as if caused by underpopulation.
+                    (Cell::Alive, n) if n < 2 => Cell::Dead,
+                    // Rule 2: Any live cell with two or three live neighbors
+                    // lives on to the next generation.
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Rule 3: Any live cell with more than three live
+                    // neighbours dies, as if by overpopulation.
+                    (Cell::Alive, n) if n > 3 => Cell::Dead,
+                    // Rule 4: Any dead cell with exactly three live neighbours
+                    // becomes a live cell, as if by reproduction
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // All other cells remain in the same state.
+                    (state, _) => state,
+                };
+
+                next[index] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+
+    /// Get a cell's index in the one-dimensional array using a row and column
+    fn index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, col: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (col + delta_col) % self.width;
+                let index = self.index(neighbor_row, neighbor_col);
+                count += self.cells[index] as u8
+            }
+        }
+
+        count
+    }
+}
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in self.cells.chunks(self.width as usize) {
+            for cell in row {
+                write!(f, "{}", if *cell == Cell::Dead { '◻' } else { '◼' })?;
+            }
+            writeln!(f)?;
+        }
+
+        Ok(())
+    }
+}
 
 #[wasm_bindgen]
 extern "C" {
@@ -8,6 +115,6 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
 }
--- sc-24sp/assignments/poperigby/wasm-game-of-life/www/index.js
+++ sc-24sp/assignments/poperigby/wasm-game-of-life/www/index.js
@@ -1,3 +1,3 @@
 import * as wasm from "wasm-game-of-life";
 
-wasm.greet();
+wasm.greet("Rust 🦀");
