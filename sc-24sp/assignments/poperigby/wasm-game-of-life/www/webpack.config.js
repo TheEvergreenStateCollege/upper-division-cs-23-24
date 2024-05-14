bd1cd456be6efd25f3806c70091050e2df84b589
Pope_Rigby <poperigby@mailbox.org>
2024-05-14 13:40:27 -0700

    Uhhh

--- sc-24sp/assignments/poperigby/wasm-game-of-life/www/webpack.config.js
+++ sc-24sp/assignments/poperigby/wasm-game-of-life/www/webpack.config.js
@@ -1,14 +1,21 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
-  },
-  mode: "development",
-  plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ],
+    entry: "./bootstrap.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bootstrap.js",
+    },
+    mode: "development",
+    experiments: {
+        syncWebAssembly: true // Enable WebAssembly experiments
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "index.html", to: "index.html" }
+            ]
+        })
+    ],
 };
