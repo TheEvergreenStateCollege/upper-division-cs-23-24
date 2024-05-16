23047dd19be17cadbaf8755c4e8a3b112e43a8c2
SP <bunnyhoppy3@gmail.com>
2024-05-16 22:00:53 +0000

    suffer

---
+++ ai-24sp/assignments/whereismyprozac/week7ch3/compact_attention3.4.py
@@ -0,0 +1,87 @@
+import torch.nn as nn
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key   = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+ 
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        attn_scores = queries @ keys.T # omega
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+        torch.manual_seed(123)
+        sa_v1 = SelfAttention_v1(d_in, d_out)
+        print(sa_v1(inputs))
+
+        queries = sa_v2.W_query(inputs)
+        keys = sa_v2.W_key(inputs) 
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=1)
+        print(attn_weights)
+
+        context_length = attn_scores.shape[0]
+        mask_simple = torch.tril(torch.ones(context_length, context_length))
+        print(mask_simple)
+
+        masked_simple = attn_weights*mask_simple
+        print(masked_simple)
+
+        row_sums = masked_simple.sum(dim=1, keepdim=True)
+        masked_simple_norm = masked_simple / row_sums
+        print(masked_simple_norm)
+
+        mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+        print(masked)
+
+        attn_weights = torch.softmax(masked / keys.shape[-1]**0.5, dim=1)
+        print(attn_weights)
+
+        torch.manual_seed(123)
+        dropout = torch.nn.Dropout(0.5)
+        example = torch.ones(6, 6)
+        print(dropout(example))
+
+        torch.manual_seed(123)
+        print(dropout(attn_weights))
+
+        batch = torch.stack((inputs, inputs), dim=0)
+        print(batch.shape)
+
+        class CausalAttention(nn.Module):
+     def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+           'mask',
+           torch.triu(torch.ones(context_length, context_length),
+           diagonal=1)
+        )
+ 
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        New batch dimension b
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+ 
+        attn_scores = queries @ keys.transpose(1, 2)
+        attn_scores.masked_fill_(
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf) 
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+ 
+        context_vec = attn_weights @ values
+        return context_vec
