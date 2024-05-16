69e1344cb7455800eb013901b745de29d74f1cb3
rilesbe <115034721+rilesbe@users.noreply.github.com>
2024-05-16 21:40:54 +0000

    Did week 7 AI lab

---
+++ ai-24sp/assignments/rilesbe/week7/process.py
@@ -0,0 +1,6 @@
+raw_text = "" 
+with open("../data/betty-crocker.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+print(f"Total number of characters: {len(raw_text)}")
+print(raw_text[:100])
