51a8d9cbc6d57a2f559f43cd9e79aa0b037f0124
pswish <pswish@gmail.com>
2024-05-17 01:00:35 +0000

    Adding more test code for extreme programming

--- sc-24sp/secpol/rilespaul/tests/test_app.py
+++ sc-24sp/secpol/rilespaul/tests/test_app.py
@@ -8,3 +8,14 @@ def client():
         "SQLALCHEMY_DATABASE_URI":"sqlite:///memory:",
         "TESTING":True,
     })
+
+    with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
+            yield client
+
+    def test_create_task(client):
+            response = client.post("/tasks",json={"Title":"New Task"})
+            assert response.status_code == 200
+            assert b"New Task" in response.data
+            assert Task.query.count() == 1
