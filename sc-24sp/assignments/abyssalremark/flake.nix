a8ed3785a552f77e198f00c23c59f950818639f7
AbyssalRemark <dev.dominicsevere@gmail.com>
2024-05-20 19:29:17 -0700

    ok now that I added the rest, now it works

--- sc-24sp/assignments/abyssalremark/flake.nix
+++ sc-24sp/assignments/abyssalremark/flake.nix
@@ -1,38 +1,43 @@
 {
-    #the packages we pull from
-    inputs = {
-        nixpkgs.url = "nixpkgs/nixos-23.11";
-        utils.url = "github:numtide/flake-utils";
-        fenix = {
-            url = "github:nix-community/fenix";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  description = "Replace me";
 
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-    #defines a function that ... 
-    outputs = {self, nixpkgs, utils, fenix, ...}: 
-        utils.lib.eachDefaultSystem ( system:
-            let 
-                pkgs = import nixpkgs {
-                    inherit system; #this makes it so we can make this for anything that can nix
-                };
-                feRust = with fenix.packages.${system}; combine [
-                    stable.toolchain 
-                    targets.wasm32-unknown-unknown.stable.rust-std
+  outputs = { self, fenix, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          f = with fenix.packages.${system}; combine [
+            stable.toolchain
+            targets.wasm32-unknown-unknown.stable.rust-std
+          ];
+        in
+          {
+            devShells.default = 
+              pkgs.mkShell {
+                name = "nixoflife";
+
+                packages = with pkgs; [
+                  f
+                  linuxPackages_latest.perf
+                  lldb
+                  llvmPackages.bintools
+                  nodePackages.typescript-language-server
+                  nodejs_21
+                  vscode-langservers-extracted
+                  wasm-pack
                 ];
-            in
-                {
-                    devShells.default = pkgs.mkShell {
-                        packages = with pkgs; [
-                            nodejs_21
-                            wasm-pack
-                            feRust
-                            llvmPackages.bintools
-                        ];
-                        CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
-                    };
-                }
-        );
 
+                CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
+              };
+          }
+      );
 }
