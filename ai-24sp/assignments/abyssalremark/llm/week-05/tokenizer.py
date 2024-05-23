0d0ff46cf5c67e98954ea1a2db3597777d107164
AbyssalRemark <dominicsevere@gmail.com>
2024-05-23 14:58:00 -0700

    Finally pushing code for ai

--- ai-24sp/assignments/abyssalremark/llm/week-05/tokenizer.py
+++ ai-24sp/assignments/abyssalremark/llm/week-05/tokenizer.py
@@ -1,11 +1,18 @@
-from importlib.metadata import version
+import re
 
-import tiktoken
-import torch
-
-with open("input.txt", "r", encoding="utf-8") as f:
-    raw_text = f.read()
-
-print("Total number of character:", len(raw_text))
-print(raw_text[:99])
-
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+    
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids]) 
+        
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
---
+++ ai-24sp/assignments/abyssalremark/llm/week-07/2_8_positional.py
@@ -0,0 +1,31 @@
+#guess were going full brainless...
+import torch 
+from dataloader import create_dataloader_v1
+
+with open("../data/mark-twain-autobio.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+output_dim = 256
+vocab_size = 50257
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+
+max_length = 4
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False
+)
+data_iter = iter(dataloader)
+
+inputs, targets = next(data_iter)
+
+print("Token ID:\n", inputs)
+print("\n Inputs shape:\n", inputs.shape)
+
+token_embeddings = token_embedding_layer(inputs)
+print("Token embeddings: ", token_embeddings)
+print("Token embeddings shape: ", token_embeddings.shape)
+
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_encoding = torch.arange(context_length)
+print("Positional encoding", pos_encoding)
+
