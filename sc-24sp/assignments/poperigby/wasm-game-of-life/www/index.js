5c0b160ab7078471769bc23d32e14253071a335e
Pope_Rigby <poperigby@mailbox.org>
2024-06-03 13:23:09 -0700

    Add fps counter

--- sc-24sp/assignments/poperigby/wasm-game-of-life/www/index.js
+++ sc-24sp/assignments/poperigby/wasm-game-of-life/www/index.js
@@ -11,6 +11,50 @@ const universe = Universe.new(128, 128);
 const width = universe.width();
 const height = universe.height();
 
+const fps = new class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = 1 / delta * 1000;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // Find the max, min, and mean of our 100 latest timings.
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+  }
+};
+
+
 // Give the canvas room for all of our cells and a 1px border
 // around each of them.
 const canvas = document.getElementById("game-of-life-canvas");
@@ -69,6 +113,8 @@ const drawCells = () => {
 };
 
 const renderLoop = () => {
+    fps.render();
+    
     universe.tick();
 
     drawGrid();
