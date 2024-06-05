d2a006c5c1b3a3ec03a463afa5a35d5d32426119
mk-code <114369920+MKrause-code@users.noreply.github.com>
2024-06-05 12:28:23 -0700

    Add files via upload

--- web-24wi/projects/twigMAK/auth/scripts/API.js
+++ web-24wi/projects/twigMAK/auth/scripts/API.js
@@ -1,13 +1,14 @@
+
 const API = {
     endpoint: "/auth/",
     // ADD HERE ALL THE OTHER API FUNCTIONS
     login: async (user) => {
-        return await API.makePostRequest(API.endpoint + "login", user);
+        return await API.makePostRequest(API.endpoint + "login", user)
     },
     register: async (user) => {
-        return await API.makePostRequest(API.endpoint + "register", user);
+        return await API.makePostRequest(API.endpoint + "register", user)
     },
-    
+
     makePostRequest: async (url, data) => {
         const response = await fetch(url, {
             method: "POST",
@@ -22,4 +23,3 @@ const API = {
 }
 
 export default API;
-
