94697adc4fd05c555b695be14a214d7587e06c25
AkinaSS <114690470+AkinaSS@users.noreply.github.com>
2024-06-05 12:15:10 -0700

    Getting AI code for testing

---
+++ ai-24sp/assignments/AkinaS/week7/3_3_1_self-attention.py
@@ -0,0 +1,33 @@
+import torch
+
+inputs = torch.tensor(
+  [[0.43, 0.15, 0.89], # Your     (x^1)
+   [0.55, 0.87, 0.66], # journey  (x^2)
+   [0.57, 0.85, 0.64], # starts   (x^3)
+   [0.22, 0.58, 0.33], # with     (x^4)
+   [0.77, 0.25, 0.10], # one      (x^5)
+   [0.05, 0.80, 0.55]] # step     (x^6)
+)
+
+query = inputs[1]  # 2nd input token is the query
+
+attn_scores_2 = torch.empty(inputs.shape[0])
+for i, x_i in enumerate(inputs):
+    attn_scores_2[i] = torch.dot(x_i, query) # dot product (transpose not necessary here since they are 1-dim vectors)
+
+print(attn_scores_2)
+
+# Approach 1: Common normalization
+attn_weights_2_tmp = attn_scores_2 / attn_scores_2.sum()
+
+print("Attention weights:", attn_weights_2_tmp)
+print("Sum:", attn_weights_2_tmp.sum())
+
+# Approach 2: Softmax function
+def softmax_naive(x):
+    return torch.exp(x) / torch.exp(x).sum(dim=0)
+
+attn_weights_2_naive = softmax_naive(attn_scores_2)
+
+print("Attention weights:", attn_weights_2_naive)
+print("Sum:", attn_weights_2_naive.sum())
