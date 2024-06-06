f67387d426221f28f69c54bed3ff57ebd922656f
ty <tyresefreeman99@gmail.com>
2024-06-05 22:47:41 -0700

    update

--- web-24wi/projects/rand-lang-gen/new-project/src/db.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient, Prisma } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export default prisma;
--- web-24wi/projects/rand-lang-gen/rand-lang-gen/src/index.ts
+++
@@ -1,5 +0,0 @@
-import app from './server'
-
-app.listen(3001, () => {
-    console.log('hello on http://localhost:3001')
-})
