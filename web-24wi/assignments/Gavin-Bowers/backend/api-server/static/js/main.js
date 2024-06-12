12cd2e4eca082ee51bff86157d9b123921041030
Gavin Bowers <125422388+Gavin-Bowers@users.noreply.github.com>
2024-06-11 21:02:34 -0700

    added music player functionality and changed visuals

--- web-24wi/assignments/Gavin-Bowers/backend/api-server/static/js/main.js
+++ web-24wi/assignments/Gavin-Bowers/backend/api-server/static/js/main.js
@@ -10,34 +10,105 @@ const playlistTag = document.getElementById('playlist');
 
 const endpoint = "https://gavin-bowers.arcology.builders/"
 
-var paused = true;
 var musiclist = [];
 var playlist = [];
 var playlistIndex = 0;
+var repeat = false;
+var shuffleOn = false;
+var musicStarted = false;
 
 function playAudio() {
-    if (paused) {
+    if (audio.paused) {
+        if (!musicStarted) {
+            playSong();
+            musicStarted = true;
+        }
         audio.play();
     } else {
         audio.pause();
     }
 }
 
+function previousSong() {
+    if (playlistIndex > 0) {
+        playlistIndex--;
+        playSong();
+    }
+}
+
+function nextSong() {
+    if (repeat) {
+        playSong();
+    } else if (playlistIndex < playlist.length - 1) {
+        playlistIndex++;
+        playSong();
+    }
+}
+
+function toggleRepeat() {
+    if (repeat) {
+        repeat = false;
+        document.getElementById('loop-icon').innerHTML = 'repeat';
+    } else {
+        repeat = true;
+        document.getElementById('loop-icon').innerHTML = 'repeat_on';
+    }
+}
+
+function shuffle() {
+    if (shuffleOn) {
+        shuffleOn = false;
+        document.getElementById('shuffle-icon').innerHTML = 'shuffle';
+        playlist = [];
+        for (let song of musiclist) {
+            playlist.push(song);
+        }
+        playlistIndex = 0;
+        let index = 0;
+        for (let song of playlist) {
+            if (currentTitle.textContent ===  song.title && 
+                currentArtist.textContent === song.artist) {
+                playlistIndex = index;
+            }
+            index++;
+        }
+    } else {
+        shuffleOn = true;
+        document.getElementById('shuffle-icon').innerHTML = 'shuffle_on';
+        shufflePlaylist();
+    }
+    displayPlaylist();
+    playlistTag.children[playlistIndex].children[0].style.background = 
+    "linear-gradient(90deg, #f39d72 0%, #e19d9d 23%, #edd492 100%)";
+}
+
+function shufflePlaylist() {
+    for (let i = playlist.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * i + 1);
+        [playlist[i], playlist[j]] = [playlist[j], playlist[i]];
+    }
+    let index = 0;
+        for (let song of playlist) {
+            if (currentTitle.textContent ===  song.title && 
+                currentArtist.textContent === song.artist) {
+                playlistIndex = index;
+            }
+            index++;
+        }
+    [playlist[playlistIndex], playlist[0]] = [playlist[0], playlist[playlistIndex]];
+    playlistIndex = 0;
+}
+
 audio.onplay = (event) => {
-    paused = false;
     playIcon.innerHTML = 'pause';
 }
 
 audio.onpause = (event) => {
-    paused = true;
     playIcon.innerHTML = 'play_arrow';
 }
 
 audio.onended = (event) => {
-    if (playlistIndex < playlist.length - 1) {
-        playlistIndex++;
-        playSong();
-    }
+    nextSong();
 }
 
 volumeSlider.addEventListener('input', (event) => {
@@ -67,13 +138,21 @@ async function getMusicData() {
     for (let song of musiclist) {
         playlist.push(song);
     }
+    displayPlaylist();
+}
+
+function displayPlaylist() {
+    while (playlistTag.lastChild) {
+        playlistTag.removeChild(playlistTag.lastChild);
+    }
     let index = 0;
     for (let song of playlist) {
         let playlistElement = document.createElement('li');
         let songButton = document.createElement('button');
+        songButton.className = "playlist-song"
         let closureIndex = index; //Copies the index value for the following closure
         songButton.onclick = function() {playlistIndex = closureIndex; playSong();};
-        songButton.innerHTML = song.title;
+        songButton.innerHTML = song.artist + ' — ' + song.title;
         playlistElement.append(songButton);
         playlistTag.append(playlistElement);
         index++;
@@ -82,11 +161,16 @@ async function getMusicData() {
 
 async function playSong() {
     let song = playlist[playlistIndex];
-    console.log(playlistIndex);
-    console.log(song);
     currentAudio.src = endpoint + 'audio/' + song.filename;
     currentArtist.innerHTML = song.artist;
     currentTitle.innerHTML = song.title;
+
+    for (let child of playlistTag.children) {
+        child.children[0].style.background = 
+            "linear-gradient(90deg, rgba(238,120,62,1) 0%, rgba(214,122,122,1) 23%, rgba(227,189,88,1) 100%)";
+    }
+    playlistTag.children[playlistIndex].children[0].style.background = 
+        "linear-gradient(90deg, #f39d72 0%, #e19d9d 23%, #edd492 100%)";
     currentAudio.load();
     await audio.play();
 }
